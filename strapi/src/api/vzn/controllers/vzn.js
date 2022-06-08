c5bd5303c136622156c18c0fc376eb146af06e63
mohsen <mohsen@kernadler.com>
2022-06-08 16:09:46 +0430

    added new changes

---
+++ strapi/src/api/vzn/controllers/vzn.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  vzn controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::vzn.vzn');
---
+++ strapi/src/api/vzn/services/vzn.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * vzn service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::vzn.vzn');
