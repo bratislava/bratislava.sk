34806b71ea93c4a4c716d8351bb0747df3b70909
MiralemMemic <miralem@kernadler.com>
2022-06-14 10:19:21 +0200

    restore default database.js

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -5,8 +5,8 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'postgres4'),
-      user: env('DATABASE_USERNAME', 'miralem'),
-      password: env('DATABASE_PASSWORD', 'miralem'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'password'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
     },
     debug: false,
