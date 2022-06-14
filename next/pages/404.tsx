ca6a72325c8efa12533cee69c92e1ceaf69aafc1
MiralemMemic <miralem@kernadler.com>
2022-06-14 22:01:41 +0200

    adding translations

--- next/pages/404.tsx
+++ next/pages/404.tsx
@@ -2,9 +2,32 @@ import { Button } from '@bratislava/ui-bratislava/Button/Button'
 import NoResultsIlustration from '../assets/images/no-results-small.svg'
 import ChevronRight from '../assets/images/chevron-right.svg'
 import { useUIContext } from '@bratislava/common-frontend-ui-context'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { AsyncServerProps } from '@utils/types'
+import { useTranslation } from 'next-i18next'
+
+// export const getServerSideProps = async (ctx: any) => {
+//   const locale = ctx.locale ?? 'sk'
+//   return {
+//     props: {
+//       page: {
+//         locale: ctx.locale,
+//         localizations: ['sk', 'en']
+//           .filter((l) => l !== ctx.locale)
+//           .map((l) => ({
+//             slug: '',
+//             locale: l,
+//           })),
+//       },
+
+//       ...(await serverSideTranslations(locale, ['common', 'footer'])),
+//     },
+//   }
+//}
 
 const NotFoundPage = () => {
   const { Link } = useUIContext()
+  const { t } = useTranslation('common')
 
   return (
     <div className="w-screen h-screen flex px-7 py-10 md:pl-36 md:pr-32 xl:pl-80 xl:pr-70">
@@ -13,9 +36,7 @@ const NotFoundPage = () => {
         {/* text-5xl font-extrabold does not work */}
         <div className="flex flex-col items-center lg:items-start">
           <div className="text-[48px] lg:text-[64px] font-[800] pb-4">404</div>
-          <div className="text-center lg:text-left lg:text-default max-w-xs pb-10">
-            Ľutujeme, pre dané vyhľadávaniesa nenašli žiadne výsledky.
-          </div>
+          <div className="text-center lg:text-left lg:text-default max-w-xs pb-10">{t('sorryNoResultsFound')}</div>
           <Link href="/">
             <Button
               variant="transparent-black"
