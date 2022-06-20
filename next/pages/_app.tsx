b1a2359ce19497fef5f7fb0c7f6c9923526f34c2
miralem1 <miralem@kernadler.com>
2022-06-20 13:50:56 +0200

    removed msal provider

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -5,39 +5,35 @@ import Link from 'next/link'
 import React from 'react'
 import ContentImage from '../components/atoms/ContentImage'
 import { HomepageMarkdown } from '../components/atoms/HomepageMarkdown'
-import { MsalProvider } from '@azure/msal-react'
-import { msalInstance } from '../services/msal'
 import './index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MsalProvider instance={msalInstance}>
-      <UIContextProvider
-        components={{
-          Link: ({ href, className, children, locale, target, rel }) => {
-            if (href === undefined || href === null) return null
-            return (
-              <Link href={href} locale={locale}>
-                <a target={target} rel={rel} href={href} className={className}>
-                  {children}
-                </a>
-              </Link>
-            )
-          },
-          Image: ({ alt, src, shadow }) => <ContentImage alt={alt} src={src} shadow={shadow} />,
-          Markdown: ({ className, content, numericalList, hasBackground }) => (
-            <HomepageMarkdown
-              className={className}
-              content={content}
-              numericalList={numericalList}
-              hasBackground={hasBackground}
-            />
-          ),
-        }}
-      >
-        <Component {...pageProps} />
-      </UIContextProvider>
-    </MsalProvider>
+    <UIContextProvider
+      components={{
+        Link: ({ href, className, children, locale, target, rel }) => {
+          if (href === undefined || href === null) return null
+          return (
+            <Link href={href} locale={locale}>
+              <a target={target} rel={rel} href={href} className={className}>
+                {children}
+              </a>
+            </Link>
+          )
+        },
+        Image: ({ alt, src, shadow }) => <ContentImage alt={alt} src={src} shadow={shadow} />,
+        Markdown: ({ className, content, numericalList, hasBackground }) => (
+          <HomepageMarkdown
+            className={className}
+            content={content}
+            numericalList={numericalList}
+            hasBackground={hasBackground}
+          />
+        ),
+      }}
+    >
+      <Component {...pageProps} />
+    </UIContextProvider>
   )
 }
 
