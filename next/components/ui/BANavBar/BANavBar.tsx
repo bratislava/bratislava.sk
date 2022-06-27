a8744623f06266c2aee05dd54fe880fbe558f22f
Martin Pinter <martin.pinter@vacuumlabs.com>
2022-06-27 10:50:48 +0200

    Header font color

--- next/components/ui/BANavBar/BANavBar.tsx
+++ next/components/ui/BANavBar/BANavBar.tsx
@@ -73,7 +73,7 @@ export const BANavBar = ({ className, menuItems, handleSearch, ...languageSelect
             className="flex-1"
             url="/"
             title={
-              <p className="text-red-universal-300">
+              <p className="text-sm text-font">
                 {languageKey === 'en' && <span className="font-semibold">Bratislava </span>}
                 {navBarTexts[languageKey].capitalCity}
                 {languageKey !== 'en' && <span className="font-semibold"> Bratislava</span>}
