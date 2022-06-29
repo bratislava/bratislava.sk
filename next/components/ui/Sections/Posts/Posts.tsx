39e8742598d091e251c4b74935ac7068c74f2647
kenonoke123 <101096442+kenonoke123@users.noreply.github.com>
2022-06-29 11:14:37 +0200

    rozkopavky  margin and hover (#79)
    Co-authored-by: Kenan Omic <kenanomic@Kenans-MacBook-Pro.local>

--- next/components/ui/Sections/Posts/Posts.tsx
+++ next/components/ui/Sections/Posts/Posts.tsx
@@ -8,7 +8,13 @@ import { Button } from '../../Button/Button'
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { ArrowRight, ChevronRight } from '@assets/images'
 import { useUIContext } from '@bratislava/common-frontend-ui-context'
-import { Homepage, BlogPost, HomepageQuery, LatestBlogsWithTagsQuery, BlogPostFragment } from '@bratislava/strapi-sdk-homepage'
+import {
+  Homepage,
+  BlogPost,
+  HomepageQuery,
+  LatestBlogsWithTagsQuery,
+  BlogPostFragment,
+} from '@bratislava/strapi-sdk-homepage'
 import { LatestBlogsFragment, NewsCardBlogFragment } from '@bratislava/strapi-sdk-homepage'
 import { DocumentCards } from '../../DocumentCards/DocumentCards'
 import { DocumentCard } from '../../DocumentCard/DocumentCard'
@@ -143,9 +149,9 @@ export const Posts = ({
         </div>
       )}
       {activeTab > 1 && (
-        <div className="mt-23 px-8 font-sans font-normal lg:text-md text-default text-center items-end">
+        <div className="mt-14 px-8 font-sans font-normal lg:text-md text-default text-center items-end">
           Všetky informácie nájdete na stránke
-          <UILink className="underline" href="https://zverejnovanie.bratislava.sk">
+          <UILink className="underline hover:text-red-brick" href="https://zverejnovanie.bratislava.sk">
             {
               <div className="lg:hidden">
                 <br></br>
