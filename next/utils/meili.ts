0161c4857ec989a51082709286f0fc10308a65ae
miralem1 <miralem@kernadler.com>
2022-06-29 18:14:59 +0200

    Create meili.ts

---
+++ next/utils/meili.ts
@@ -0,0 +1,32 @@
+export interface SearchRequest {
+  index: string
+  keyword: string
+}
+
+export const search = async ({ index, keyword }: SearchRequest): Promise<any> => {
+  const raw = JSON.stringify({
+    q: keyword,
+  })
+  const result = await fetch(`${process.env.MEILI_URL}indexes/${index}/search`, {
+    method: 'post',
+    headers: {
+      Authorization: `Bearer ${process.env.MEILI_API_KEY}`,
+    },
+    body: raw,
+  })
+
+  const resultData = await result.json()
+
+  if (resultData.error) {
+    const error = new Error(resultData.error.message)
+    console.error(error)
+
+    return {
+      value: [],
+    }
+  }
+
+  return resultData
+}
+
+export const pagesFetcher = (keyword: string) => fetch(`/api/search`).then((r) => r.json())
