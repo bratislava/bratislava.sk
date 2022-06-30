2118bd26eefb0668a8630948f41a02a00c21d1b2
jay-kern <jay@kernadler.com>
2022-06-30 16:35:11 +0530

    fix: switch-case in revalidate

--- next/pages/api/revalidate.ts
+++ next/pages/api/revalidate.ts
@@ -1,5 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+type strapiWebhookPayload = {
+  model: string;
+  entry: {
+    slug: string;
+    locale: string;
+  }
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.SECRET_TOKEN) {
@@ -8,25 +16,31 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     // Check model
-    const payload = req.body;
-    if (payload?.model === 'blog-post') {
-      let blogPostUrl = ``;
-      if (payload?.entry?.locale === 'en') {
-        blogPostUrl += `/en`
-      };
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const payload: strapiWebhookPayload = req.body;
 
-      blogPostUrl += `/blog/${payload?.entry?.slug}`;
-      await res.unstable_revalidate(blogPostUrl);
-    }
+    console.log(payload.model)
+    switch(payload?.model) {
+      case 'blog-post': {
+        let blogPostUrl = ``;
+        if (payload?.entry?.locale === 'en') {
+          blogPostUrl += `/en`
+        };
+        blogPostUrl += `/blog/${payload?.entry?.slug}`;
+        await res.unstable_revalidate(blogPostUrl);
+      }
 
-    if (payload?.model === 'page') {
-      let pageUrl = ``;
-      if (payload?.entry?.locale === 'en') {
-        pageUrl += `/en`
-      };
+      case 'page': {
+        let pageUrl = ``;
+        if (payload?.entry?.locale === 'en') {
+          pageUrl += `/en`
+        };
+        pageUrl = `/${payload?.entry?.slug}`;
+        await res.unstable_revalidate(pageUrl);
+      }
 
-      pageUrl = `/${payload?.entry?.slug}`;
-      await res.unstable_revalidate(pageUrl);
+      default:
+        break;
     }
 
     return res.json({ revalidated: true })
