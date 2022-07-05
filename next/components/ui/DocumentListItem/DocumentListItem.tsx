e25797a5b9831f2dc2e5da13c51c90b3ef1c57cb
Martin Pinter <mpinter@users.noreply.github.com>
2022-07-06 01:23:48 +0200

    VZN search (unstyled) (#104)
    * wip
    
    * Official board & search with mocked data
    
    * Fix homepage officialBoard link
    
    * VZN search (unstyled)
    
    * Update search now that index can sort
    
    * Fix download link of main VZN doc
    
    * Fix typescript
    
    * Yarn install
    
    * Download from list, re-add meilisearch
    
    * Add meili env to .env.production

--- next/components/ui/DocumentListItem/DocumentListItem.tsx
+++ next/components/ui/DocumentListItem/DocumentListItem.tsx
@@ -4,14 +4,23 @@ import truncate from 'lodash/truncate'
 
 interface DocumentListItemProps {
   categoryName: string
-  discription: string
+  title: string
   Icon: string
   count: number
   onClick: Function
+  mainDocumentHref?: string
   id: string
 }
 
-export const DocumentListItem = ({ categoryName, discription, Icon, count, onClick, id }: DocumentListItemProps) => {
+export const DocumentListItem = ({
+  categoryName,
+  title,
+  Icon,
+  count,
+  onClick,
+  id,
+  mainDocumentHref,
+}: DocumentListItemProps) => {
   return (
     <div className="flex md:w-full min-w-[280px] md:items-center flex-col md:flex-row shadow-md bg-white mb-1">
       <div className="flex-1 md:justify-center flex py-5 pl-6">
@@ -19,7 +28,7 @@ export const DocumentListItem = ({ categoryName, discription, Icon, count, onCli
       </div>
       <div className="flex justify-start flex-col px-5 pb-3 flex-[4]">
         <div className="text-xs">{categoryName}</div>
-        <div className="text-sm font-semibold text-truncate-2">{truncate(discription, { length: 150 })}</div>
+        <div className="text-sm font-semibold text-truncate-2">{truncate(title, { length: 150 })}</div>
         <div className="text-xs pt-3"> Počet dokumentov: {count}</div>
       </div>
       <div className="flex items-center md:justify-evenly justify-between md:py-11 pl-5 md:px-2 py-4 flex-[2] bg-zinc-50">
@@ -35,9 +44,13 @@ export const DocumentListItem = ({ categoryName, discription, Icon, count, onCli
             Zobraziť
           </Button>
         </div>
-        <div className="pr-6 md:pr-0">
-          <Download />
-        </div>
+        {mainDocumentHref && (
+          <div className="pr-6 md:pr-0">
+            <a href={mainDocumentHref} download>
+              <Download />
+            </a>
+          </div>
+        )}
       </div>
     </div>
   )
---
+++ next/utils/meilisearch.ts
@@ -0,0 +1,15 @@
+import { MeiliSearch } from 'meilisearch'
+
+export const MEILI_PAGE_SIZE = 10
+
+const meiliClient = new MeiliSearch({
+  host: process.env.NEXT_PUBLIC_MEILI_HOST,
+  apiKey: process.env.NEXT_PUBLIC_MEILI_API_KEY,
+})
+
+export const searchVZN = async (search: string, offset: number) => {
+  return await meiliClient.index('vzn').search(search || '*', {
+    sort: ['validFrom:desc'],
+    offset,
+  })
+}
