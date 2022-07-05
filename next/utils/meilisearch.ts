d0fb2f556de6ec51a3ac43e8ec455b3e94834632
Martin Pinter <martin.pinter@vacuumlabs.com>
2022-07-06 00:47:57 +0200

    Update search now that index can sort

--- next/utils/meilisearch.ts
+++ next/utils/meilisearch.ts
@@ -2,9 +2,6 @@ import { MeiliSearch } from 'meilisearch'
 
 export const MEILI_PAGE_SIZE = 10
 
-console.log(process.env.NEXT_PUBLIC_MEILI_HOST)
-console.log(process.env.NEXT_PUBLIC_MEILI_API_KEY)
-
 const meiliClient = new MeiliSearch({
   host: process.env.NEXT_PUBLIC_MEILI_HOST,
   apiKey: process.env.NEXT_PUBLIC_MEILI_API_KEY,
@@ -12,8 +9,7 @@ const meiliClient = new MeiliSearch({
 
 export const searchVZN = async (search: string, offset: number) => {
   return await meiliClient.index('vzn').search(search || '*', {
-    // TODO fix sortable attributes
-    // sort: ['publishedAt:desc'],
-    // offset,
+    sort: ['validFrom:desc'],
+    offset,
   })
 }
