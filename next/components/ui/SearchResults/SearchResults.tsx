217e6202c0e4a76c836523d7090d1e009376d008
miralem1 <miralem@kernadler.com>
2022-07-05 09:56:06 +0200

    fixed endless loop

--- next/components/ui/SearchResults/SearchResults.tsx
+++ next/components/ui/SearchResults/SearchResults.tsx
@@ -3,7 +3,7 @@ import { NoResultsFound } from '../NoResultsFound/NoResultsFound'
 import { PageCards } from '../PageCards/PageCards'
 import { FileList } from '../FileList/FileList'
 import { useTranslation } from 'next-i18next'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import useSWR from 'swr'
 import { SearchOptionProps } from '../AdvancedSearch/AdvancedSearch'
 import { searchArticles, searchPages } from '@utils/meili'
@@ -25,12 +25,14 @@ export const SearchResults = ({ checkedOptions, keyword }: SearchResultsProps) =
   const [articles, setArticles] = useState([])
   const [pages, setPages] = useState([])
 
-  data?.articles
-    ?.then((a) => {
-      setArticles(a)
-    })
-    .catch((e) => console.log(e))
-  data?.pages?.then((p) => setPages(p)).catch((e) => console.log(e))
+  useEffect(() => {
+    data?.articles
+      ?.then((a) => {
+        setArticles(a)
+      })
+      .catch((e) => console.log(e))
+    data?.pages?.then((p) => setPages(p)).catch((e) => console.log(e))
+  }, [data])
 
   const noResultsFound = articles?.length == 0 && pages?.length == 0 && documents?.length == 0
 
@@ -43,8 +45,8 @@ export const SearchResults = ({ checkedOptions, keyword }: SearchResultsProps) =
         <NoResultsFound title={t('weDidntFindAnything')} message={t('tryEnteringSomethingElse')} />
       ) : (
         <div className="flex flex-col gap-y-14 lg:gap-y-24 py-14 lg:py-24">
-          {articles?.length > 0 && articlesSelected && <BlogSearchCards title={t('articles')} blogs={articles} />}
-          {pages?.length > 0 && pagesSelected && <PageCards title={t('websites')} pages={pages} />}
+          {articlesSelected && articles?.length > 0 && <BlogSearchCards title={t('articles')} blogs={articles} />}
+          {pagesSelected && pages?.length > 0 && <PageCards title={t('websites')} pages={pages} />}
           {documents?.length > 0 && (
             <div className="flex flex-col gap-y-3 lg:gap-y-6">
               <div className="text-default lg:text-md font-semibold">{t('documents')}</div>
