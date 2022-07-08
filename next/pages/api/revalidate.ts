a9585954166f460165b282df6734eb9fe9a01e5d
Martin Pinter <martin.pinter@vacuumlabs.com>
2022-07-08 20:11:34 +0200

    Revert changes needed for prod deploy

--- next/pages/api/revalidate.ts
+++ next/pages/api/revalidate.ts
@@ -11,9 +11,9 @@ type strapiWebhookPayload = {
 
 const handler = async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Check for secret to confirm this is a valid request
-  // if (req.query.secret !== process.env.STRAPI_REVALIDATE_SECRET_TOKEN) {
-  //   return res.status(401).json({ message: 'Invalid tokenn' })
-  // }
+  if (req.query.secret !== process.env.STRAPI_REVALIDATE_SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid tokenn' })
+  }
 
   try {
     // Check model
