a12d5777daf0b068e2dc443fe660e3d7e925dbf6
miralem1 <miralem@kernadler.com>
2022-07-12 22:24:45 +0200

    added redirect

--- next/pages/api/robots.ts
+++ next/pages/api/robots.ts
@@ -1,8 +1,9 @@
 import { withSentry } from '@sentry/nextjs'
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import robots from '../../../strapi/public/robots.txt'
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-  return res.json('henlo')
+  res.setHeader('Content-Type', 'text/plain')
+  return res.json(robots)
 }
 
 export default withSentry(handler)
