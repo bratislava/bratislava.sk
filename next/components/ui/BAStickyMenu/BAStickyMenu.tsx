95574bb66f97c2c84a85b17c9e32fb495346605b
Ajay <ajay@kernadler.com>
2022-07-19 12:34:07 +0530

    BA-526 : Mega menu delay 0.5sec

--- next/components/ui/BAStickyMenu/BAStickyMenu.tsx
+++ next/components/ui/BAStickyMenu/BAStickyMenu.tsx
@@ -3,11 +3,12 @@ import cx from 'classnames'
 import React, { useEffect, useState } from 'react'
 import { useRef } from 'react'
 
-import { ArrowRight, ChevronDownSmall, ChevronRight } from '../../../assets/images'
-import StickyMenuTopper from '../../../assets/images/sticky-menu-topper.svg'
+import { ArrowRight, ChevronDownSmall, ChevronRight } from '@assets/images'
+import StickyMenuTopper from '@assets/images/sticky-menu-topper.svg'
 import { getIcon, MenuMainItem, Panel, Waves } from '../index'
 import { isItExternal } from './external-link'
 import { useOutsideClick } from 'rooks'
+import { debounce } from 'lodash'
 
 interface IProps {
   className?: string
@@ -19,22 +20,32 @@ export const BAStickyMenu = ({ className, menuItems, active }: IProps) => {
   const [panelHidden, setPanelHidden] = useState(true)
   const [disableHover, setDisableHover] = useState(false)
 
+  const debouncedHandleMouseEnter = debounce(() => setDisableHover(false), 500)
+
   useEffect(() => {
     if (panelHidden) setPanelHidden(false)
   }, [panelHidden])
 
   const handleMenuCellClick = () => {
-    setPanelHidden(!panelHidden)
-    setDisableHover(!disableHover)
+    setTimeout(() => {
+      setPanelHidden(!panelHidden)
+      setDisableHover(!disableHover)
+    }, 500)
+  }
+
+  const handlOnMouseLeave = () => {
+    setDisableHover(true)
+    debouncedHandleMouseEnter.cancel()
   }
 
   return (
     <div
       className={cx('flex max-w-screen-1.5lg m-auto w-full justify-between', className)}
-      onMouseEnter={() => setDisableHover(false)}
+      onMouseEnter={debouncedHandleMouseEnter}
+      onMouseLeave={handlOnMouseLeave}
     >
       {menuItems.map((item, i) => (
-        <div key={i} className="group flex-1 cursor-pointer">
+        <div key={i} className="group flex-1 cursor-pointer transition delay-500 duration-300 ease-in-out">
           <MenuCell item={item} isActive={item.id === active} handleClick={handleMenuCellClick} />
           <MenuPanel
             item={item}
@@ -58,25 +69,31 @@ const MenuCell = ({ item, isActive, handleClick }: MenuCellProps) => (
   <div className="flex h-[106px] w-40 flex-col items-center " onClick={handleClick}>
     <StickyMenuTopper
       style={{ color: item.colorDark }}
-      className={cx('absolute top-0 transition opacity-0 group-hover:opacity-100 w-30', {
-        'opacity-100': isActive,
-      })}
+      className={cx(
+        'absolute top-0 transition opacity-0 group-hover:opacity-100 w-30 transition delay-500 duration-300 ease-in-out',
+        {
+          'opacity-100': isActive,
+        }
+      )}
     />
     <p
-      className={cx('font-medium text-base mt-5 transition group-hover:font-bold text-center whitespace-pre', {
-        'font-bold': isActive,
-      })}
+      className={cx(
+        'font-medium text-base mt-5 transition group-hover:font-bold text-center whitespace-pre transition delay-500 duration-300 ease-in-out',
+        {
+          'font-bold': isActive,
+        }
+      )}
     >
       {item.title}
     </p>
     <ChevronDownSmall
-      className={cx('mt-3 group-hover:hidden', {
+      className={cx('mt-3 group-hover:hidden transition delay-500 duration-300 ease-in-out', {
         hidden: isActive,
       })}
     />
     <ChevronDownSmall
       style={{ color: item.colorDark }}
-      className={cx('mt-3 group-hover:block', {
+      className={cx('mt-3 group-hover:block transition delay-500 duration-300 ease-in-out', {
         hidden: !isActive,
         block: isActive,
       })}
@@ -100,14 +117,16 @@ const MenuPanel = ({ item, panelHidden, setPanelHidden, disableHover }: MenuPane
   return (
     <div
       className={cx(
-        'cursor-default h-screen hidden pointer-events-none group-hover:pointer-events-auto fixed top-[106px] left-0 right-0 bottom-0 z-30 w-full bg-blackTransparent',
-        { hidden: panelHidden },
-        { 'opacity-0': panelHidden === true },
-        { 'group-hover:flex': !disableHover }
+        'cursor-default h-screen hidden opacity-0 pointer-events-none group-hover:pointer-events-auto fixed top-[106px] left-0 right-0 bottom-0 z-30 w-full bg-blackTransparent transition delay-500 duration-300 ease-in-out',
+        // { hidden: panelHidden },
+        { 'opacity-100': panelHidden === false },
+        { 'group-hover:flex': disableHover === false }
       )}
     >
       <div
-        className={cx('cursor-default grid absolute top-0 left-0 right-0 z-30 w-full pb-20 bg-transparent')}
+        className={cx(
+          'cursor-default grid absolute top-0 left-0 right-0 z-30 w-full pb-20 bg-transparent opacity-0 group-hover:opacity-100 transition delay-500 duration-300 ease-in-out'
+        )}
         ref={ref}
       >
         <Panel style={{ backgroundColor: item.color }} className={cx('px-6 pt-10 pb-10 rounded-none')}>
