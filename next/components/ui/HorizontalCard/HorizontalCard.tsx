f592ecde649f74eac22ffae179b4ff3e6001a410
Kenan Omic <kenanomic@Kenans-MacBook-Pro.local>
2022-07-21 11:20:46 +0200

    removing image clip for mobile devices on homepage

--- next/components/ui/HorizontalCard/HorizontalCard.tsx
+++ next/components/ui/HorizontalCard/HorizontalCard.tsx
@@ -17,7 +17,6 @@ export const HorizontalCard = ({ accessory, className, children, imageSrc, ...re
             style={{
               backgroundImage: `url(${imageSrc})`,
               paddingTop: '71.4%',
-              clipPath: 'polygon(0 0, 100% 0, 100% 90%, 0% 100%)',
             }}
           />
           <div
