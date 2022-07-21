a031cbe6eb17cdfc7c382fc0d36b31fae9721132
kenonoke123 <101096442+kenonoke123@users.noreply.github.com>
2022-07-21 17:57:53 +0200

    Update strapi/config/database.js

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'homepage_v4'),
-      user: env('DATABASE_USERNAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'miralem'),
       password: env('DATABASE_PASSWORD', 'postgres'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
     },
