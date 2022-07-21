492630c0123ead64befdb80cce5c7faf80b75df4
Ajay <ajay@kernadler.com>
2022-07-21 18:09:12 +0530

    BA-557 : BUG ISSUE - calculator

--- next/components/molecules/sections/MinimumCalculator.tsx
+++ next/components/molecules/sections/MinimumCalculator.tsx
@@ -3,6 +3,8 @@ import { Button, Input } from '@bratislava/ui-bratislava'
 import cx from 'classnames'
 import { useTranslation } from 'next-i18next'
 import React, { FormEvent } from 'react'
+import MinusIcon from '../../../assets/images/minus.svg'
+import PlusIcon from '../../../assets/images/plus.svg'
 
 interface IProps {
   className?: string
@@ -27,9 +29,9 @@ const calculateLivingSituation = (
 const MinimumCalculator = ({ className, singleAdultValue, anotherAdultValue, childValue }: IProps) => {
   const { t } = useTranslation('minimum-calculator')
 
-  const [adults, setAdults] = React.useState('1')
-  const [children, setChildren] = React.useState('0')
-  const [income, setIncome] = React.useState('0')
+  const [adults, setAdults] = React.useState(1)
+  const [children, setChildren] = React.useState(0)
+  const [income, setIncome] = React.useState(0)
 
   const [submitted, setSubmitted] = React.useState(false)
 
@@ -48,8 +50,8 @@ const MinimumCalculator = ({ className, singleAdultValue, anotherAdultValue, chi
 
   return (
     <div className={cx('text-center bg-secondary text-font', className)}>
-      <p className="text-lg font-semibold">{t('title')}</p>
-      <p className="m-auto mt-6 w-10/12 pt-0.5 text-default font-medium">{t('description')}</p>
+      <p className="text-default lg:text-lg font-semibold">{t('title')}</p>
+      <p className="m-auto mt-6 w-10/12 pt-0.5 text-sm lg:text-default font-medium">{t('description')}</p>
       <form className="mt-10 flex flex-col items-center gap-y-8 text-default font-medium" onSubmit={handleSubmit}>
         <InputField
           id="adults"
@@ -61,6 +63,9 @@ const MinimumCalculator = ({ className, singleAdultValue, anotherAdultValue, chi
           }}
           placeholder={t('placeholder')}
           min={1}
+          onAddSub={(v) => {
+            setAdults(v > 0 ? v : adults)
+          }}
         />
 
         <InputField
@@ -72,6 +77,9 @@ const MinimumCalculator = ({ className, singleAdultValue, anotherAdultValue, chi
             setSubmitted(false)
           }}
           placeholder={t('placeholder')}
+          onAddSub={(v) => {
+            setChildren(v >= 0 ? v : children)
+          }}
         />
 
         <InputField
@@ -84,10 +92,13 @@ const MinimumCalculator = ({ className, singleAdultValue, anotherAdultValue, chi
           }}
           placeholder={t('placeholder')}
           step="0.01"
+          onAddSub={(v) => {
+            setIncome(v >= 0 ? v : income)
+          }}
         />
 
         <Button
-          className="mt-6 bg-primary py-4 px-6 text-default font-semibold text-font"
+          className="mt-6 bg-primary py-3 px-5 lg:py-4 lg:px-6 text-sm lg:text-default font-semibold text-font"
           icon={<ChevronRight />}
           hoverIcon={<ArrowRight />}
           type="submit"
@@ -113,8 +124,9 @@ const MinimumCalculator = ({ className, singleAdultValue, anotherAdultValue, chi
 interface IInputFieldProps {
   id: string
   label: string
-  value: string
-  onChange: (newValue: string) => void
+  value: number
+  onChange: (newValue: number) => void
+  onAddSub: (newValue: number) => void
 }
 
 const InputField = ({
@@ -122,21 +134,40 @@ const InputField = ({
   label,
   value,
   onChange,
+  onAddSub,
   ...rest
 }: IInputFieldProps &
   Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'onChange'>) => (
-  <label className="flex flex-col items-center text-center" htmlFor={id}>
+  <label className="flex flex-col items-center text-center text-sm lg:text-default" htmlFor={id}>
     {label}
-    <Input
-      id={id}
-      value={value}
-      onChange={(ev) => onChange(ev.target.value ?? '')}
-      className="mt-3 box-border w-64 rounded-lg py-4 px-10 text-center"
-      required
-      type="number"
-      min={0}
-      {...rest}
-    />
+    <div className="flex items-center mt-3 relative">
+      <span
+        className="h-8 w-8 inline-block absolute left-4 flex items-center justify-center cursor-pointer"
+        onClick={() => onAddSub(value - 1)}
+      >
+        <MinusIcon />
+      </span>
+      <Input
+        id={id}
+        value={value}
+        onChange={(e) => {
+          if (e.target.value === '' || /^[0-9\b]+$/.test(e.target.value)) {
+            onChange(parseInt(e.target.value))
+          }
+        }}
+        className="box-border w-64 rounded-lg py-4 px-10 text-center text-default number-control-none"
+        required
+        type="number"
+        min={0}
+        {...rest}
+      />
+      <span
+        className="h-8 w-8 inline-block absolute right-4 flex items-center justify-center cursor-pointer"
+        onClick={() => onAddSub(value + 1)}
+      >
+        <PlusIcon />
+      </span>
+    </div>
   </label>
 )
 
