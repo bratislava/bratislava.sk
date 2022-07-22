55b4eb1e4526b81ddaec88f7d0c9149662344807
Martin Pinter <martin.pinter@vacuumlabs.com>
2022-07-22 10:13:10 +0200

    Updated footer

--- next/components/ui/Footer/Footer.tsx
+++ next/components/ui/Footer/Footer.tsx
@@ -44,8 +44,7 @@ export const Footer = ({
   const { Link: UILink } = useUIContext()
 
   return (
-    <footer className={cx(className, 'text-base text-gray-universal-800 pt-14')}>
-      <hr />
+    <footer className={cx(className, 'text-base text-gray-universal-800')}>
       <section className="flex items-center justify-between pt-14" aria-label="Logo and Social Media Links">
         <BABrand />
         <div className="flex items-center justify-between lg:w-52">
