a43e3b3ce68186bec484fe8c9230901332dd1493
Martin Pinter <martin.pinter@vacuumlabs.com>
2022-08-07 01:01:31 +0200

    Favicon and readme link to docs

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -9,10 +9,20 @@ import React from 'react'
 
 import ContentImage from '../components/atoms/ContentImage'
 import { HomepageMarkdown } from '../components/atoms/HomepageMarkdown'
+import Head from 'next/head'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#e46054" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
