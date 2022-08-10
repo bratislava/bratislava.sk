5bf941f65ba51d59e77b1cf3d16203f9676e74a8
Adam Grund <adam.grund@gmail.com>
2022-08-10 17:27:01 +0200

    BA-671 add bottom margin to HomepageMarkdown blockquote

--- next/components/atoms/HomepageMarkdown.tsx
+++ next/components/atoms/HomepageMarkdown.tsx
@@ -87,7 +87,7 @@ export const HomepageMarkdown = ({ className, content, numericalList, hasBackgro
           </UILink>
         ),
         img: ({ src, alt }) => <div className="flex justify-center">{src && <ContentImage src={src} alt={alt} />}</div>,
-        blockquote: ({ children }) => <div className="border-l-4 border-primary pl-10">{children}</div>,
+        blockquote: ({ children }) => <div className="border-l-4 border-primary pl-10 mb-5 lg:mb-10  last:mb-0">{children}</div>,
         table: ({ children }) => <table className="w-full table-block">{children}</table>,
         tr: ({ children }) => (
           <tr className="py-8 px-1 md:p-0 w-[280px] md:w-full flex flex-col md:table-row rounded-lg bg-white md:odd:bg-white md:even:bg-transparent">
