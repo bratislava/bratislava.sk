67d856626f6be9d617c16c97b82cc92c20138ece
Adam Grund <adam.grund@gmail.com>
2022-08-11 09:44:33 +0200

    BA-654 footer links fix

--- next/components/ui/FooterSection/FooterSection.tsx
+++ next/components/ui/FooterSection/FooterSection.tsx
@@ -1,3 +1,4 @@
+import { useUIContext } from '@bratislava/common-frontend-ui-context';
 import cx from 'classnames'
 
 export interface FooterSectionProps {
@@ -6,19 +7,26 @@ export interface FooterSectionProps {
   pageLinks?: { title: string; url: string }[]
 }
 
-export const FooterSection = ({ className, title, pageLinks }: FooterSectionProps) => (
+export const FooterSection = ({ className, title, pageLinks }: FooterSectionProps) => {
+  const { Link: UILink } = useUIContext()
+
+  return (
   <div className={cx(className, 'flex flex-col gap-y-6 lg:gap-y-11')} aria-label="Footer Section">
     <p className="font-semibold">{title}</p>
     <ul className="flex flex-col gap-y-3 lg:gap-y-5">
       {pageLinks?.map((link, i) => (
         <li className="cursor-pointer" key={i}>
-          <a href={`/${link.url}`} className="hover:underline">
+          <UILink
+            href={link.url?.startsWith('http') || link.url?.startsWith('https') ? link.url : `/${link.url}` ?? '#'}
+            className="hover:underline"
+            target={link.url?.startsWith('http') ? '_blank' : null}
+          >
             {link.title}
-          </a>
+          </UILink>
         </li>
       ))}
     </ul>
   </div>
-)
+)}
 
 export default FooterSection
