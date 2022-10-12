84e177958883d19779c47f8c71a7e1559018f557
matejpeluha <matej.peluha@uxtweak.com>
2022-10-12 17:43:06 +0200

    feat: add upload icon

--- next/components/forms/Upload.tsx
+++ next/components/forms/Upload.tsx
@@ -1,3 +1,4 @@
+import UploadIcon from '@assets/images/forms/upload-icon.svg'
 import cx from 'classnames'
 import { FC } from 'react'
 
@@ -9,16 +10,43 @@ const Upload: FC<UploadProps> = ({ type }: UploadProps) => {
   const inputClassStyles = cx(
     "",
     {
-      "flex w-36 h-14 rounded-lg bg-white": type === 'button',
+      "flex w-36 h-14 rounded-lg bg-white py-3 px-6": type === 'button',
       "flex-column align-items-center": ['button', 'dragAndDrop'].includes(type)
     }
   )
 
+  const getButtonContent = () => {
+    return (
+      <div className="flex flex-row">
+        <UploadIcon/>
+        <p className="text-default">Upload</p>
+      </div>
+    )
+  }
 
+  const getDragAndDropContent = () => {
+    return (
+      <div></div>
+    )
+  }
+
+  const getFileContent = () => {
+    return (
+      <div></div>
+    )
+  }
 
   return (
     <section className={inputClassStyles}>
-
+      { /* CONTENT */
+        type === 'button'
+          ? getButtonContent()
+          : type === 'dragAndDrop'
+            ? getDragAndDropContent()
+            : type === 'file'
+              ? getFileContent()
+              : <div/>
+      }
     </section>
   )
 }
