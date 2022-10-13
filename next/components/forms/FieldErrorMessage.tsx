7f7c2aa4ff56d9e502e2f5306861d561eab65027
Anton Zezul <53222637+AntonZezul@users.noreply.github.com>
2022-10-13 16:05:55 +0200

    Add to FieldErrorMessage.tsx prop disabled

--- next/components/forms/FieldErrorMessage.tsx
+++ next/components/forms/FieldErrorMessage.tsx
@@ -1,17 +1,17 @@
-import cx from 'classnames'
 import React, { DOMAttributes, FC } from 'react'
 
 interface FieldErrorMessageProps {
   errorMessage?: string
   errorMessageProps?: DOMAttributes<never>
+  disabled?: boolean
 }
 
-const FieldErrorMessage: FC<FieldErrorMessageProps> = ({errorMessage, errorMessageProps}) => {
-  return errorMessage ? (
+const FieldErrorMessage: FC<FieldErrorMessageProps> = ({ errorMessage, errorMessageProps, disabled }) => {
+  return errorMessage && !disabled ? (
     <div className="mt-1 text-sm text-error" {...errorMessageProps}>
       {errorMessage}
     </div>
-  ): null;
+  ) : null
 }
 
-export default FieldErrorMessage;
+export default FieldErrorMessage
