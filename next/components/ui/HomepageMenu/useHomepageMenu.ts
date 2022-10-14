06fa1709241a930146ec16bab1037ae27da96de8
JacobMu <me@jakubmurin.dev>
2022-10-14 09:32:38 +0200

    BA-488: Correct hook

--- next/components/ui/HomepageMenu/useHomepageMenu.ts
+++ next/components/ui/HomepageMenu/useHomepageMenu.ts
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import { MutableRefObject, useCallback, useState } from 'react'
-
-import { useIntersectionObserver } from '../../layouts/useIntersectionObserver'
-import { getStickyMenuState, isObserverIntersectingBottom, STICKY_MENU_VISIBILITY } from './HomePageService'
-
-interface HookResult {
-  rootNodeRef: MutableRefObject<HTMLDivElement>
-  observableNodeRef: MutableRefObject<HTMLDivElement>
-  stickyMenuState: STICKY_MENU_VISIBILITY
-  observableNodeHeight: number
-}
-
-export const useHomePageMenu = (): HookResult => {
-  const [stickyMenuState, setStickyMenuState] = useState(STICKY_MENU_VISIBILITY.INVISIBLE)
-  const handleIntersection = useCallback((entries: IntersectionObserverEntry[]) => {
-    const { boundingClientRect } = entries[0]
-    if (!isObserverIntersectingBottom(boundingClientRect.bottom)) {
-      return
-    }
-    setStickyMenuState(getStickyMenuState(entries[0]))
-  }, [])
-
-  const { rootNodeRef, observableNodeRef } = useIntersectionObserver({
-    onCrossingThreshold: handleIntersection,
-    threshold: 0.9,
-  })
-
-  return {
-    rootNodeRef,
-    observableNodeRef,
-    stickyMenuState,
-    observableNodeHeight: rootNodeRef.current?.clientHeight,
-=======
 import { debounce } from 'lodash'
 import { useState } from 'react'
 
@@ -68,6 +34,5 @@ export const useHomepageMenu = (): HookResult => {
     handleClick,
     handleMouseEnter,
     handleMouseLeave,
->>>>>>> 8da9879 (BA-488: Show background color when button active)
   }
 }
