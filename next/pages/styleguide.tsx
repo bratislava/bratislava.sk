3dd3c3f0970b0f4af3de7060b74ee63c1b7225c6
Valerii Medvezhonkov <65829237+AkihitoVM@users.noreply.github.com>
2022-10-19 22:37:56 +0200

    basic style for toggle component (#345)

--- next/pages/styleguide.tsx
+++ next/pages/styleguide.tsx
@@ -10,6 +10,7 @@ import ButtonShowCase from '../components/styleguide/showcases/ButtonShowCase'
 import FieldHeaderShowCase from '../components/styleguide/showcases/FieldHeaderShowCase'
 import SelectFieldShowCase from '../components/styleguide/showcases/SelectFieldShowCase'
 import TagShowCase from '../components/styleguide/showcases/TagShowCase'
+import ToggleShowCase from '../components/styleguide/showcases/ToggleShowCase'
 import StyleGuideWrapper from '../components/styleguide/StyleGuideWrapper'
 
 const Styleguide = ({ page }: AsyncServerProps<typeof getServerSideProps>) => {
@@ -27,6 +28,7 @@ const Styleguide = ({ page }: AsyncServerProps<typeof getServerSideProps>) => {
         <DatePickerShowCase />
         <InputFieldShowCase />
         <SelectFieldShowCase/>
+        <ToggleShowCase/>
       </StyleGuideWrapper>
     </PageWrapper>
   )
