b46b07d94bbe4605e82d49b406c457c2118fd2da
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2022-10-20 22:58:02 +0200

    added onChange prop & code refactoring

--- next/components/styleguide/showcases/SearchFieldShowCase.tsx
+++ next/components/styleguide/showcases/SearchFieldShowCase.tsx
@@ -5,11 +5,13 @@ import { Stack } from '../Stack'
 import { Wrapper } from '../Wrapper'
 
 const SearchFieldShowCase = () => {
+  const [text, setText] = React.useState('Value')
   return (
-    <Wrapper direction="column" title="Search Field">
-      <Stack direction="column">
-        <SearchField label="Label" placeholder="Placeholder" resetIcon/>
-        <SearchField label="Label" placeholder="Placeholder" value="Value" />
+    <Wrapper direction='column' title='Search Field'>
+      <Stack direction='column'>
+        <SearchField label='Label' value={text} placeholder='Placeholder' onChange={(el) => setText(el)} resetIcon />
+        <SearchField label='Label' placeholder='Placeholder' resetIcon />
+        <SearchField label='Label' placeholder='Placeholder' onChange={(el) => setText(el)}/>
         <SearchField label="Label" placeholder="Placeholder" errorMessage="Error message" />
         <SearchField label="Label" placeholder="Placeholder" errorMessage="Error message" disabled />
       </Stack>
