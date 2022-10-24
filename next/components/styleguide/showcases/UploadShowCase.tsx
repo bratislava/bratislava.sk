7d0ddf69283081fed3ce43928872869cb406bd2c
matejpeluha <matej.peluha@uxtweak.com>
2022-10-24 07:21:53 +0200

    refactor: change code styling in show case

--- next/components/styleguide/showcases/UploadShowCase.tsx
+++ next/components/styleguide/showcases/UploadShowCase.tsx
@@ -25,70 +25,41 @@ const UploadShowCase: FC<UploadShowCaseProps> = ({}: UploadShowCaseProps) => {
     <Wrapper title="Upload" direction="column">
       <Stack direction="column">
         <div className="w-96">
-          <UploadedFile fileName="fish.jpg"
-                        onRemove={() => console.log('REMOVE')}/>
+          <UploadedFile fileName="fish.jpg" onRemove={() => console.log('REMOVE')}/>
         </div>
         <div className="w-96">
-          <UploadedFile fileName="something_wrong.jp"
-                        errorMessage="Error message"
+          <UploadedFile fileName="something_wrong.jp" errorMessage="Error message"
                         onRemove={() => console.log('REMOVE')}/>
         </div>
       </Stack>
       <Stack>
-        <Upload type="button"
-                ref={ref}
-                value={files1}
+        <Upload type="button" ref={ref} value={files1}
                 onChange={(newValue) => {
                   setFiles1(newValue)
                   console.log("REF RESULT:", ref.current?.getAttribute("data-value"))
                 }} />
-        <Upload type="button"
-              disabled/>
+        <Upload type="button" disabled/>
       </Stack>
       <Stack direction="column">
-        <Upload type="button"
-                sizeLimit={5}
-                supportedFormats={['.jpg', '.png', '.pdf']}
-                value={files2}
+        <Upload type="button" sizeLimit={5} supportedFormats={['.jpg', '.png', '.pdf']} value={files2}
                 onChange={(newValue) => setFiles2(newValue)}/>
-        <Upload type="button"
-                sizeLimit={5}
-                value={files3}
-                onChange={(newValue) => setFiles3(newValue)}/>
-        <Upload type="button"
-                supportedFormats={['.jpg', '.png', '.pdf']}
-                value={files4}
+        <Upload type="button" sizeLimit={5} value={files3} onChange={(newValue) => setFiles3(newValue)}/>
+        <Upload type="button" supportedFormats={['.jpg', '.png', '.pdf']} value={files4}
                 onChange={(newValue) => setFiles4(newValue)}/>
-        <Upload type="button"
-                sizeLimit={5}
-                supportedFormats={['.jpg', '.png', '.pdf']}
-                disabled/>
+        <Upload type="button" sizeLimit={5} supportedFormats={['.jpg', '.png', '.pdf']} disabled/>
       </Stack>
       <Stack direction="column">
-        <Upload type="dragAndDrop"
-                value={files5}
-                onChange={(newValue) => setFiles5(newValue)}/>
-        <Upload type="dragAndDrop"
-                disabled/>
+        <Upload type="dragAndDrop" value={files5} onChange={(newValue) => setFiles5(newValue)}/>
+        <Upload type="dragAndDrop" disabled/>
       </Stack>
       <Stack direction="column">
-        <Upload type="dragAndDrop"
-                sizeLimit={5}
-                supportedFormats={['.jpg', '.png', '.pdf']}
-                value={files6}
+        <Upload type="dragAndDrop" sizeLimit={5} supportedFormats={['.jpg', '.png', '.pdf']} value={files6}
                 onChange={(newValue) => setFiles6(newValue)}/>
-        <Upload type="dragAndDrop"
-                sizeLimit={5}
-                value={files7}
+        <Upload type="dragAndDrop" sizeLimit={5} value={files7}
                 onChange={(newValue) => setFiles7(newValue)} />
-        <Upload type="dragAndDrop"
-                supportedFormats={['.jpg', '.png', '.pdf']}
-                value={files8}
+        <Upload type="dragAndDrop" supportedFormats={['.jpg', '.png', '.pdf']} value={files8}
                 onChange={(newValue) => setFiles8(newValue)}/>
-        <Upload type="dragAndDrop"
-                sizeLimit={5}
-                supportedFormats={['.jpg', '.png', '.pdf']}
-                disabled/>
+        <Upload type="dragAndDrop" sizeLimit={5} supportedFormats={['.jpg', '.png', '.pdf']} disabled/>
       </Stack>
     </Wrapper>
   )
