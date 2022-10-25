892a7dc705a5ce0544fbd6b66383977cc7400fef
Kristian Duda <kristianduda83@gmail.com>
2022-10-25 08:53:54 +0200

    xslt3 docs

--- next/utils/xslt.ts
+++ next/utils/xslt.ts
@@ -1,5 +1,16 @@
 import * as SaxonJS from 'saxon-js'
 
+/**
+ * Returns XSLT 3.0 transformation
+ *
+ * @remarks
+ * To compile a stylesheet held in xslt to a SEF file, use the command line
+ * `xslt3 -xsl:schema.xslt -export:schema.sef.json -t`
+ * 
+ * @param xsltPath - Path to Stylesheet in SEF format
+ * @param data - Data in XML format
+ * @returns transformed data
+ */
 export const transform = async (xsltPath: string, data: string): Promise<string> => {
   const output = await SaxonJS.transform(
     {
