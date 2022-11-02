773aa845ee0eb636ab83fb925d8c11e77818f493
Kristian Duda <kristianduda83@gmail.com>
2022-11-02 10:56:51 +0100

    refactor validateFormName

--- next/pages/api/eforms/[id]/transform/pdf.ts
+++ next/pages/api/eforms/[id]/transform/pdf.ts
@@ -1,30 +1,31 @@
+import forms, { EFormKey, EFormValue } from '@backend/forms'
 import { withSentry } from '@sentry/nextjs'
-import { validateFormName } from '@utils/forms'
 import { forceString } from '@utils/utils'
-import { execFile } from 'child_process'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { execFile } from 'node:child_process'
 import { readFile, unlink, writeFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
 import { cwd } from 'node:process'
 import { v4 as uuid } from 'uuid'
 
 const fop = async (xsltPath: string, xml: string) => {
-  const id = uuid()
+  const id : string = uuid()
   const pdfPath = resolve(cwd(), 'fop', 'tmp', `${id}.pdf`)
   const xmlPath = resolve(cwd(), 'fop', 'tmp', `${id}.xml`)
 
   await writeFile(xmlPath, xml)
 
   const configPath = resolve(cwd(), 'fop', 'fop.xconf')
-  var childArgs = ['-xml', xmlPath, '-xsl', xsltPath, '-pdf', pdfPath, '-c', configPath]
+  const childArgs = ['-xml', xmlPath, '-xsl', xsltPath, '-pdf', pdfPath, '-c', configPath]
 
-  var isWin = /^win/.test(process.platform)
+  const isWin = process.platform.startsWith('win')
   const fopPath = resolve(cwd(), 'fop', `fop${isWin ? '.bat' : ''}`)
 
   return new Promise((resolve, reject) => {
-    execFile(fopPath, childArgs, async (error, stdout, stderr) => {
+    execFile(fopPath, childArgs, async (error) => {
       if (error) {
-        return reject(error)
+        reject(error)
+        return;
       }
 
       try {
@@ -32,8 +33,8 @@ const fop = async (xsltPath: string, xml: string) => {
         resolve(data)
         await unlink(xmlPath);
         await unlink(pdfPath);
-      } catch (error) {
-        reject(error)
+      } catch (error_) {
+        reject(error_)
       }
     })
   })
@@ -43,15 +44,20 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST' || typeof req.body?.data !== 'string')
     return res.status(400).json({ message: 'Invalid method or missing "data" field on body' })
 
-  try {
-    const formName = await validateFormName(req.query.id)
-    if (!formName) throw new Error(`Invalid form name - validateFormName returned: ${formName}`)
-  } catch (e) {
-    console.error(e)
-    return res.status(400).json({ message: 'Invalid form name or url' })
-  }
+    let formSlug: EFormKey
+    let eform: EFormValue
+    try {
+      formSlug = forceString(req.query.id) as any
+      eform = forms[formSlug]
+      // sanity check
+      if (!eform) throw new Error(`Invalid form name - validateFormName returned: ${formSlug}`)
+      else if (!eform.pdfStylesheetPath) throw new Error(`PDF stylesheet not found - validateFormName returned: ${formSlug}`)
+    } catch (error) {
+      console.error(error)
+      return res.status(400).json({ message: 'Invalid form name or url' })
+    }
 
-  const xsltPath = resolve(cwd(), 'forms', forceString(req.query.id), 'Content', 'form.fo.xslt')
+  const xsltPath = resolve(cwd(), 'backend', 'forms', eform.pdfStylesheetPath)
   const data = await fop(xsltPath, req.body.data)
 
   res.setHeader('Content-Type', 'application/pdf')
