c7b73f776fb268e094f2e2276891300f415f8800
Kristian Duda <kristianduda83@gmail.com>
2022-11-15 07:40:23 +0100

    xml to json

--- next/pages/api/eforms/[id]/transform/xmlToJson.ts
+++ next/pages/api/eforms/[id]/transform/xmlToJson.ts
@@ -1,11 +1,9 @@
+import { EFormValue } from '@backend/forms'
+import { getEform, removeNeedlessXmlTransformArraysRecursive } from '@backend/utils/forms'
 import { withSentry } from '@sentry/nextjs'
-import { removeNeedlessXmlTransformArraysRecursive } from '@backend/utils/forms'
-import { forceString } from '@utils/utils'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { readFile } from 'node:fs/promises'
-import { resolve } from 'node:path'
-import { cwd } from 'node:process'
 import { parseStringPromise } from 'xml2js'
+import { firstCharLowerCase } from 'xml2js/lib/processors'
 
 // takes slovensko.sk-ready xml (perhaps serialized from previously filled in eFrom and loaded into browser by user) and converts back to json which we can work with
 // TODO figure out if usable or if it should be rewritten
@@ -14,22 +12,19 @@ import { parseStringPromise } from 'xml2js'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST' || typeof req.body?.data !== 'string')
     return res.status(400).json({ message: 'Invalid method or missing "data" field on body' })
-  let jsonSchema
+
+  let eform: EFormValue
   try {
-    // TODO change how to look up for the schema
-    const formName = req.query.id
-    if (!formName) throw new Error(`Invalid form name - validateFormName returned: ${formName}`)
-    const filePath = resolve(cwd(), 'forms', forceString(req.query.id), 'schema.json')
-    jsonSchema = JSON.parse(await readFile(filePath, 'utf8'))
-    // TODO validate shcema is valid ?
+    eform = getEform(req.query.id)
   } catch (error) {
     console.error(error)
     return res.status(400).json({ message: 'Invalid form name or url' })
   }
+
   // xml2js has issues when top level element isn't a single node
   const wrappedXmlString = `<wrapper>${req.body.data}</wrapper>`
-  const obj = (await parseStringPromise(wrappedXmlString))['wrapper']
-  removeNeedlessXmlTransformArraysRecursive(obj, [], jsonSchema)
+  const obj = (await parseStringPromise(wrappedXmlString, { tagNameProcessors: [firstCharLowerCase]})).wrapper
+  removeNeedlessXmlTransformArraysRecursive(obj, [], eform.schema)
   return res.json(obj)
 }
 
