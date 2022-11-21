8d5152e1b5a9ed1c20cd76002b907a30ba0d07bb
Kristian Duda <kristianduda83@gmail.com>
2022-11-21 12:25:23 +0100

    forms - add XSD schema

--- next/backend/forms/00603481.dopravneZnacenie.sk/xmlTemplate.ts
+++ next/backend/forms/00603481.dopravneZnacenie.sk/xmlTemplate.ts
@@ -1,5 +1,4 @@
-export default `
-<?xml version="1.0" encoding="utf-8"?>
+export default `<?xml version="1.0" encoding="utf-8"?>
 <E-form xmlns="http://schemas.gov.sk/doc/eform/00603481.dopravneZnacenie.sk/0.2"
         xsi:schemaLocation="http://schemas.gov.sk/doc/eform/00603481.dopravneZnacenie.sk/0.2"
         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
@@ -13,6 +12,4 @@ export default `
     <EformUuid>5ea0cad2-8759-4826-8d4c-c59c1d09ec29</EformUuid>
     <SenderID>mailto:hruska@example.com</SenderID>
   </Meta>
-  <Body>
-  </Body>
 </E-form>`
--- next/backend/forms/index.ts
+++ next/backend/forms/index.ts
@@ -5,9 +5,11 @@
 import dopravneZnacenieHtmlStylesheet from './00603481.dopravneZnacenie.sk/form.html.sef.json'
 import dopravneZnacenieTextStylesheet from './00603481.dopravneZnacenie.sk/form.sb.sef.json'
 import dopravneZnacenieSchema from './00603481.dopravneZnacenie.sk/schema.json'
+import dopravneZnacenieXsd from './00603481.dopravneZnacenie.sk/schema.xsd'
 import dopravneZnacenieUiSchema from './00603481.dopravneZnacenie.sk/uiSchema.json'
 import dopravneZnacenieXmlTemplate from './00603481.dopravneZnacenie.sk/xmlTemplate'
 import testSchema from './test/schema.json'
+import testXsd from './test/schema.xsd'
 import testUiSchema from './test/uiSchema.json'
 import testXmlTemplate from './test/xmlTemplate'
 
@@ -15,6 +17,7 @@ const eforms = {
   dopravneZnacenie: {
     schema: dopravneZnacenieSchema,
     uiSchema: dopravneZnacenieUiSchema,
+    xsd: dopravneZnacenieXsd,
     xmlTemplate: dopravneZnacenieXmlTemplate,
     textStylesheet: dopravneZnacenieTextStylesheet,
     htmlStylesheet: dopravneZnacenieHtmlStylesheet,
@@ -22,6 +25,7 @@ const eforms = {
   test: {
     schema: testSchema,
     uiSchema: testUiSchema,
+    xsd: testXsd,
     xmlTemplate: testXmlTemplate,
     textStylesheet: null,
     htmlStylesheet: null,
