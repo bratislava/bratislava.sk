914ea2427ed3ba2fa25aeb4867ff726a4f819977
matejpeluha <matej.peluha@gmail.com>
2022-11-22 17:53:59 +0100

    fix: missing files on client component when going back in stepper

--- next/components/forms/widgets/UploadWidgetRJSF.tsx
+++ next/components/forms/widgets/UploadWidgetRJSF.tsx
@@ -1,13 +1,15 @@
 import { UploadMinioFile } from '@backend/dtos/minio/upload-minio-file.dto'
 import { StrictRJSFSchema, WidgetProps } from '@rjsf/utils'
-import { useState } from 'react'
 import cx from 'classnames'
+import { useEffect, useState } from 'react'
+
 import Upload from '../Upload/Upload'
 import UploadRJSFOptions from '../Upload/UploadRJSFOptions'
 
 interface UploadWidgetRJSFProps extends WidgetProps{
   options: UploadRJSFOptions
   schema: StrictRJSFSchema
+  value: string|string[]
   disabled?: boolean
   multiple?: boolean
   onChange: (value: string|string[]) => void;
@@ -17,6 +19,7 @@ const UploadWidgetRJSF = (props: UploadWidgetRJSFProps) => {
   const {
     options,
     schema,
+    value,
     disabled,
     onChange
   } = props
@@ -32,11 +35,49 @@ const UploadWidgetRJSF = (props: UploadWidgetRJSFProps) => {
   const multiple = schema.type === "array"
   const widgetClassName = cx("px-2 py-4", className)
 
-  const [value, setValue] = useState<UploadMinioFile[]>([])
+  const fileNameToMinioFile = (fileName: string): UploadMinioFile => {
+    const fileNameArray = fileName.split("_")
+    fileNameArray.splice(0,2)
+    const originalName = fileNameArray.join("_")
+    console.log(originalName)
+    return {
+      file: new File([], fileName),
+      isUploading: false,
+      originalName
+    }
+  }
+
+  const transformInitialValue = () => {
+    const files: UploadMinioFile[] = []
+
+    if (!value || value.length === 0) {
+      return files
+    } else if (schema.type === "string" && typeof value === 'string') {
+      const minioFile = fileNameToMinioFile(value)
+      files.push(minioFile)
+    } else if (schema.type === "array" && typeof value !== 'string') {
+      value.forEach(file => {
+        const minioFile = fileNameToMinioFile(file)
+        files.push(minioFile)
+      })
+    }
+
+    return files
+  }
+
+  const [innerValue, setInnerValue] = useState<UploadMinioFile[]>([])
+
+  useEffect(() => {
+    // I need save multiple info about file, which are not available in possible value of rjsf, but needed DURING upload
+    // I am saving these info only in innerValue of widget
+    // but when I go to previous step of stepper, component are rebuild and I still need (now just) fileName, so I read fileNames and transform it
+    setInnerValue(transformInitialValue)
+  }, [])
 
   const handleOneFile = (files: UploadMinioFile[]) => {
-    const fileName = files[0]?.file.name
-    onChange(fileName)
+    if (!files[0].isUploading && !files[0].errorMessage) {
+      onChange(files[0]?.file.name)
+    }
   }
 
   const handleMultipleFiles = (files: UploadMinioFile[]) => {
@@ -50,7 +91,7 @@ const UploadWidgetRJSF = (props: UploadWidgetRJSFProps) => {
   }
 
   const handleOnChange = (files: UploadMinioFile[]) => {
-    setValue(files)
+    setInnerValue(files)
     if (multiple) {
       handleMultipleFiles(files)
     } else {
@@ -58,7 +99,7 @@ const UploadWidgetRJSF = (props: UploadWidgetRJSFProps) => {
     }
   }
 
-  return <Upload type={type} multiple={multiple} value={value} className={widgetClassName} sizeLimit={size} supportedFormats={supportedFormats}
+  return <Upload type={type} multiple={multiple} value={innerValue} className={widgetClassName} sizeLimit={size} supportedFormats={supportedFormats}
                  disabled={disabled} onChange={handleOnChange}/>
 }
 
