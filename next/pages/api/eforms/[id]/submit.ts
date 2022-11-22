0559eda54fcf4ecb1972aa14b5f50030423c8444
Kristian Duda <kristianduda83@gmail.com>
2022-11-22 07:18:40 +0100

    submit - fix getEform

--- next/pages/api/eforms/[id]/submit.ts
+++ next/pages/api/eforms/[id]/submit.ts
@@ -1,7 +1,6 @@
-import forms, { EFormKey, EFormValue } from '@backend/forms'
-import { validateDataWithJsonSchema } from '@backend/utils/forms'
+import { EFormValue } from '@backend/forms'
+import { getEform, validateDataWithJsonSchema } from '@backend/utils/forms'
 import { withSentry } from '@sentry/nextjs'
-import { forceString } from '@utils/utils'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -10,15 +9,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req.body)
   if (req.method !== 'POST') return res.status(400).json({ message: 'Invalid method or missing "data" field on body' })
 
-  let formSlug: EFormKey | undefined
-  let eform: EFormValue | undefined
+  let eform: EFormValue
   try {
-    formSlug = forceString(req.query.id) as EFormKey
-    eform = forms[formSlug]
-    if (!eform) return { notFound: true }
+    eform = getEform(req.query.id)
   } catch (error) {
     console.error(error)
-    return { notFound: true }
+    return res.status(400).json({ message: 'Invalid form name or url' })
   }
 
   const errors = validateDataWithJsonSchema(req.body, eform.schema)
