e0ec2dbd52086fa31460cbc8f1b91cf304b0a034
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2022-11-23 00:03:25 +0100

    rewrote container style, fixed tooltip, still mess with tailwind config

--- next/components/forms/SingleCheckbox.tsx
+++ next/components/forms/SingleCheckbox.tsx
@@ -1,10 +1,10 @@
-// import HelpIcon from '@assets/images/forms/icon-help.svg'
+import HelpIcon from '@assets/images/forms/icon-help.svg'
 import cx from 'classnames'
 import React from 'react'
-import { useCheckbox, useFocusRing } from 'react-aria'
+import { useCheckbox, useFocusRing, VisuallyHidden } from 'react-aria'
 import { useToggleState } from 'react-stately'
 
-// import Tooltip from './Tooltip'
+import Tooltip from './Tooltip'
 
 type CheckBoxBase = {
   variant?: 'basic' | 'boxed'
@@ -28,7 +28,7 @@ const SingleCheckBox = (
     ...rest
   }: CheckBoxBase,
 ) => {
-  // const [isTooltipOpened, setIsTooltipOpened] = React.useState<boolean>(false)
+  const [isTooltipOpened, setIsTooltipOpened] = React.useState<boolean>(false)
   const state = useToggleState(rest)
   const ref = React.useRef<HTMLInputElement>(null)
   const { inputProps } = useCheckbox({ ...rest, isDisabled, children }, state, ref)
@@ -37,7 +37,7 @@ const SingleCheckBox = (
   const isSelected = state.isSelected && !rest.isIndeterminate
 
   const checkboxStyle = cx(
-    'focus-visible: outline-none focus:outline-none appearance-none items-center justify-center w-6 h-6 rounded border-2 border-solid border-gray-universal-700 left-0 right-0 top-0 bottom-0',
+    'flex items-center justify-center w-6 h-6 rounded border-2 border-solid border-gray-universal-700',
     {
       'bg-gray-universal-700': (isSelected || rest.isIndeterminate) && !error,
       'group-hover:border-gray-universal-600':
@@ -52,7 +52,7 @@ const SingleCheckBox = (
     },
   )
 
-  const containerStyle = cx('group flex flex-row relative items-center justify-center p-0 ', rest.className, {
+  const containerStyle = cx('group flex flex-row  items-center justify-center p-0 gap-4', rest.className, {
     'h-12 py-3 px-4 bg-white border-2 border-solid rounded-[8px]': variant === 'boxed',
     'border-gray-universal-300 group-hover:border-[#858585]':
       variant === 'boxed' && !isSelected && rest.isIndeterminate && !isDisabled && !error,
@@ -66,70 +66,70 @@ const SingleCheckBox = (
   })
 
   const labelStyle = cx(
-    'flex select-none not-italic font-normal text-default leading-8 text-gray-universal-700 gap-4 ml-4',
+    'flex select-none not-italic font-normal text-default leading-8 text-gray-universal-700 gap-4',
     {},
   )
 
   return (
-    <label className={containerStyle}>
-      {/*{*/}
-      {/*  tooltip && (*/}
-      {/*    <div className="relative">*/}
-      {/*      <Tooltip text={tooltip} visible={isTooltipOpened} arrow="bottom" alignArrow="right" bottom={20} right={-118} absolute/>*/}
-      {/*    </div>*/}
-      {/*  )*/}
-      {/*}*/}
-      <input className={checkboxStyle} {...inputProps} {...focusProps} ref={ref} />
-
-      {isSelected && (
-        <svg
-          className={cx('absolute', {
-            hidden: !isSelected,
-            'left-[4px]': variant === 'basic',
-            'left-[20px]': variant === 'boxed',
-          })}
-          width='16'
-          height='12'
-          fill='none'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path
-            d='M5.49999 9.47504L2.02499 6.00004L0.845825 7.17921L5.49999 11.8334L15.5 1.83337L14.3208 0.654205L5.49999 9.47504Z'
-            fill='white'
-          />
-        </svg>
-      )}
-      {rest.isIndeterminate && (
-        <svg
-          className={cx('absolute', {
-            hidden: !rest.isIndeterminate,
-            'left-[6px]': variant === 'basic',
-            'left-[23px]': variant === 'boxed',
-          })}
-          width='12'
-          height='2'
-          fill='none'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path d='M11.8333 1.83333H0.166656V0.166668H11.8333V1.83333Z' fill='white' />
-        </svg>
-      )}
-
-      <div className={labelStyle}>
-        {children}
-        {/*{*/}
-        {/*  tooltip && (*/}
-        {/*    <div className='flex-column flex items-center'>*/}
-        {/*      <HelpIcon*/}
-        {/*        className='cursor-pointer'*/}
-        {/*        onMouseOver={() => setIsTooltipOpened(true)}*/}
-        {/*        onMouseLeave={() => setIsTooltipOpened(false)}*/}
-        {/*      />*/}
-        {/*    </div>*/}
-        {/*  )*/}
-        {/*}*/}
-      </div>
-    </label>
+    <div>
+      {
+        tooltip && (
+          <div className="relative">
+            <Tooltip className='w-max' text={tooltip} visible={isTooltipOpened} arrow="bottom" alignArrow="right" bottom={0} right={variant === 'basic' ? -14 : 5} absolute/>
+          </div>
+        )
+      }
+      <label htmlFor={rest.value} className={containerStyle}>
+        <VisuallyHidden>
+          <input id={rest.value} {...inputProps} {...focusProps} ref={ref} />
+        </VisuallyHidden>
+        <div className={checkboxStyle}>
+          {isSelected && (
+            <svg
+              className={cx('', {
+                hidden: !isSelected,
+              })}
+              width='16'
+              height='12'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='M5.49999 9.47504L2.02499 6.00004L0.845825 7.17921L5.49999 11.8334L15.5 1.83337L14.3208 0.654205L5.49999 9.47504Z'
+                fill='white'
+              />
+            </svg>
+          )}
+          {rest.isIndeterminate && (
+            <svg
+              className={cx('', {
+                hidden: !rest.isIndeterminate,
+              })}
+              width='12'
+              height='2'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path d='M11.8333 1.83333H0.166656V0.166668H11.8333V1.83333Z' fill='white' />
+            </svg>
+          )}
+        </div>
+        <div className={labelStyle}>
+          {children}
+          {
+            tooltip && (
+              <div className='flex items-center'>
+                <HelpIcon
+                  className='cursor-pointer'
+                  onMouseOver={() => setIsTooltipOpened(true)}
+                  onMouseLeave={() => setIsTooltipOpened(false)}
+                />
+              </div>
+            )
+          }
+        </div>
+      </label>
+    </div>
   )
 }
 
--- next/components/styleguide/showcases/SingleCheckboxShowCase.tsx
+++ next/components/styleguide/showcases/SingleCheckboxShowCase.tsx
@@ -7,12 +7,12 @@ import { Wrapper } from '../Wrapper'
 const SingleCheckboxShowCase = () => {
   return (
     <Wrapper direction="column" title="Single Checkbox">
-      <Stack>
+      <Stack direction="column">
         <SingleCheckbox value='value4' isIndeterminate tooltip="This is some tooltip">Value</SingleCheckbox>
         <SingleCheckbox value='value5' isDisabled>Value</SingleCheckbox>
         <SingleCheckbox value='value6' error>Value</SingleCheckbox>
       </Stack>
-      <Stack>
+      <Stack direction="column">
         <SingleCheckbox value='value7' variant='boxed' tooltip="This is some tooltip">Value</SingleCheckbox>
         <SingleCheckbox value='value8' isIndeterminate variant='boxed' isDisabled >Value</SingleCheckbox>
         <SingleCheckbox value='value9' variant='boxed' error>Value</SingleCheckbox>
