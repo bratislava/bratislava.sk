7996c0bcd8aeb681cce7ea9e4dfdb2a4054ba241
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2022-11-23 15:25:06 +0100

    Fixed progress bar component via new tailwind config

---
+++ next/components/forms/ProgressBar.tsx
@@ -0,0 +1,35 @@
+import cx from 'classnames'
+import {useProgressBar} from 'react-aria'
+
+type ProgressBarBase = {
+  type?: 'success' | 'default'
+  value: number
+  minValue?: number
+  maxValue?: number
+  className?: string
+}
+
+const ProgressBar = ({ type = 'default', value = 0, minValue = 0, maxValue = 100, className }:ProgressBarBase) => {
+  const {
+    progressBarProps
+  } = useProgressBar({value,minValue,maxValue});
+
+  const percentage = (value - minValue) / (maxValue - minValue);
+  const barWidth = `${Math.round(percentage * 100)}%`;
+
+  const progressBarStyleContainer = cx('flex flex-row items-center p-0 gap-4 w-full h-6', className, {
+  })
+  return (
+    <div {...progressBarProps} className={progressBarStyleContainer}>
+      <div className={cx('flex flex-column items-center w-full h-2 bg-gray-200 rounded-full')}>
+        <div style={{ width: barWidth }} className={cx('rounded-full h-2',{
+          'bg-gray-700': type === 'default',
+          'bg-success-700': type === 'success'
+        })} />
+      </div>
+      <div className={cx('text-p-base not-italic')}>{value}%</div>
+    </div>
+  )
+}
+
+export default ProgressBar
