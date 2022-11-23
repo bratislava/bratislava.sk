b590ad8996ad31e0b250d78858b04a4f4b8d29fe
matejpeluha <matej.peluha@gmail.com>
2022-11-23 22:38:19 +0100

    fix: if value not string, there was always shown label of dropdown row

--- next/components/forms/SelectField/DropdownRow.tsx
+++ next/components/forms/SelectField/DropdownRow.tsx
@@ -23,8 +23,8 @@ const DropdownRow = ({option, selected, type, divider, selectHashCode, onChooseO
   const rowClassName = cx(
     "dropdown flex flex-col w-full px-5 bg-white [&>div]:last:border-0 cursor-pointer hover:bg-form-plain-black-hover",
     {
-      "h-14": option.label === "" || option.label === option.value,
-      "h-full xs:h-[84px]": option.label !== "" && option.label !== option.value
+      "h-14": option.label === "" || option.label === String(option.value),
+      "h-full xs:h-[84px]": option.label !== "" && option.label !== String(option.value)
     },
     selectHashCode
   )
@@ -32,7 +32,7 @@ const DropdownRow = ({option, selected, type, divider, selectHashCode, onChooseO
   const optionClassName = cx(
     "dropdown text-p-md w-full",
     {
-      "font-semibold": option.label !== "" && option.label !== option.value
+      "font-semibold": option.label !== "" && option.label !== String(option.value)
     },
     selectHashCode
   )
@@ -72,7 +72,7 @@ const DropdownRow = ({option, selected, type, divider, selectHashCode, onChooseO
             <div className={`${selectHashCode} dropdown absolute inset-0 z-10`}/>
           </div>
         </div>
-        { option.label !== option.value && <p className={`${selectHashCode} dropdown text-p-sm`}>{option.label}</p> }
+        { option.label !== String(option.value) && <p className={`${selectHashCode} dropdown text-p-sm`}>{option.label}</p> }
       </div>
       { divider && <div className={`${selectHashCode} dropdown border-b-2 border-form-input-default`}/> }
     </div>
