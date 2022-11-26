c818304da375bf048b71bc9e38e7f9f9bf35594d
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2022-11-26 01:41:04 +0100

    fix eslint, removed hardcoded container width

--- next/components/forms/Accordion.tsx
+++ next/components/forms/Accordion.tsx
@@ -12,55 +12,78 @@ type AccordionBase = {
   className?: string
 }
 
-const Accordion = ({ title, content, size, icon = false }: AccordionBase) => {
+const Accordion = ({ title, content, size, icon = false, className }: AccordionBase) => {
   const [isActive, setIsActive] = useState(false)
 
-  const accordionContainerStyle = cx('flex flex-col gap-4 w-full max-w-[480px] border-2 border-solid rounded-xl bg-gray-0 hover:border-gray-500', {
-    'p-5': size === 'sm',
-    'py-6 px-8': size === 'md',
-    'py-8 px-10': size === 'lg',
-    'border-gray-200': !isActive,
-    'border-gray-700': isActive,
-  })
+  const accordionContainerStyle = cx(
+    'flex flex-col gap-4 w-full border-2 border-solid rounded-xl bg-gray-0 hover:border-gray-500',
+    className,
+    {
+      'p-5': size === 'sm',
+      'py-6 px-8': size === 'md',
+      'py-8 px-10': size === 'lg',
+      'border-gray-200': !isActive,
+      'border-gray-700': isActive,
+    },
+  )
 
   return (
     <div className={accordionContainerStyle}>
-      <div className={cx('flex w-full items-center justify-between cursor-pointer gap-4', {
-      })} onClick={() => setIsActive(!isActive)}>
-        {icon &&
-          <div className={cx('flex items-center justify-center',{
-            'w-6 h-6': size === 'sm',
-            'w-8 h-8': size === 'md',
-            'w-10 h-10': size === 'lg'
-          })}>
-            <PersonIcon className={cx('',{
-              'w-4 h-4': size === 'sm',
-              'w-5 h-5': size === 'md',
-              'w-6 h-6': size === 'lg'
-            })} />
+      <div
+        className={cx('flex w-full items-center justify-between cursor-pointer gap-4', {})}
+        onClick={() => setIsActive(!isActive)}
+      >
+        {icon && (
+          <div
+            className={cx('flex items-center justify-center', {
+              'w-6 h-6': size === 'sm',
+              'w-8 h-8': size === 'md',
+              'w-10 h-10': size === 'lg',
+            })}
+          >
+            <PersonIcon
+              className={cx('', {
+                'w-4 h-4': size === 'sm',
+                'w-5 h-5': size === 'md',
+                'w-6 h-6': size === 'lg',
+              })}
+            />
           </div>
-        }
-        <div className={cx('font-semibold not-italic w-full', {
-          'text-h-base': size === 'sm',
-          'text-h-md': size === 'md',
-          'text-h-lg': size === 'lg',
-        })}>
+        )}
+        <div
+          className={cx('font-semibold not-italic w-full', {
+            'text-h-base': size === 'sm',
+            'text-h-md': size === 'md',
+            'text-h-lg': size === 'lg',
+          })}
+        >
           {title}
         </div>
-        <div className={cx('flex items-center justify-center', {
-          'w-10 h-10': size === 'lg',
-          'w-8 h-8': size === 'md',
-          'w-6 h-6': size === 'sm',
-        })}>
-          <ExpandMoreIcon className={cx('', {
-            'transform rotate-180': isActive,
-          })} size={size} />
+        <div
+          className={cx('flex items-center justify-center', {
+            'w-10 h-10': size === 'lg',
+            'w-8 h-8': size === 'md',
+            'w-6 h-6': size === 'sm',
+          })}
+        >
+          <ExpandMoreIcon
+            className={cx('', {
+              'transform rotate-180': isActive,
+            })}
+            size={size}
+          />
         </div>
       </div>
-      {isActive && <div className={cx('flex w-full items-center font-normal not-italic', {
-        'text-h-sm': size === 'sm',
-        'text-p-md': size === 'lg' || size === 'md'
-      })}>{content}</div>}
+      {isActive && (
+        <div
+          className={cx('flex w-full items-center font-normal not-italic', {
+            'text-h-sm': size === 'sm',
+            'text-p-md': size === 'lg' || size === 'md',
+          })}
+        >
+          {content}
+        </div>
+      )}
     </div>
   )
 }
--- next/components/styleguide/showcases/AccordionShowCase.tsx
+++ next/components/styleguide/showcases/AccordionShowCase.tsx
@@ -8,12 +8,18 @@ const AccordionShowCase = () => {
   return (
     <Wrapper direction="column" title="Accordion">
       <Stack direction="column">
-        <Accordion size='sm' title={'Headline'} content={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s'}/>
-        <Accordion size='md' title={'Headline'} content={'Text'}/>
-        <Accordion size='lg' title={'Headline'} content={'Text'}/>
-        <Accordion size='sm' content='Text' title='Headline' icon/>
-        <Accordion size='md' content='Text' title='Headline' icon/>
-        <Accordion size='lg' content='Text' title='Headline' icon/>
+        <Accordion
+          size="sm"
+          title="Headline"
+          content={
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+          }
+        />
+        <Accordion size="md" title="Headline" content="Text" />
+        <Accordion size="lg" title="Headline" content="Text" />
+        <Accordion size="sm" content="Text" title="Headline" icon />
+        <Accordion size="md" content="Text" title="Headline" icon />
+        <Accordion size="lg" content="Text" title="Headline" icon className="w-[500px]" />
       </Stack>
     </Wrapper>
   )
