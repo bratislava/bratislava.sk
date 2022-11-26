ff330b1e998883a841c717c21ec961337a8bab72
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2022-11-26 01:18:24 +0100

    fix eslint

--- next/components/forms/ProgressBar.tsx
+++ next/components/forms/ProgressBar.tsx
@@ -1,5 +1,5 @@
 import cx from 'classnames'
-import {useProgressBar} from 'react-aria'
+import { useProgressBar } from 'react-aria'
 
 type ProgressBarBase = {
   type?: 'success' | 'default'
@@ -9,23 +9,33 @@ type ProgressBarBase = {
   className?: string
 }
 
-const ProgressBar = ({ type = 'default', value = 0, minValue = 0, maxValue = 100, className }:ProgressBarBase) => {
-  const {
-    progressBarProps
-  } = useProgressBar({value,minValue,maxValue});
+const ProgressBar = ({
+  type = 'default',
+  value = 0,
+  minValue = 0,
+  maxValue = 100,
+  className,
+}: ProgressBarBase) => {
+  const { progressBarProps } = useProgressBar({ value, minValue, maxValue })
 
-  const percentage = (value - minValue) / (maxValue - minValue);
-  const barWidth = `${Math.round(percentage * 100)}%`;
+  const percentage = (value - minValue) / (maxValue - minValue)
+  const barWidth = `${Math.round(percentage * 100)}%`
 
-  const progressBarStyleContainer = cx('flex flex-row items-center p-0 gap-4 w-full h-6', className, {
-  })
+  const progressBarStyleContainer = cx(
+    'flex flex-row items-center p-0 gap-4 w-full h-6',
+    className,
+    {},
+  )
   return (
     <div {...progressBarProps} className={progressBarStyleContainer}>
       <div className={cx('flex flex-column items-center w-full h-2 bg-gray-200 rounded-full')}>
-        <div style={{ width: barWidth }} className={cx('rounded-full h-2',{
-          'bg-gray-700': type === 'default',
-          'bg-success-700': type === 'success'
-        })} />
+        <div
+          style={{ width: barWidth }}
+          className={cx('rounded-full h-2', {
+            'bg-gray-700': type === 'default',
+            'bg-success-700': type === 'success',
+          })}
+        />
       </div>
       <div className={cx('text-p-base not-italic')}>{value}%</div>
     </div>
--- next/components/styleguide/showcases/ProgressBarShowCase.tsx
+++ next/components/styleguide/showcases/ProgressBarShowCase.tsx
@@ -8,10 +8,10 @@ const ProgressBarShowCase = () => {
   return (
     <Wrapper direction="column" title="Progress Bar">
       <Stack direction="column">
-        <ProgressBar value={0}/>
-        <ProgressBar value={69}/>
-        <ProgressBar value={10} type='success'/>
-        <ProgressBar value={51} type='success'/>
+        <ProgressBar value={0} />
+        <ProgressBar value={69} className="w-[300px]" />
+        <ProgressBar value={10} type="success" />
+        <ProgressBar value={51} type="success" />
       </Stack>
     </Wrapper>
   )
