242dfc82515da75068d6409437ac14c51b3ed8a0
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2022-11-29 21:46:44 +0100

    Basic logic for radio button in RJSF + changed som logic inside radio button and radio group

--- next/components/forms/ThemedForm.tsx
+++ next/components/forms/ThemedForm.tsx
@@ -1,16 +1,16 @@
 import { withTheme } from '@rjsf/core'
 
+import RadioButtonsWidgetRJSF from './widgets/RadioButtonWidgetRJSF'
 import SelectFieldWidgetRJSF from './widgets/SelectFieldWidgetRJSF'
 
 // you can add custom widgets as well as override the default ones
 // we'll want to override all the default widgets listed here https://react-jsonschema-form.readthedocs.io/en/latest/advanced-customization/custom-widgets-fields/
 const theme = {
   widgets: {
-    SelectField: SelectFieldWidgetRJSF
+    SelectField: SelectFieldWidgetRJSF,
+    RadioButton: RadioButtonsWidgetRJSF,
   },
-  fields: {
-
-  },
+  fields: {},
 }
 
 export const ThemedForm = withTheme(theme)
