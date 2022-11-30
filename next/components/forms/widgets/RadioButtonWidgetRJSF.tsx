0d2ac8b17823f39cfaaa797ab2289f50f26e8636
Valerii Medvezhonkov <65829237+AkihitoVM@users.noreply.github.com>
2022-11-30 21:32:15 +0100

    Basic logic for radio button in RJSF + changed som logic inside radio button and radio group (#420)

---
+++ next/components/forms/widgets/RadioButtonWidgetRJSF.tsx
@@ -0,0 +1,55 @@
+import { EnumOptionsType, StrictRJSFSchema, WidgetProps } from '@rjsf/utils'
+import React from 'react'
+
+import Radio from '../RadioButton/Radio'
+import RadioGroup from '../RadioButton/RadioGroup'
+
+type RadioButtonRJSFOptions = {
+  enumOptions?: EnumOptionsType[]
+  tooltip?: string
+  dropdownDivider?: boolean
+  selectAllOption?: boolean
+  // selectType?: 'one' | 'multiple' | 'arrow' | 'radio'
+  description?: string
+  className?: string
+  variant?: 'basic' | 'boxed' | 'card'
+}
+
+interface RadioButtonFieldWidgetRJSFProps extends WidgetProps {
+  label: string
+  options: RadioButtonRJSFOptions
+  value: any | any[]
+  errorMessage?: string
+  required?: boolean
+  disabled?: boolean
+  placeholder?: string
+  schema: StrictRJSFSchema
+  onChange: (value: any | any[]) => void
+}
+
+const RadioButtonsWidgetRJSF = (props: RadioButtonFieldWidgetRJSFProps) => {
+  const { options, value, onChange } = props
+  const { enumOptions, className } = options
+
+  if (!enumOptions || Array.isArray(value)) return null
+  return (
+    <RadioGroup value={value} onChange={onChange} className={className}>
+      {enumOptions.map((radioElement: any) => {
+        return (
+          <Radio
+            key={radioElement.value}
+            isDisabled={radioElement.schema.disabled}
+            variant={options.variant}
+            value={radioElement.value}
+            error={radioElement.schema.error}
+            tooltip={radioElement.schema.tooltip}
+          >
+            {radioElement.label}
+          </Radio>
+        )
+      })}
+    </RadioGroup>
+  )
+}
+
+export default RadioButtonsWidgetRJSF
