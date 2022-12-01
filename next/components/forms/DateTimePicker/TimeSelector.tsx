f2b5ce1081ae7229a76944566b16c9c9de9a1baf
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2022-12-01 18:41:44 +0100

    FORM-175
    Change TimeSelector like a native selector, add active selection, fix review comment

--- next/components/forms/DateTimePicker/TimeSelector.tsx
+++ next/components/forms/DateTimePicker/TimeSelector.tsx
@@ -2,15 +2,15 @@
 import cx from 'classnames'
 import Button from 'components/forms/Button'
 import padStart from 'lodash/padStart'
-import { ReactNode, useRef, WheelEvent } from 'react'
+import React, { ReactNode, useRef, useState } from 'react'
 import { useButton } from 'react-aria'
 import { useDidMount } from 'rooks'
 
 type TimeSelectorBase = {
   onClose?: () => void
   onSubmit?: () => void
-  setHour: (value: string) => void
-  setMinute: (value: string) => void
+  setHour: React.Dispatch<React.SetStateAction<string>>
+  setMinute: React.Dispatch<React.SetStateAction<string>>
   hour: string
   minute: string
 }
@@ -20,14 +20,10 @@ type ButtonBase = {
   className?: string
   onPress?: () => void
 }
-
-type SelectorBase = {
-  array: number[]
-  type: 'hour' | 'minute'
-}
-
+// Constants
 const HOURS_LIMIT = 23
 const MINUTES_LIMIT = 59
+const SCROLL_STEP = 40
 
 const TimeSelector = ({
   onClose,
@@ -37,8 +33,77 @@ const TimeSelector = ({
   hour,
   minute,
 }: TimeSelectorBase) => {
+  const hourRef = useRef<HTMLDivElement>(null)
+  const minuteRef = useRef<HTMLDivElement>(null)
   const hoursArray = Array.from({ length: HOURS_LIMIT + 1 }, (_, i) => i + 0)
   const minutesArray = Array.from({ length: MINUTES_LIMIT + 1 }, (_, i) => i + 0)
+
+  const [translateYHour, setTranslateYHour] = useState<number>(+hour * -SCROLL_STEP)
+  const [translateYMinute, setTranslateYMinute] = useState<number>(+minute * -SCROLL_STEP)
+
+  const lastPositionHour = -SCROLL_STEP * HOURS_LIMIT
+  const lastPositionMinute = -SCROLL_STEP * MINUTES_LIMIT
+
+  // Function for pass a condition by type (limits of scrolling)
+  const translateMoveCondition = (
+    prevState: number,
+    nextPosition: number,
+    type: 'hour' | 'minute',
+  ): boolean => {
+    if (type === 'hour') {
+      return prevState - nextPosition <= -prevState && prevState - nextPosition >= lastPositionHour
+    }
+    if (type === 'minute') {
+      return (
+        prevState - nextPosition <= -prevState && prevState - nextPosition >= lastPositionMinute
+      )
+    }
+    return false
+  }
+
+  // Set value when scroll with limits by type
+  const setValueCondtion = (prevState: number, delta: number, type: 'hour' | 'minute'): string => {
+    if (prevState === 0 && delta > 0) return `${prevState + delta}`
+    if (type === 'hour' && prevState === HOURS_LIMIT && delta > 0) return `${prevState}`
+    if (type === 'minute' && prevState === MINUTES_LIMIT && delta > 0) return `${prevState}`
+    if (prevState > 0) return `${prevState + delta}`
+    return `${prevState}`
+  }
+
+  const wheelHandler = (event: WheelEvent, type: 'hour' | 'minute') => {
+    // delta is direction (up or down)
+    const delta = event.deltaY > 0 ? 1 : -1
+    const nextPosition = SCROLL_STEP * delta
+
+    if (type === 'hour') {
+      setTranslateYHour((prev) =>
+        translateMoveCondition(prev, nextPosition, type) ? prev - nextPosition : prev,
+      )
+      setHour((prev) => setValueCondtion(+prev, delta, type))
+    }
+    if (type === 'minute') {
+      setTranslateYMinute((prev) =>
+        translateMoveCondition(prev, nextPosition, type) ? prev - nextPosition : prev,
+      )
+      setMinute((prev) => setValueCondtion(+prev, delta, type))
+    }
+    event.preventDefault()
+  }
+  // Events
+  useDidMount(() => {
+    hourRef?.current?.addEventListener('wheel', (e) => wheelHandler(e, 'hour'), { passive: false })
+    hourRef?.current?.addEventListener('mousedown', (e) => e.preventDefault())
+    minuteRef?.current?.addEventListener('wheel', (e) => wheelHandler(e, 'minute'), {
+      passive: false,
+    })
+    minuteRef?.current?.addEventListener('mousedown', (e) => e.preventDefault())
+    return () => {
+      hourRef?.current?.removeEventListener('wheel', (e) => wheelHandler(e, 'hour'))
+      minuteRef?.current?.removeEventListener('wheel', (e) => wheelHandler(e, 'minute'))
+      hourRef?.current?.removeEventListener('mousedown', (e) => e.preventDefault())
+      minuteRef?.current?.removeEventListener('mousedown', (e) => e.preventDefault())
+    }
+  })
 
   const ButtonSelector = ({ children, className, onPress, ...rest }: ButtonBase) => {
     const ref = useRef<HTMLButtonElement>(null)
@@ -57,71 +122,75 @@ const TimeSelector = ({
     )
   }
 
-  const Selector = ({ array, type }: SelectorBase) => {
-    const ref = useRef<HTMLDivElement>(null)
-
-    useDidMount(() => {
-      const wheel = (delta: number) => {
-        const step = 40
-        const position = ref?.current?.scrollTop
-        const nextPos = position ? position + step * delta : 0
-        ref?.current?.scrollTo({ top: nextPos })
-      }
-      const wheelHandler = (event: { deltaY: number; preventDefault: () => void }) => {
-        // TODO: an error with WheelEvent<HTMLDivElement> type (???)
-        // const wheelHandler = (event: WheelEvent<HTMLDivElement>) => {
-        const delta = event.deltaY / 100
-        if (delta > -2 && delta < 2) {
-          wheel(delta)
-        }
-        event.preventDefault()
-      }
-      ref?.current?.addEventListener('wheel', wheelHandler, { passive: false })
-
-      return () => ref?.current?.removeEventListener('wheel', wheelHandler)
-    })
-
-    return (
-      <div
-        ref={ref}
-        className="flex flex-col items-center justify-start overflow-y-auto overflow-x-hidden scrollbar-hide"
-      >
-        <span className="pt-[84px] focus:outline-none" />
-        {array?.map((item) => (
-          <ButtonSelector
-            key={item}
-            onPress={() => {
-              if (type === 'hour') setHour(`${item}`)
-              if (type === 'minute') setMinute(`${item}`)
-            }}
-            className={cx(
-              'cursor-pointer rounded-lg px-10 py-2 hover:bg-gray-50 focus:outline-none',
-              {
-                'bg-gray-100':
-                  (hour === `${item}` && type === 'hour') ||
-                  (minute === `${item}` && type === 'minute'),
-              },
-            )}
-          >
-            <span className="flex h-6 w-12 items-center justify-center">
-              {padStart(`${item}`, 2, '0')}
-            </span>
-          </ButtonSelector>
-        ))}
-        <span className="pb-[84px] focus:outline-none" />
-      </div>
-    )
-  }
-
   return (
     <div className="w-full max-w-xs rounded-lg border-2 border-gray-700 bg-white">
       <div className="flex w-full flex-col justify-between py-10">
         <div className="flex h-fit max-h-52 justify-between overflow-hidden px-4">
-          <Selector array={hoursArray} type="hour" />
+          <div
+            ref={hourRef}
+            className="overflow-x-hidden flex flex-col items-center justify-start overflow-y-auto scrollbar-hide"
+          >
+            <div
+              className="flex flex-col"
+              style={{
+                transform: `translate3d(0px, ${translateYHour}px, 0px)`,
+                transition: 'transform 1000ms cubic-bezier(0.19, 1, 0.22, 1) 0s',
+              }}
+            >
+              <span className="pt-[84px] focus:outline-none" />
+              {hoursArray?.map((item) => (
+                <ButtonSelector
+                  key={item}
+                  onPress={() => {
+                    setHour(`${item}`)
+                    setTranslateYHour(item * -SCROLL_STEP)
+                  }}
+                  className={cx('cursor-pointer rounded-lg px-10 py-2 focus:outline-none', {
+                    'bg-gray-100': +hour === item,
+                  })}
+                >
+                  <span className="flex h-6 w-12 items-center justify-center">
+                    {padStart(`${item}`, 2, '0')}
+                  </span>
+                </ButtonSelector>
+              ))}
+              <span className="pb-[84px] focus:outline-none" />
+            </div>
+          </div>
           <div className="flex items-center">
             <span className="text-20 flex h-6 w-6 items-center justify-center">:</span>
           </div>
-          <Selector array={minutesArray} type="minute" />
+          <div
+            ref={minuteRef}
+            className="overflow-x-hidden flex flex-col items-center justify-start overflow-y-auto scrollbar-hide"
+          >
+            <div
+              className="flex flex-col"
+              style={{
+                transform: `translate3d(0px, ${translateYMinute}px, 0px)`,
+                transition: 'transform 1000ms cubic-bezier(0.19, 1, 0.22, 1) 0s',
+              }}
+            >
+              <span className="pt-[84px] focus:outline-none" />
+              {minutesArray?.map((item) => (
+                <ButtonSelector
+                  key={item}
+                  onPress={() => {
+                    setMinute(`${item}`)
+                    setTranslateYMinute(item * -SCROLL_STEP)
+                  }}
+                  className={cx('cursor-pointer rounded-lg px-10 py-2 focus:outline-none', {
+                    'bg-gray-100': +minute === item,
+                  })}
+                >
+                  <span className="flex h-6 w-12 items-center justify-center">
+                    {padStart(`${item}`, 2, '0')}
+                  </span>
+                </ButtonSelector>
+              ))}
+              <span className="pb-[84px] focus:outline-none" />
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex items-center justify-between border-t-2 border-gray-700 py-3 px-4">
