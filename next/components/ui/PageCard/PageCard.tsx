a88e8cab49d2364456dad0a9ddc22693ed0e5c85
Radoslav Zeman <radoslav.zeman@bratislava.sk>
2022-12-05 03:11:15 +0100

    fix colors on search page results, more minor fixes

--- next/components/ui/PageCard/PageCard.tsx
+++ next/components/ui/PageCard/PageCard.tsx
@@ -1,18 +1,18 @@
+import ChevronRight from '@assets/images/chevron-right-large.svg'
+import PageBlueIcon from '@assets/images/page-blue-icon.svg'
+import PageBlueIconSmall from '@assets/images/page-blue-icon-small.svg'
+import PageBrownIcon from '@assets/images/page-brown-icon.svg'
+import PageBrownIconSmall from '@assets/images/page-brown-icon-small.svg'
+import PageGreenIcon from '@assets/images/page-green-icon.svg'
+import PageGreenIconSmall from '@assets/images/page-green-icon-small.svg'
+import PagePurpleIcon from '@assets/images/page-purple-icon.svg'
+import PagePurpleIconSmall from '@assets/images/page-purple-icon-small.svg'
+import PageRedIcon from '@assets/images/page-red-icon.svg'
+import PageRedIconSmall from '@assets/images/page-red-icon-small.svg'
+import PageYellowIcon from '@assets/images/page-yellow-icon.svg'
+import PageYellowIconSmall from '@assets/images/page-yellow-icon-small.svg'
 import { useUIContext } from '@bratislava/common-frontend-ui-context'
 
-import ChevronRight from '../../../assets/images/chevron-right-large.svg'
-import PageBlueIcon from '../../../assets/images/page-blue-icon.svg'
-import PageBlueIconSmall from '../../../assets/images/page-blue-icon-small.svg'
-import PageBrownIcon from '../../../assets/images/page-brown-icon.svg'
-import PageBrownIconSmall from '../../../assets/images/page-brown-icon-small.svg'
-import PageGreenIcon from '../../../assets/images/page-green-icon.svg'
-import PageGreenIconSmall from '../../../assets/images/page-green-icon-small.svg'
-import PagePurpleIcon from '../../../assets/images/page-purple-icon.svg'
-import PagePurpleIconSmall from '../../../assets/images/page-purple-icon-small.svg'
-import PageRedIcon from '../../../assets/images/page-red-icon.svg'
-import PageRedIconSmall from '../../../assets/images/page-red-icon-small.svg'
-import PageYellowIcon from '../../../assets/images/page-yellow-icon.svg'
-import PageYellowIconSmall from '../../../assets/images/page-yellow-icon-small.svg'
 import Panel from '../Panel/Panel'
 
 const findIconByColor = (pageColor: string) => {
@@ -49,11 +49,41 @@ export interface PageCardProps {
 export const PageCard = ({ pageColor, title, slug }: PageCardProps) => {
   const { Link: UILink } = useUIContext()
 
+  // TODO refactor to use common function
+  const colors = [
+    {
+      pageColor: 'red',
+      backgroundColor: 'rgb(var(--color-main-200))',
+    },
+    {
+      pageColor: 'blue',
+      backgroundColor: 'rgb(var(--color-transport-200))',
+    },
+    {
+      pageColor: 'green',
+      backgroundColor: 'rgb(var(--color-environment-200))',
+    },
+    {
+      pageColor: 'yellow',
+      backgroundColor: 'rgb(var(--color-social-200))',
+    },
+    {
+      pageColor: 'purple',
+      backgroundColor: 'rgb(var(--color-education-200))',
+    },
+    {
+      pageColor: 'brown',
+      backgroundColor: 'rgb(var(--color-culture-200))',
+    },
+  ]
+
   const backgroundColor =
     colors.find((c) => {
       return c.pageColor === pageColor
     })?.backgroundColor ?? 'rgb(var(--color-red--light))'
   const { default: PageIcon, small: SmallPageIcon } = findIconByColor(pageColor)
+
+  console.log('pageColor', pageColor, backgroundColor)
 
   return (
     <UILink href={slug ? `/${slug}` : ''}>
@@ -84,30 +114,3 @@ export const PageCard = ({ pageColor, title, slug }: PageCardProps) => {
     </UILink>
   )
 }
-
-const colors = [
-  {
-    pageColor: 'red',
-    backgroundColor: 'rgb(var(--color-red--light))',
-  },
-  {
-    pageColor: 'blue',
-    backgroundColor: 'rgb(var(--color-blue--light))',
-  },
-  {
-    pageColor: 'green',
-    backgroundColor: 'rgb(var(--color-green--light))',
-  },
-  {
-    pageColor: 'yellow',
-    backgroundColor: 'rgb(var(--color-yellow--light))',
-  },
-  {
-    pageColor: 'purple',
-    backgroundColor: 'rgb(var(--color-purple--light))',
-  },
-  {
-    pageColor: 'brown',
-    backgroundColor: 'rgb(var(--color-brown--light))',
-  },
-]
