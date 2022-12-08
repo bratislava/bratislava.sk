f2e754d3ee1674224c3dc4918dbbacbe61c40896
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2022-12-08 16:24:15 +0100

    FORM-212
    Add spacing props in InputFieldWidgetRJSF

--- next/components/forms/widgets/InputFieldWidgetRJSF.tsx
+++ next/components/forms/widgets/InputFieldWidgetRJSF.tsx
@@ -2,6 +2,8 @@ import { StrictRJSFSchema, WidgetProps } from '@rjsf/utils'
 import InputField from 'components/forms/InputField'
 import React from 'react'
 
+type Spacing = 'large' | 'default' | 'small' | 'none'
+
 type InputFieldRJSFOptions = {
   tooltip?: string
   type?: string
@@ -10,6 +12,8 @@ type InputFieldRJSFOptions = {
   resetIcon?: boolean
   leftIcon?: 'person' | 'mail' | 'call' | 'lock'
   explicitOptional?: boolean
+  spaceBottom?: Spacing
+  spaceTop?: Spacing
 }
 
 interface InputFieldWidgetRJSFProps extends WidgetProps {
@@ -34,27 +38,58 @@ const InputFieldWidgetRJSF = ({
   disabled,
   onChange,
 }: InputFieldWidgetRJSFProps) => {
-  const { description, tooltip, className, resetIcon, leftIcon, explicitOptional, type } = options
+  const {
+    description,
+    tooltip,
+    className,
+    resetIcon,
+    leftIcon,
+    explicitOptional,
+    type,
+    spaceBottom = 'none',
+    spaceTop = 'default',
+  } = options
 
   const handleOnChange = (newValue?: string) => (newValue ? onChange(newValue) : onChange())
 
+  const spacingHandler = (space: Spacing): string => {
+    switch (space) {
+      case 'none':
+        return '0'
+      case 'large':
+        return '40px'
+      case 'default':
+        return '32px'
+      case 'small':
+        return '24px'
+      default:
+        return '0'
+    }
+  }
   return (
-    <InputField
-      label={label}
-      type={type}
-      placeholder={placeholder}
-      value={value}
-      errorMessage={errorMessage}
-      required={required}
-      disabled={disabled}
-      description={description}
-      tooltip={tooltip}
-      className={className}
-      resetIcon={resetIcon}
-      leftIcon={leftIcon}
-      onChange={handleOnChange}
-      explicitOptional={explicitOptional}
-    />
+    <div
+      style={{
+        paddingBottom: spacingHandler(spaceBottom),
+        paddingTop: spacingHandler(spaceTop),
+      }}
+    >
+      <InputField
+        label={label}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        errorMessage={errorMessage}
+        required={required}
+        disabled={disabled}
+        description={description}
+        tooltip={tooltip}
+        className={className}
+        resetIcon={resetIcon}
+        leftIcon={leftIcon}
+        onChange={handleOnChange}
+        explicitOptional={explicitOptional}
+      />
+    </div>
   )
 }
 export default InputFieldWidgetRJSF
