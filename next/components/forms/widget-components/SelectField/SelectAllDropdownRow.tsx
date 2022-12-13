7bcc047c4b1d20a9a82a97bd0bedf2443ff62c01
matejpeluha <matej.peluha@gmail.com>
2022-12-13 14:43:52 +0100

    feat: add deselect all option to dropdown

--- next/components/forms/widget-components/SelectField/SelectAllDropdownRow.tsx
+++ next/components/forms/widget-components/SelectField/SelectAllDropdownRow.tsx
@@ -3,22 +3,37 @@ import { useTranslation } from 'next-i18next'
 interface SelectAllDropdownRowProps {
   divider?: boolean
   selectHashCode?: string
-  onSelectAll: () => void
+  isEverythingDeselected: boolean
+  onSelectAll: (isEverythingSelect: boolean) => void
 }
 
-const SelectAllDropdownRow = ({ divider, selectHashCode, onSelectAll }: SelectAllDropdownRowProps) => {
-  const [t] = useTranslation("forms")
+const SelectAllDropdownRow = ({
+  divider,
+  selectHashCode,
+  isEverythingDeselected,
+  onSelectAll,
+}: SelectAllDropdownRowProps) => {
+  const [t] = useTranslation('forms')
+
+  const handleOnClick = () => {
+    onSelectAll(isEverythingDeselected)
+  }
 
   return (
-    <div className={`${selectHashCode} dropdown flex h-14 w-full cursor-pointer flex-col bg-white px-5 hover:text-gray-600`} onClick={onSelectAll}>
+    <div
+      className={`${selectHashCode} dropdown flex h-14 w-full cursor-pointer flex-col bg-white px-5 hover:text-gray-600`}
+      onClick={handleOnClick}
+    >
       <div className={`${selectHashCode} dropdown flex h-full flex-col justify-center`}>
         <div className={`${selectHashCode} dropdown flex flex-row justify-center`}>
-          <p className={`${selectHashCode} dropdown w-full text-20-semibold`}>
-            { t("selectAll") }
+          <p className={`${selectHashCode} dropdown text-20-semibold w-full`}>
+            {isEverythingDeselected ? t('selectAll') : t('deselectAll')}
           </p>
         </div>
       </div>
-      { divider && <div className={`${selectHashCode} dropdown border-b-2 border-form-input-default`}/> }
+      {divider && (
+        <div className={`${selectHashCode} dropdown border-form-input-default border-b-2`} />
+      )}
     </div>
   )
 }
