8cd3356502226d6901d7a378e61780de37082be3
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2022-12-19 12:28:23 +0100

    FORM-233
    Fix errorMessage prop in Upload changing by mistake

--- next/components/forms/widget-components/Upload/UploadedFile.tsx
+++ next/components/forms/widget-components/Upload/UploadedFile.tsx
@@ -10,24 +10,19 @@ import Spinner from '../../simple-components/Spinner'
 
 interface UploadedFileProps {
   fileName: string
-  errorMessage?: string[]
+  errorMessage?: string
   isUploading?: boolean
   onRemove?: () => void
 }
 
-const UploadedFile = ({
-  fileName,
-  errorMessage = [],
-  isUploading,
-  onRemove,
-}: UploadedFileProps) => {
+const UploadedFile = ({ fileName, errorMessage, isUploading, onRemove }: UploadedFileProps) => {
   const [isHovered, setIsHovered] = useState<boolean>(false)
 
   const classNames = cx(
     'cursor:pointer group linear text-20 flex w-full flex-row gap-2 rounded-lg py-1 px-2 transition-all',
     {
-      'text-error': errorMessage?.length > 0,
-      'hover:bg-gray-100 hover:text-gray-500': !(errorMessage?.length > 0) && !isUploading,
+      'text-error': errorMessage,
+      'hover:bg-gray-100 hover:text-gray-500': !errorMessage && !isUploading,
     },
   )
 
@@ -47,7 +42,7 @@ const UploadedFile = ({
         <div className="flex flex-col justify-center">
           {isUploading ? (
             <Spinner size="sm" className="self-center" />
-          ) : errorMessage?.length > 0 ? (
+          ) : errorMessage ? (
             <PinFileErrorIcon />
           ) : isHovered ? (
             <PinFileHoverIcon />
@@ -58,7 +53,7 @@ const UploadedFile = ({
         <p>{fileName}</p>
       </div>
       <div className="align-center flex w-5 flex-row gap-2">
-        {errorMessage?.length > 0 ? (
+        {errorMessage ? (
           <TrashBinErrorIcon className="cursor-pointer" onClick={handleOnRemove} />
         ) : (
           <TrashBinIcon
