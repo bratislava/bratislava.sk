49c8278262856732ca8967835203e3900a94df8f
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2022-12-20 18:22:41 +0100

    Fix: accidentally forgot to place back other showcases for Field-groups

--- next/components/styleguide/showcases/FieldGroupsShowCase.tsx
+++ next/components/styleguide/showcases/FieldGroupsShowCase.tsx
@@ -27,44 +27,44 @@ const FieldGroupsShowCase = () => {
           addNew="Pridať ďalší riadok"
         />
       </Stack>
-      {/* <Stack direction="column"> */}
-      {/*  <DateFromTo DateToTooltip="Date To Tooltip" label="Dátum (od – do)" /> */}
-      {/* </Stack> */}
-      {/* <Stack direction="column"> */}
-      {/*  <TimeFromTo TimeFromTooltip="Time From Tooltip" label="Čas (od – do)" /> */}
-      {/* </Stack> */}
-      {/* <Stack direction="column"> */}
-      {/*  <DoubledInputField */}
-      {/*    FirstInputLabel="Mesto" */}
-      {/*    SecondInputLabel="PSČ" */}
-      {/*    FirstInputPlaceholder="" */}
-      {/*    SecondInputPlaceholder="" */}
-      {/*  /> */}
-      {/* </Stack> */}
-      {/* <Stack direction="column"> */}
-      {/*  <DateTimePicker DateLabel="Date + time picker" /> */}
-      {/* </Stack> */}
-      {/* <Stack direction="column"> */}
-      {/*  <InputUploadGroup */}
-      {/*    InputLabel="Label" */}
-      {/*    UploadLabel="Nahrajte súbor" */}
-      {/*    middleText="alebo" */}
-      {/*    UploadSizeLimit={5} */}
-      {/*    UploadSupportedFormats={['.jpg', '.png', '.pdf']} */}
-      {/*    InputPlaceholder="" */}
-      {/*    UploadType="button" */}
-      {/*  /> */}
-      {/* </Stack> */}
-      {/* <Stack direction="column"> */}
-      {/*  <TextareaUploadGroup */}
-      {/*    TextareaLabel="Mesto" */}
-      {/*    UploadLabel="Nahrajte súbor" */}
-      {/*    middleText="alebo" */}
-      {/*    UploadSizeLimit={5} */}
-      {/*    UploadSupportedFormats={['.jpg', '.png', '.pdf']} */}
-      {/*    UploadType="button" */}
-      {/*  /> */}
-      {/* </Stack> */}
+      <Stack direction="column">
+        <DateFromTo DateToTooltip="Date To Tooltip" label="Dátum (od – do)" />
+      </Stack>
+      <Stack direction="column">
+        <TimeFromTo TimeFromTooltip="Time From Tooltip" label="Čas (od – do)" />
+      </Stack>
+      <Stack direction="column">
+        <DoubledInputField
+          FirstInputLabel="Mesto"
+          SecondInputLabel="PSČ"
+          FirstInputPlaceholder=""
+          SecondInputPlaceholder=""
+        />
+      </Stack>
+      <Stack direction="column">
+        <DateTimePicker DateLabel="Date + time picker" />
+      </Stack>
+      <Stack direction="column">
+        <InputUploadGroup
+          InputLabel="Label"
+          UploadLabel="Nahrajte súbor"
+          middleText="alebo"
+          UploadSizeLimit={5}
+          UploadSupportedFormats={['.jpg', '.png', '.pdf']}
+          InputPlaceholder=""
+          UploadType="button"
+        />
+      </Stack>
+      <Stack direction="column">
+        <TextareaUploadGroup
+          TextareaLabel="Mesto"
+          UploadLabel="Nahrajte súbor"
+          middleText="alebo"
+          UploadSizeLimit={5}
+          UploadSupportedFormats={['.jpg', '.png', '.pdf']}
+          UploadType="button"
+        />
+      </Stack>
     </Wrapper>
   )
 }
