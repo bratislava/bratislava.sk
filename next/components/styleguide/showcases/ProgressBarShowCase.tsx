84f4b330b3235e6b84c97eef246fd24ed2c871a7
Valerii Medvezhonkov <65829237+ValeriiMedvezhonkov@users.noreply.github.com>
2022-12-23 12:45:20 +0100

    Form-184/Fix: Label warning (#469)

--- next/components/styleguide/showcases/ProgressBarShowCase.tsx
+++ next/components/styleguide/showcases/ProgressBarShowCase.tsx
@@ -9,7 +9,7 @@ const ProgressBarShowCase = () => {
     <Wrapper direction="column" title="Progress Bar">
       <Stack direction="column">
         <ProgressBar value={0} />
-        <ProgressBar value={69} label="Loading..." className="sm:w-[800px]" />
+        <ProgressBar value={69} label="Loading..." className="sm:w-[500px]" />
         <ProgressBar value={10} label="Loading..." type="success" />
         <ProgressBar value={51} type="success" />
       </Stack>
