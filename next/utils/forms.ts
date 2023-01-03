64b31c524cbfd22c0575804a4b68ae451ae224a0
matejpeluha <matej.peluha@gmail.com>
2023-01-03 13:33:06 +0100

    refactor: forms hook

--- next/utils/forms.ts
+++ next/utils/forms.ts
@@ -6,12 +6,47 @@ import { RefObject, useEffect, useRef, useState } from 'react'
 // TODO persist state for session
 // TODO figure out if we need to step over uiSchemas, or having a single one is enough (seems like it is for now)
 export const useFormStepper = (eformSlug: string, schema: StrictRJSFSchema) => {
+  // since Form can be undefined, useRef<Form> is understood as an overload of useRef returning MutableRef, which does not match expected Ref type be rjsf
+  // also, our code expects directly RefObject otherwise it will complain of no `.current`
+  // this is probably a bug in their typing therefore the cast
+  const formRef = useRef<Form>() as RefObject<Form>
+
   const [stepIndex, setStepIndex] = useState(0)
   const [formData, setFormData] = useState<RJSFSchema>({})
-
   const [errors, setErrors] = useState<RJSFValidationError[][]>([])
 
-  console.log('UPDATED STATE:', formData)
+  const steps = schema?.allOf
+  const stepsLength: number = steps?.length ?? -1
+  const isComplete = stepIndex === stepsLength
+
+  const currentSchema = steps ? (steps[stepIndex] as RJSFSchema) : {}
+  const nextSchema = steps ? (steps[stepIndex + 1] as RJSFSchema) : {}
+  const previousSchema = steps ? (steps[stepIndex - 1] as RJSFSchema) : {}
+
+  useEffect(() => {
+    // effect to reset all internal state when critical input 'props' change
+    setFormData({})
+    setStepIndex(0)
+  }, [eformSlug, schema])
+
+  useEffect(() => {
+    // stepIndex allowed to climb one step above the length of steps - i.e. to render a final overview or other custom components but still allow to return back
+    if (stepIndex > stepsLength) {
+      // stepIndex larger than last step index + 1
+      setStepIndex(stepsLength)
+    }
+  }, [stepIndex, steps, stepsLength])
+
+  const previous = () => setStepIndex(stepIndex - 1)
+  const next = () => setStepIndex(stepIndex + 1)
+
+  const setStepFormData = (stepFormData: RJSFSchema) => {
+    const newState = { ...formData }
+    Object.entries(stepFormData).forEach(([key, value]: [string, RJSFSchema]) => {
+      newState[key] = value
+    })
+    setFormData(newState)
+  }
 
   const setUniqueErrors = (newErrors: RJSFValidationError[], actualStepIndex: number) => {
     let updatedErrors: RJSFValidationError[][] = []
@@ -26,61 +61,11 @@ export const useFormStepper = (eformSlug: string, schema: StrictRJSFSchema) => {
     setErrors(updatedErrors)
   }
 
-  // console.log('DATA:', state)
-
-  // since Form can be undefined, useRef<Form> is understood as an overload of useRef returning MutableRef, which does not match expected Ref type be rjsf
-  // also, our code expects directly RefObject otherwise it will complain of no `.current`
-  // this is probably a bug in their typing therefore the cast
-  const formRef = useRef<Form>() as RefObject<Form>
-
-  // TODO validate
-  const steps = schema?.allOf
-  const stepsLength: number = steps?.length ?? -1
-  const isComplete = stepIndex === stepsLength
-
-  const previous = () => setStepIndex(stepIndex - 1)
-  const next = () => {
-    setStepIndex(stepIndex + 1)
-  }
-
-  const forceNext = () => {
-    formRef?.current?.submit()
-  }
-
-  const setStepFormData = (stepFormData: RJSFSchema) => {
-    const newState = { ...formData }
-    Object.entries(stepFormData).forEach(([key, value]: [string, RJSFSchema]) => {
-      newState[key] = value
-    })
-    setFormData(newState)
-  }
-
-  const currentSchema = steps ? (steps[stepIndex] as RJSFSchema) : {}
-
-  // these are used to display header
-  const nextSchema = steps ? (steps[stepIndex + 1] as RJSFSchema) : {}
-  const previousSchema = steps ? (steps[stepIndex - 1] as RJSFSchema) : {}
-
   const validate = () => {
     if (formRef?.current?.validate) {
       formRef?.current?.validateForm()
     }
   }
-
-  // TODO consider validating steps can be merged into single schema without error on mount
-  useEffect(() => {
-    // effect to reset all internal state when critical input 'props' change
-    setFormData({})
-    setStepIndex(0)
-  }, [eformSlug, schema])
-
-  useEffect(() => {
-    // stepIndex allowed to climb one step above the length of steps - i.e. to render a final overview or other custom components but still allow to return back
-    if (stepIndex > stepsLength) {
-      // stepIndex larger than last step index + 1
-      setStepIndex(stepsLength)
-    }
-  }, [stepIndex, steps, stepsLength])
 
   return {
     stepIndex,
