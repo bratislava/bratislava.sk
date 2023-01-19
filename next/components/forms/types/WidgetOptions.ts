e9cba48f53868491d737f1e788b53fdd6d00f59a
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2023-01-19 18:22:14 +0100

    FORM-229
    Add commentar to WidgetOption

--- next/components/forms/types/WidgetOptions.ts
+++ next/components/forms/types/WidgetOptions.ts
@@ -3,6 +3,7 @@ export type FormSpacingType = 'large' | 'default' | 'small' | 'none'
 export type WidgetOptions = {
   tooltip?: string
   helptext?: string
+  // description prop is system prop that we need to be, we use helptext prop instead of description prop
   description?: string
   className?: string
   explicitOptional?: 'none' | 'right' | 'left'
