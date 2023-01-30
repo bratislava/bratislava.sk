1a1e3cc855fe4fb6f64b6e538cc87552af6cfc4f
Valerii Medvezhonkov <65829237+ValeriiMedvezhonkov@users.noreply.github.com>
2023-01-30 20:37:00 +0100

    Form-240/RJSF-FieldGroups (#513)
    * dateFromTo,timeFromTo,dateTime,doubledInput field group widgets
    
    * Fix: Errors messages in field components, timepicker component + styles in timeFromTo field widget
    
    * Fix:
    * changed regex for time format HH:MM to support format as H:MM ( previous regex does not work). Regex was tested, all good
    * For Date[From-To] and Time[From-To] was added RJSF validation logic, and now we should use predefined names for fields like [startDate-endDate] or [startTime-endTime]
    * Fixed DateFromTo and TimeFromTo widgets to use direct predefined names for fields instead of  'index' point (maybe we should do this for others widgets, will be discussed later)
    * Fixed TimePicker component, now it allows us to switch between "user input in field" and input via "modal popup" and use it in a correct way
    * Fixed Date[From-To] logic in DateFromTo widget
    
    * Fix:
    * Fix WidgetWrapping for all field-group fields, added accordions to field-group fields
    * Fix TimePicker, added function to convert time to valid format
    * Fixed error message render, UI styling for field-group widgets
    * Tested all possible variations of time picker and date picker field-group usage

--- next/components/forms/widget-components/DateTimePicker/Calendar/CalendarCell.tsx
+++ next/components/forms/widget-components/DateTimePicker/Calendar/CalendarCell.tsx
@@ -7,23 +7,25 @@ import { CalendarState } from 'react-stately'
 type CalendarCellBase = {
   state: CalendarState
   date: CalendarDate
+  isDisabled?: boolean
 }
 
-const CalendarCell = ({ state, date }: CalendarCellBase) => {
+const CalendarCell = ({ state, date, isDisabled }: CalendarCellBase) => {
   const ref = useRef<HTMLDivElement>(null)
   const { cellProps, buttonProps, isSelected, isOutsideVisibleRange, formattedDate } =
-    useCalendarCell({ date }, state, ref)
+    useCalendarCell({ date, isDisabled }, state, ref)
   return (
     <div {...cellProps}>
       <div
         {...buttonProps}
         ref={ref}
         className={cx(
-          'flex xs:h-10 xs:w-10 h-8 w-8 items-center justify-center text-p2-medium focus:rounded-lg focus:bg-gray-700 focus:text-white focus-visible:outline-none',
+          'text-p2-medium flex xs:h-10 xs:w-10 h-8 w-8 items-center justify-center focus:rounded-lg focus:bg-gray-700 focus:text-white focus-visible:outline-none',
           {
             'rounded-lg bg-gray-700 text-white': isSelected,
-            'hover:rounded-lg hover:bg-gray-50': !isOutsideVisibleRange && !isSelected,
-            'opacity-50': isOutsideVisibleRange,
+            'hover:rounded-lg hover:bg-gray-50':
+              !isOutsideVisibleRange && !isSelected && !isDisabled,
+            'opacity-50': isOutsideVisibleRange || isDisabled,
           },
         )}
       >
