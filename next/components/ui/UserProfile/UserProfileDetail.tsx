9d7932652e0aebd02f0ee3d8b52a5f7398317138
matejpeluha <matej.peluha@gmail.com>
2023-01-30 16:14:28 +0100

    refactor: user profile details button move to independent component

--- next/components/ui/UserProfile/UserProfileDetail.tsx
+++ next/components/ui/UserProfile/UserProfileDetail.tsx
@@ -1,6 +1,5 @@
-import EditIcon from '@assets/images/forms/edit_icon.svg'
-import WhiteEditIcon from '@assets/images/forms/edit_white.svg'
 import UserProfileDetailEdit from '@bratislava/ui-bratislava/UserProfile/UserProfileDetailEdit'
+import UserProfileDetailsButtons from '@bratislava/ui-bratislava/UserProfile/UserProfileDetailsButtons'
 import UserProfileDetailView from '@bratislava/ui-bratislava/UserProfile/UserProfileDetailView'
 import UserProfilePhoto from '@bratislava/ui-bratislava/UserProfile/UserProfilePhoto'
 import UserProfileSection from '@bratislava/ui-bratislava/UserProfile/UserProfileSection'
@@ -8,52 +7,6 @@ import UserProfileSectionHeader from '@bratislava/ui-bratislava/UserProfile/User
 import { UserData } from '@utils/useAccount'
 import cx from 'classnames'
 import { useTranslation } from 'next-i18next'
-
-import Button from '../../forms/simple-components/Button'
-
-interface ButtonSectionProps {
-  isEditing?: boolean
-  onChangeIsEditing: (isEditing: boolean) => void
-}
-
-const ButtonSection = ({ isEditing, onChangeIsEditing }: ButtonSectionProps) => {
-  const { t } = useTranslation('account')
-  return (
-    <div className="width-fit">
-      {isEditing ? (
-        <div className="flex flex-row gap-5 items-center">
-          <Button
-            variant="plain-black"
-            size="sm"
-            text={t('profile_detail.stop_edit_button')}
-            onPress={() => onChangeIsEditing(false)}
-          />
-          <Button
-            variant="black"
-            size="sm"
-            text={t('profile_detail.save_edit_button')}
-            onPress={() => onChangeIsEditing(false)}
-          />
-        </div>
-      ) : (
-        <div className="width-fit">
-          <Button
-            variant="black"
-            startIcon={<WhiteEditIcon />}
-            size="sm"
-            text={t('profile_detail.start_edit_button')}
-            className="hidden xs:block"
-            onPress={() => onChangeIsEditing(true)}
-          />
-          <EditIcon
-            className="block xs:hidden cursor-pointer"
-            onClick={() => onChangeIsEditing(true)}
-          />
-        </div>
-      )}
-    </div>
-  )
-}
 
 interface UserProfileDetailProps {
   userData?: UserData | null
@@ -73,7 +26,7 @@ const UserProfileDetails = (props: UserProfileDetailProps) => {
         text={t('profile_detail.text')}
         underline
       >
-        <ButtonSection isEditing={isEditing} onChangeIsEditing={onChangeIsEditing} />
+        <UserProfileDetailsButtons isEditing={isEditing} onChangeIsEditing={onChangeIsEditing} />
       </UserProfileSectionHeader>
       <div className={cx('flex p-4 flex-col gap-8', 'xs:p-8 xs:flex-row xs:gap-16 xs:flex-wrap')}>
         <UserProfilePhoto userData={userData ?? {}} />
---
+++ next/components/ui/UserProfile/UserProfileDetailsButtons.tsx
@@ -0,0 +1,54 @@
+import EditIcon from '@assets/images/forms/edit_icon.svg'
+import WhiteEditIcon from '@assets/images/forms/edit_white.svg'
+import { useTranslation } from 'next-i18next'
+
+import Button from '../../forms/simple-components/Button'
+
+interface UserProfileDetailsButtonsProps {
+  isEditing?: boolean
+  onChangeIsEditing: (isEditing: boolean) => void
+}
+
+const UserProfileDetailsButtons = ({
+  isEditing,
+  onChangeIsEditing,
+}: UserProfileDetailsButtonsProps) => {
+  const { t } = useTranslation('account')
+  return (
+    <div className="width-fit">
+      {isEditing ? (
+        <div className="flex flex-row gap-5 items-center">
+          <Button
+            variant="plain-black"
+            size="sm"
+            text={t('profile_detail.stop_edit_button')}
+            onPress={() => onChangeIsEditing(false)}
+          />
+          <Button
+            variant="black"
+            size="sm"
+            text={t('profile_detail.save_edit_button')}
+            onPress={() => onChangeIsEditing(false)}
+          />
+        </div>
+      ) : (
+        <div className="width-fit">
+          <Button
+            variant="black"
+            startIcon={<WhiteEditIcon />}
+            size="sm"
+            text={t('profile_detail.start_edit_button')}
+            className="hidden xs:block"
+            onPress={() => onChangeIsEditing(true)}
+          />
+          <EditIcon
+            className="block xs:hidden cursor-pointer"
+            onClick={() => onChangeIsEditing(true)}
+          />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default UserProfileDetailsButtons
