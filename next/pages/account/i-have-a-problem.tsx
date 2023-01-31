90f5961e3179899454960c376984c8396b4810a9
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2023-01-31 15:10:21 +0100

    FORM-270: Page structure - mestske konto (#530)
    * FORM-270
    
    Create account page, create AccountPageLayout, modify AccountNavBar with navigation, create new folder in assets with icons for account page
    
    * FORM-270
    
    delete activeSection state, edit all conditions by url, add all sections like component and swith there by url,  add en language to AccountLayout
    
    * FORM-270
    
    Delete dynamic page /account/[menu], add separate pages to /account, comment next.config rewrites section (/account)
    
    * FORM-270
    
    Delete AccountTypes.ts, change comment in next.config, delete AccountHeaderSection in AccountPageLayout and add it to each subpages, in AccountNavBar add Link instead of li in navigation,  implement react-hook-form/react-hook-form#8622 in .eslintrc.js
    
    * FORM-270
    
    delete update from .eslintrs.js, need to be done in separate PR
    
    * FORM-270
    
    Change navigation list div -> ul, li, a tags(semantically correct), delete second useTranslation and use it just once

---
+++ next/pages/account/i-have-a-problem.tsx
@@ -0,0 +1,40 @@
+import { AsyncServerProps } from '@utils/types'
+import { isProductionDeployment } from '@utils/utils'
+import HelpSection from 'components/forms/segments/AccountSections/HelpSection/HelpSection'
+import AccountPageLayout from 'components/layouts/AccountPageLayout'
+import PageWrapper from 'components/layouts/PageWrapper'
+import { GetServerSidePropsContext } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  if (isProductionDeployment()) return { notFound: true }
+
+  const locale = ctx.locale ?? 'sk'
+
+  return {
+    props: {
+      page: {
+        locale: ctx.locale,
+        localizations: ['sk', 'en']
+          .filter((l) => l !== ctx.locale)
+          .map((l) => ({
+            slug: '',
+            locale: l,
+          })),
+      },
+      ...(await serverSideTranslations(locale)),
+    },
+  }
+}
+
+const AccountHelpPage = ({ page }: AsyncServerProps<typeof getServerSideProps>) => {
+  return (
+    <PageWrapper locale={page.locale} localizations={page.localizations}>
+      <AccountPageLayout>
+        <HelpSection />
+      </AccountPageLayout>
+    </PageWrapper>
+  )
+}
+
+export default AccountHelpPage
