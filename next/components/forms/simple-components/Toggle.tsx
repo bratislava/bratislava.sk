42ee5430dcb608c560a65e4bca2913702dcb9d11
Matej Peluha <45636442+matejpeluha@users.noreply.github.com>
2023-02-01 23:00:56 +0100

    Form 257/my profile (#547)
    * feat: page component for user profile
    
    * feat: basic page for profile
    
    * feat: consents
    
    * chore: prepare onChange
    
    * style: small css changes
    
    * feat: user profile password
    
    * refactor: divide code into subclasses and create component for sections
    
    * feat: use UserProfileSection with header in consents
    
    * feat: button in password section
    
    * feat: load logged user
    
    * feat: redirect from change password
    
    * fix: bug in node modules and importing after merge
    
    * fix: bug in node modules and importing after merge
    
    * fix: height of parent container and gaps in mobile version
    
    * refactor: change state of toggles in user-profile and try to use it but not working
    
    * fix: toggle button
    
    * feat: preapre edit progile detail section
    
    * feat: change route
    
    * feat: buttons to edit profile
    
    * feat: mobile version of buttons in edit profile
    
    * fix: behaviour of buttons
    
    * fix: onPress event
    
    * feat: prototype for profile photo
    
    * chore: trye to test if user is logged before rendering of page
    
    * feat: read initial letters for photo
    
    * refactor: files folders
    
    * feat: user profile detail view
    
    * feat: create user profile detail edit
    
    * feat: add some other fields for editing
    
    * feat: temporaryUserData for editing mode which is not confirmed
    
    * refactor: user profile details button move to independent component
    
    * feat: stop editing of details
    
    * feat: update profile attributes
    
    * feat: edit address (broken updating, fix later)
    
    * feat: mobile version of edit
    
    * feat: submit
    
    * feat: alerts
    
    * fix: address update
    
    * feat: modal for email edit
    
    * fix: change password url
    
    * fix: missing palceholder in input
    
    * chore: rename general layout

--- next/components/forms/simple-components/Toggle.tsx
+++ next/components/forms/simple-components/Toggle.tsx
@@ -34,6 +34,7 @@ const Toggle = ({ children, isDisabled = false, ...rest }: ToggleBase) => {
   const { isSelected } = state
   const toggleContainer = cx('group select-none flex flex-row items-center p-0 gap-4', {
     'opacity-50 cursor-not-allowed': isDisabled,
+    'cursor-pointer': !isDisabled,
   })
   const labelStyle = cx('text-20 select-none leading-8 text-gray-700')
 
