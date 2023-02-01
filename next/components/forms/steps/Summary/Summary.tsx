4ac9fcab15a4ded01f618a039a49953405518ad8
matejpeluha <matej.peluha@gmail.com>
2023-02-01 20:59:22 +0100

    feat: parse data for summary

--- next/components/forms/steps/Summary/Summary.tsx
+++ next/components/forms/steps/Summary/Summary.tsx
@@ -1,6 +1,6 @@
 import { ErrorSchema, RJSFValidationError, StrictRJSFSchema } from '@rjsf/utils'
-import { getLabel, JsonSchema } from '@utils/forms'
-import { JSONSchema7Definition } from 'json-schema'
+import { getAllPossibleJsonSchemaProperties, getLabel, JsonSchema } from '@utils/forms'
+import { JSONSchema7, JSONSchema7Definition } from 'json-schema'
 
 import SummaryStep from './SummaryStep'
 import { TransformedFormData, TransformedFormStep } from './TransformedFormData'
@@ -49,15 +49,63 @@ const Summary = ({ schema, formData, formErrors, extraErrors, onGoToStep }: Summ
     })
   }
 
-  const transformStep = ([key, step]: [string, JSONSchema7Definition]): TransformedFormStep => {
-    const label = schema?.allOf ? getLabel(schema.allOf, key) : key
-    const data: TransformedFormData[] = []
-    const stepExtraErrors = extraErrors[key]
-    getAllTransformedData(data, `.${key}`, stepExtraErrors, step)
-    return { key, label, data }
+  const getAllSchemaData = (
+    data: TransformedFormData[],
+    schemaContent: JsonSchema,
+    schemaPath: string,
+    currentFormData?: any,
+    currentExtraErrors?: ErrorSchema,
+  ) => {
+    const properties = getAllPossibleJsonSchemaProperties(schemaContent)
+
+    Object.entries(properties).forEach(([key, value]: [string, JsonSchema]) => {
+      const newSchemaPath = `${schemaPath}.${key}`
+      const childExtraErrors = currentExtraErrors ? currentExtraErrors[key] : undefined
+      const childFormData =
+        currentFormData && typeof currentFormData !== 'boolean' ? currentFormData[key] : undefined
+
+      if (value && typeof value !== 'boolean' && (!value.type || value.type === 'object')) {
+        getAllSchemaData(data, value, newSchemaPath, childFormData, childExtraErrors)
+      } else if (value && typeof value !== 'boolean') {
+        const extraErrorCount = childExtraErrors?.__errors?.length ?? 0
+        const field: TransformedFormData = {
+          label: value.title ?? key,
+          value:
+            (childFormData && !Array.isArray(childFormData)) ||
+            (Array.isArray(childFormData) && childFormData.length > 0)
+              ? JSON.stringify(childFormData, null, '\t').replaceAll('"', '')
+              : '-',
+          schemaPath,
+          isError: extraErrorCount > 0 || isFieldError(schemaPath, key),
+        }
+        data.push(field)
+      }
+    })
   }
 
-  const transformedSteps: TransformedFormStep[] = Object.entries(formData).map(transformStep)
+  const transformStep = (step: JSONSchema7Definition): TransformedFormStep => {
+    if (typeof step === 'boolean' || !step?.properties) return { key: '', label: '', data: [] }
+    const stepContent: JSONSchema7Definition = Object.values(step.properties)[0]
+    const stepKey: string = Object.keys(step.properties)[0]
+    const label: string =
+      typeof stepContent !== 'boolean' && stepContent.title
+        ? stepContent.title
+        : typeof step !== 'boolean' && step.properties
+        ? Object.keys(step.properties)[0]
+        : ''
+    const data: TransformedFormData[] = []
+    const stepExtraErrors = extraErrors[stepKey]
+    // getAllTransformedData(data, `.${key}`, stepExtraErrors, step)
+    getAllSchemaData(data, stepContent, `.${stepKey}`, formData[stepKey], stepExtraErrors)
+    return { key: stepKey, label, data }
+  }
+
+  console.log('SCHEMA', schema)
+  console.log('Errors', formErrors)
+  console.log('extra errors', extraErrors)
+  const transformedSteps: TransformedFormStep[] = schema?.allOf
+    ? schema.allOf.map(transformStep)
+    : []
 
   return (
     <div className="my-10">
