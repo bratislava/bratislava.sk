717fce5897e7311f0e6654b66ce041e4899180da
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2023-02-02 10:10:19 +0100

    FORM
    delte eslint ignore comment and delete fragment

--- next/components/forms/info-components/FieldErrorMessage.tsx
+++ next/components/forms/info-components/FieldErrorMessage.tsx
@@ -9,23 +9,16 @@ const FieldErrorMessage: FC<FieldErrorMessageProps> = ({
   errorMessage = [],
   errorMessageProps,
 }) => {
-  return (
-    // eslint-disable-next-line react/jsx-no-useless-fragment
-    <>
-      {errorMessage.length > 0 && (
-        <div
-          className="text-p3 sm:text-16 leading-5 sm:leading-6 mt-1 text-error"
-          {...errorMessageProps}
-        >
-          {errorMessage?.map((error, i) => (
-            <div key={i}>{`${error.slice(0, 1).toUpperCase()}${error
-              .slice(1)
-              .toLowerCase()}.`}</div>
-          ))}
-        </div>
-      )}
-    </>
-  )
+  return errorMessage.length > 0 ? (
+    <div
+      className="text-p3 sm:text-16 leading-5 sm:leading-6 mt-1 text-error"
+      {...errorMessageProps}
+    >
+      {errorMessage?.map((error, i) => (
+        <div key={i}>{`${error.slice(0, 1).toUpperCase()}${error.slice(1).toLowerCase()}.`}</div>
+      ))}
+    </div>
+  ) : null
 }
 
 export default FieldErrorMessage
