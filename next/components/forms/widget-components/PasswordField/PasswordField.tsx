614a9e2a29af9ad0f49276e7937c3832785e876d
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2023-02-06 15:24:32 +0100

    FORM-280
    fix hidden.svg icon in PasswordField

--- next/components/forms/widget-components/PasswordField/PasswordField.tsx
+++ next/components/forms/widget-components/PasswordField/PasswordField.tsx
@@ -57,7 +57,7 @@ const PasswordField = forwardRef<HTMLInputElement, Props>(
           <button
             type="button"
             tabIndex={0}
-            className="absolute inset-y-1/2 right-5 h-5 w-5 -translate-y-2/4 cursor-pointer"
+            className="flex items-center justify-center absolute inset-y-1/2 right-3 sm:right-4 h-6 w-6 -translate-y-2/4 cursor-pointer"
             onPointerUp={() => setType('password')}
             onPointerDown={() => setType('text')}
           >
