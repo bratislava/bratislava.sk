9bd149cae97c41d7f40e128d1bac060bdb3ecc56
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2023-02-14 17:45:31 +0100

    FORM-275: Update Tooltip (#571)
    * FORM-275
    
    Update Tooltip component and help icon in all components
    
    * FORM-275
    
    Add toggle click event on help icon
    
    * FORM-275
    
    create new Tooltip component with useHover hook, refactor and delete separate HelpIcon, now it use together
    
    * FORM-275
    
    Refactor position logic, add 1 prop instead of 6
    
    * FORM-275
    
    Delete useless props in Tooltip component, Delete TooltipComponent form /simple-components and change TooltipComponent -> Tooltip in Accordion component, delete active style onClick in Tooltip icon
    
    * FORM-275
    
    fix styles

--- next/components/forms/info-components/FieldHeader.tsx
+++ next/components/forms/info-components/FieldHeader.tsx
@@ -1,9 +1,8 @@
-import HelpIcon from '@assets/images/forms/icon-help.svg'
 import cx from 'classnames'
-import React, { DOMAttributes, FC, useState } from 'react'
+import Tooltip from 'components/forms/info-components/Tooltip/Tooltip'
+import React, { DOMAttributes } from 'react'
 
 import { ExplicitOptionalType } from '../types/ExplicitOptional'
-import Tooltip from './Tooltip'
 
 interface FieldHeaderProps {
   label: string
@@ -27,7 +26,6 @@ const FieldHeader = (props: FieldHeaderProps) => {
     descriptionProps,
     tooltip,
   } = props
-  const [isTooltipOpened, setIsTooltipOpened] = useState<boolean>(false)
 
   // STYLES
   const labelStyle = cx('text-p3-semibold sm:text-16-semibold relative text-gray-800', {
@@ -56,7 +54,7 @@ const FieldHeader = (props: FieldHeaderProps) => {
           }
           <div className="flex-column flex items-center">
             {
-              /* TOOLTIP ICON */
+              /* TOOLTIP */
               tooltip && (
                 <div
                   className={cx('flex-column flex items-center', {
@@ -64,22 +62,7 @@ const FieldHeader = (props: FieldHeaderProps) => {
                     'ml-2': !required,
                   })}
                 >
-                  <HelpIcon
-                    className="cursor-pointer w-5 h-5 sm:w-6 sm:h-6"
-                    onMouseOver={() => setIsTooltipOpened(true)}
-                    onMouseLeave={() => setIsTooltipOpened(false)}
-                  />
-                  <div className="relative">
-                    <Tooltip
-                      text={tooltip}
-                      visible={isTooltipOpened}
-                      arrow="bottom"
-                      alignArrow="right"
-                      bottom={15}
-                      right={-13}
-                      absolute
-                    />
-                  </div>
+                  <Tooltip text={tooltip} />
                 </div>
               )
             }
