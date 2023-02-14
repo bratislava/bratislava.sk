095c4604017518f58abbb5278e7334549d0e93f4
Kristian Duda <kristianduda83@gmail.com>
2023-02-14 16:47:49 +0100

    implement mobile design

--- next/components/forms/segments/AccountContainer/AccountContainer.tsx
+++ next/components/forms/segments/AccountContainer/AccountContainer.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 
 export const AccountContainer = ({ children }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className="md:py-8 md:px-10 mx-auto">
-    <div className="md:rounded-lg bg-white py-6 px-4 md:shadow md:px-12 md:py-10 md:w-[696px]">
-      {children}
-    </div>
+  <div className="md:py-8 md:px-10 mx-auto w-full max-w-[696px]">
+    <div className="md:rounded-lg bg-white py-6 px-4 md:shadow md:px-12 md:py-10">{children}</div>
   </div>
 )
 
