34d1aa220210d8bacbc9f5f8bc7aed128b4b411c
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2023-02-14 23:08:23 +0100

    Fixed Accordion component style (added 4th size and made  mobile design). Created Tax Details visual page

--- next/components/forms/simple-components/Accordion.tsx
+++ next/components/forms/simple-components/Accordion.tsx
@@ -1,5 +1,4 @@
 import cx from 'classnames'
-import TooltipPopup from 'components/forms/info-components/Tooltip/TooltipPopup'
 import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
@@ -9,25 +8,28 @@ import remarkGfm from 'remark-gfm'
 
 import ExpandMoreIcon from '../icon-components/ExpandMoreIcon'
 import PersonIcon from '../icon-components/PersonIcon'
+import TooltipComponent from '../info-components/Tooltip/Tooltip'
 
-const Tooltip = ({ children }: never) => {
-  return children ? <TooltipPopup position="top-right" text={children} /> : null
-}
-export type AccordionSizeType = 'sm' | 'md' | 'lg'
+const Tooltip = ({ children }: never) =>
+  children ? <TooltipComponent position="top-right" text={children} /> : null
+
+export type AccordionSizeType = 'xs' | 'sm' | 'md' | 'lg'
 
 export type AccordionBase = {
   size: AccordionSizeType
   title: string
+  secondTitle?: string
   content: string
   icon?: boolean
   shadow?: boolean
   className?: string
 }
 export const isAccordionSizeType = (size: string) =>
-  ['sm', 'md', 'lg'].includes(size) ? size : 'sm'
+  ['xs', 'sm', 'md', 'lg'].includes(size) ? size : 'sm'
 
 const Accordion = ({
   title,
+  secondTitle,
   content,
   size = 'sm',
   icon = false,
@@ -36,11 +38,13 @@ const Accordion = ({
 }: AccordionBase) => {
   const [isActive, setIsActive] = useState(false)
 
-  size = isAccordionSizeType(size) as AccordionSizeType
+  const accordionSize = isAccordionSizeType(size) as AccordionSizeType
+
   const accordionContainerStyle = cx('flex flex-col gap-4 w-full rounded-xl bg-gray-0', className, {
-    'p-5': size === 'sm',
-    'py-6 px-8': size === 'md',
-    'py-8 px-10': size === 'lg',
+    'px-4 py-3 lg:p-4': accordionSize === 'xs',
+    'p-4 lg:p-5': accordionSize === 'sm',
+    'p-4 lg:py-6 lg:px-8': accordionSize === 'md',
+    'py-5 px-6 lg:py-8 lg:px-10': accordionSize === 'lg',
     'border-gray-200': !isActive && !shadow,
     'border-gray-700': isActive && !shadow,
     'border-2 border-solid hover:border-gray-500': !shadow,
@@ -55,54 +59,66 @@ const Accordion = ({
         {icon && (
           <div
             className={cx('flex items-center justify-center', {
-              'w-6 h-6': size === 'sm',
-              'w-8 h-8': size === 'md',
-              'w-10 h-10': size === 'lg',
+              'w-6 h-6': accordionSize === 'sm' || accordionSize === 'xs',
+              'w-8 h-8': accordionSize === 'md',
+              'w-10 h-10': accordionSize === 'lg',
             })}
           >
             <PersonIcon
               className={cx('', {
-                'w-4 h-4': size === 'sm',
-                'w-5 h-5': size === 'md',
-                'w-6 h-6': size === 'lg',
+                'w-4 h-4': accordionSize === 'sm' || accordionSize === 'xs',
+                'w-5 h-5': accordionSize === 'md',
+                'w-6 h-6': accordionSize === 'lg',
               })}
             />
           </div>
         )}
-        <div className="flex w-full flex-col">
-          <div
-            className="flex w-full items-center cursor-pointer"
+        <div className="flex w-full flex-col gap-2 lg:gap-4">
+          <button
+            type="button"
+            className="flex cursor-pointer items-center gap-4"
             onClick={() => setIsActive(!isActive)}
           >
             <div
-              className={cx('font-semibold w-full', {
-                'text-h5': size === 'sm',
-                'text-h4': size === 'md',
-                'text-h3': size === 'lg',
+              className={cx('flex grow', {
+                'text-h6': accordionSize === 'xs',
+                'text-h5': accordionSize === 'sm',
+                'text-h4': accordionSize === 'md',
+                'text-h3': accordionSize === 'lg',
               })}
             >
               {title}
             </div>
             <div
+              className={cx('lg:font-semibold', {
+                'text-p-base': size === 'xs',
+                'text-h-base': size === 'sm',
+                'lg:text-h-md text-p-base': size === 'md',
+                'text-h-lg': size === 'lg',
+              })}
+            >
+              {secondTitle}
+            </div>
+            <div
               className={cx('flex items-center justify-center', {
-                'w-10 h-10': size === 'lg',
-                'w-8 h-8': size === 'md',
-                'w-6 h-6': size === 'sm',
+                'w-10 h-10': accordionSize === 'lg',
+                'w-8 h-8': accordionSize === 'md',
+                'w-6 h-6': accordionSize === 'sm' || accordionSize === 'xs',
               })}
             >
               <ExpandMoreIcon
                 className={cx('', {
                   'transform rotate-180': isActive,
                 })}
-                size={size}
+                size={accordionSize}
               />
             </div>
-          </div>
+          </button>
           {isActive && (
             <div
               className={cx('flex flex-col font-normal', {
-                'text-h6': size === 'sm',
-                'text-20': size === 'lg' || size === 'md',
+                'text-h6': accordionSize === 'sm' || accordionSize === 'xs',
+                'text-20': accordionSize === 'lg' || accordionSize === 'md',
               })}
             >
               <ReactMarkdown
