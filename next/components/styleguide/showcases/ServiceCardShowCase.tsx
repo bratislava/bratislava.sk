110807e092acaaf3cc0792e28a5c501b2a03fc47
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2023-02-17 14:34:00 +0100

    FORM-230
    Add service card like Link or button with href prop

--- next/components/styleguide/showcases/ServiceCardShowCase.tsx
+++ next/components/styleguide/showcases/ServiceCardShowCase.tsx
@@ -18,24 +18,28 @@ const ServiceCardShowCase = () => {
             description="Záväzné stanovisko slúži ako podklad pre konanie vedené na príslušnom stavebnom úrade."
             icon={<CompassServiceIcon />}
             buttonText="Prejsť na žiadosť"
+            href="#"
           />
           <ServiceCard
             title="Dotácia na kontajnerové stanovištia"
             description="Žiadosť o dotáciu na kontajnerové stanovište alebo o nájom mestského pozemku."
             icon={<BasketServiceIcon />}
             buttonText="Prejsť na žiadosť"
+            onPress={() => alert('Hello')}
           />
           <ServiceCard
             title="Digitálna platba dane z nehnuteľností"
             description="Digitálna platba dane z nehnuteľnosti, pohodlne a online."
             icon={<MoneyServiceIcon />}
             buttonText="Zaplatiť daň digitálne"
+            href="#"
           />
           <ServiceCard
             title="Online lístky na kúpaliská"
             description="Kúpa online lístku alebo permanentky na všetky mestské kúpalíská v Bratislave."
             icon={<PoolServiceIcon />}
             buttonText="Kúpiť lístok"
+            onPress={() => alert('Hello')}
           />
         </div>
       </Stack>
