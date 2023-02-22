1ce3b9acebedfc72d54cee63bde288d2ea308633
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2023-02-22 20:58:23 +0100

    fix footer text

--- next/components/forms/segments/AccountSections/TaxesFeesSection/TaxFooter.tsx
+++ next/components/forms/segments/AccountSections/TaxesFeesSection/TaxFooter.tsx
@@ -1,9 +1,14 @@
 const TaxFooter = () => {
   return (
-    <div>
-      Radi by ste sa v niečom uistili alebo máte otázky? Pozrite si odpovede na tie, ktoré sú často
-      kladené. Ak nenájdete odpoveď na Vašu otázku, neváhajte nás kontaktovať na adrese:
-      platbadane@bratislava.sk
+    <div className="text-20-medium">
+      Radi by ste sa v niečom uistili alebo máte otázky? Pozrite si odpovede na tie, ktoré sú{' '}
+      <a href="#" className="underline">
+        často kladené.
+      </a>{' '}
+      Ak nenájdete odpoveď na Vašu otázku, neváhajte nás kontaktovať na adrese:{' '}
+      <a href="mailto:platbadane@bratislava.sk" className="underline-offset-2 underline">
+        platbadane@bratislava.sk
+      </a>
     </div>
   )
 }
