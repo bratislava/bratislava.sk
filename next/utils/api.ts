d536817c0b12c4113bfee799f84f64b59445f1d4
Kristian Duda <kristianduda83@gmail.com>
2023-02-24 09:24:48 +0100

    update api dto

--- next/utils/api.ts
+++ next/utils/api.ts
@@ -36,8 +36,8 @@ const fetchJsonApi = async (path: string, options?: RequestInit) => {
     console.log('have json', responseJson)
     if (responseJson?.errors) {
       throw new ApiError(responseJson?.message || API_ERROR_TEXT, responseJson.errors)
-    } else if (responseJson.state === 'CustomError') {
-      throw new Error(responseJson.naming)
+    } else if (responseJson.status === 'CustomError') {
+      throw new Error(responseJson.errorName)
     } else {
       throw new Error(API_ERROR_TEXT)
     }
