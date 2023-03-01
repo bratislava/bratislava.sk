d026c96464c17353981c4cb0f598326c5f7c4cd8
Adam Grund <adam.grund@gmail.com>
2023-03-01 17:42:10 +0100

    #642 partly refactor HomepagePageLayout

--- next/components/layouts/HomepagePageLayout.tsx
+++ next/components/layouts/HomepagePageLayout.tsx
@@ -1,4 +1,6 @@
+import { HomepageHeaderFragment } from '@bratislava/strapi-sdk-homepage'
 import {
+  BAStickyMenu,
   Bookmarks,
   BookmarksProps,
   FooterProps,
@@ -6,18 +8,22 @@ import {
   SectionContainer,
 } from '@bratislava/ui-bratislava'
 import cx from 'classnames'
+import { STICKY_MENU_STATE, useWithoutMenuSection } from 'components/sections/useWithoutMenuSection'
+import { WithoutMenuSection } from 'components/sections/WithoutMenuSection'
 import React from 'react'
 
 import Footer from '../molecules/Footer'
 import NavBar from '../molecules/NavBar'
 
 interface HomepagePageLayoutProps {
+  header?: HomepageHeaderFragment | null | undefined
   footer?: FooterProps
   menuItems?: MenuMainItem[]
   bookmarks?: BookmarksProps['bookmarks']
 }
 
 const HomepagePageLayout = ({
+  header,
   className,
   children,
   footer,
@@ -26,6 +32,7 @@ const HomepagePageLayout = ({
 }: React.HTMLAttributes<HTMLDivElement> & HomepagePageLayoutProps) => {
   // const isEN = true // TODO: use localization // TODO get bookmarks determined by localization
   const [searchOpen, setSearchOpen] = React.useState(false)
+  const { elementRef, menuState } = useWithoutMenuSection()
 
   return (
     <div className={cx('font-inter', className)}>
@@ -36,7 +43,28 @@ const HomepagePageLayout = ({
       </div>
       <Bookmarks bookmarks={bookmarks} className="top-56" />
       <div className={cx({ 'hidden lg:block': searchOpen })}>
-        <div className="-mt-18">{children}</div>
+        <div className="bg-white">
+          <WithoutMenuSection
+            mainMenuItems={menuItems}
+            homepageHeader={header}
+            elementRef={elementRef}
+          />
+          <div
+            className={cx(
+              'mx-auto w-full bg-white fixed z-40 drop-shadow-sm shadow-lg left-0 hidden lg:block',
+              {
+                'top-14 transition-all duration-300': menuState === STICKY_MENU_STATE.VISIBLE,
+                '-top-14 transition-all duration-300': menuState === STICKY_MENU_STATE.HIDDEN,
+              },
+            )}
+          >
+            <BAStickyMenu
+              menuItems={menuItems}
+              isVisible={menuState === STICKY_MENU_STATE.VISIBLE}
+            />
+          </div>
+        </div>
+        {children}
 
         {footer && <Footer {...footer} />}
       </div>
