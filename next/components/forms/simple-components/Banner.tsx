a697193cc3720a0e3037013679ce5787ba20363e
AntonZezul <53222637+AntonZezul@users.noreply.github.com>
2023-03-03 13:40:48 +0100

    FORM: Update Banner component on account page (#659)
    * FORM
    
    add to banner AccountMarkdown content, buttonText prop, add text from translation files, change IntroSection Banner
    
    * FORM
    
    update Banner component on Help Section , and change styles, add mobile prop, add to Button component fullWidth prop

--- next/components/forms/simple-components/Banner.tsx
+++ next/components/forms/simple-components/Banner.tsx
@@ -1,17 +1,28 @@
 import BannerPhone from '@assets/images/banner-phone.png'
+import CallIcon from '@assets/images/forms/call.svg'
 import cx from 'classnames'
+import AccountMarkdown from 'components/forms/segments/AccountMarkdown/AccountMarkdown'
 import Image from 'next/image'
 
 import Button from './Button'
 
 type BannerBase = {
   title: string
-  content: string
+  buttonText?: string
+  mobileNumber?: string
+  content?: string
   onPress?: () => void
   className?: string
 }
 
-const Banner = ({ title, content, onPress, className }: BannerBase) => {
+const Banner = ({
+  title,
+  content,
+  buttonText = 'Button',
+  mobileNumber = '',
+  onPress,
+  className,
+}: BannerBase) => {
   return (
     <div
       className={cx(
@@ -19,19 +30,33 @@ const Banner = ({ title, content, onPress, className }: BannerBase) => {
         className,
       )}
     >
-      <div className="lg:w-1/2 w-full max-w-[488px] text-white h-full flex flex-col justify-center rounded-l-3xl gap-6 px-4 md:px-0 mb-6 lg:mb-0">
+      <div className="lg:w-1/2 w-full max-w-[488px] text-white h-full flex flex-col justify-center rounded-l-3xl gap-6 lg:gap-3 px-4 md:px-0 mb-6 lg:mb-0">
         <div className="flex flex-col items-start gap-3">
           <h2 className="text-h1 lg:text-h2">{title}</h2>
-          <div className="text-p2 text-gray-200 flex items-center font-normal">{content}</div>
+          <AccountMarkdown content={content} variant="sm" className="text-p2 text-gray-200" />
         </div>
-        <Button className="hidden lg:flex" variant="category" text="Button" onPress={onPress} />
-        <Button
-          className="flex lg:hidden"
-          size="sm"
-          variant="category"
-          text="Button"
-          onPress={onPress}
-        />
+        <div className="flex flex-col lg:flex-row items-center gap-4">
+          <Button
+            className="hidden lg:flex w-full"
+            variant="category"
+            text={buttonText}
+            onPress={onPress}
+          />
+          <Button
+            className="flex lg:hidden"
+            size="sm"
+            variant="category"
+            text={buttonText}
+            onPress={onPress}
+            fullWidth
+          />
+          {mobileNumber && (
+            <div className="text-gray-0 flex items-center gap-2 px-3 py-2">
+              <CallIcon />
+              <span className="text-p2-semibold">{mobileNumber}</span>
+            </div>
+          )}
+        </div>
       </div>
       <div className="my-auto sm:w-1/2 w-full">
         <Image src={BannerPhone} />
