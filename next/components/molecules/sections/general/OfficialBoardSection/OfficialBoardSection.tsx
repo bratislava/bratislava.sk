86879cd25d7ab8a3876c351776dc4173e6c64a72
Marek Bodinger <marek.bodinger@gmail.com>
2023-03-07 09:46:57 +0100

    OfficialBoardSection mostly copied from next/pages/mesto-bratislava/transparentne-mesto/uradna-tabula.tsx

---
+++ next/components/molecules/sections/general/OfficialBoardSection/OfficialBoardSection.tsx
@@ -0,0 +1,50 @@
+import type { ParsedOfficialBoardDocument } from '@backend/services/ginis'
+import { BasicSearchOld, DocumentCards, NoResultsFound } from '@bratislava/ui-bratislava'
+import { useTranslation } from 'next-i18next'
+import React, { useState } from 'react'
+import { useQuery } from 'react-query'
+
+const OfficialBoardSection = () => {
+  const [search, setSearch] = useState('')
+
+  const { t } = useTranslation('common')
+
+  // TODO: Refactor, prefetch and extract fetcher.
+  const { data } = useQuery({
+    queryKey: ['OfficialBoard', search],
+    queryFn: () =>
+      fetch(
+        `/api/ginis/documents-list?${new URLSearchParams({
+          search,
+        })}`,
+      ).then((r) => r.json() as Promise<ParsedOfficialBoardDocument[]>),
+    keepPreviousData: true,
+  })
+
+  return (
+    <>
+      <BasicSearchOld
+        className="pb-14 lg:pb-24"
+        placeholder={t('enterKeyword')}
+        title={t('searching')}
+        buttonText={t('search')}
+        onSubmit={(newSearch) => setSearch(newSearch)}
+        initialValue={search}
+      />
+      {data && data.length === 0 && (
+        <NoResultsFound title={t('weDidntFindAnything')} message={t('tryEnteringSomethingElse')} />
+      )}
+      {data && data.length > 0 && (
+        <DocumentCards
+          query={search}
+          title={t('recentlyAddedDocuments')}
+          viewButtonText={t('viewTheDocument')}
+          downloadButtonText={t('download')}
+          documents={data}
+        />
+      )}
+    </>
+  )
+}
+
+export default OfficialBoardSection
