970f88c5eacf596cd8536ab95fbe1f3a5f434fe4
Valerii Medvezhonkov <65829237+ValeriiMedvezhonkov@users.noreply.github.com>
2023-03-07 14:39:54 +0100

    Form-266/PaymentData_Footer_Visual (#590)
    * Added Payment tax data page, created desktop and mobile version.
    Created tax footer.
    Added translations for payment data part
    
    * copy text to clipboard
    
    * fix footer text
    
    * Added new package "html-react-parser"
    fixed desktop and mobile version of PaymentData
    
    * remove html-react-parser package, fix translations in files for TaxFooter component
    
    * fix using copy fn in clipboard component

---
+++ next/components/forms/simple-components/ClipboardCopy.tsx
@@ -0,0 +1,13 @@
+import ContentCopy from '@assets/images/content_copy.svg'
+import { useCopyToClipboard } from 'usehooks-ts'
+
+const ClipboardCopy = ({ copyText }: { copyText: string }) => {
+  const [_, copy] = useCopyToClipboard()
+  return (
+    <button type="button" onClick={() => copy(copyText)}>
+      <ContentCopy />
+    </button>
+  )
+}
+
+export default ClipboardCopy
