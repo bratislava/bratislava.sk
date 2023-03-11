7bcb5ac319130f48e8c7924a545e333e3858e633
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2023-03-11 14:43:45 +0100

    use snackbar from react-simple-snackbar package created demo how to use it without UI bug

--- next/components/styleguide/showcases/SnackbarShowCase.tsx
+++ next/components/styleguide/showcases/SnackbarShowCase.tsx
@@ -1,39 +1,115 @@
-import { useSnackbar } from '@utils/useSnackbar'
+import { useState } from 'react'
+import { useSnackbar } from 'react-simple-snackbar'
 
+import { AlertType } from '../../forms/info-components/Alert'
 import Button from '../../forms/simple-components/Button'
 import { Stack } from '../Stack'
 import { Wrapper } from '../Wrapper'
 
 const SnackbarShowCase = () => {
-  const { showSnackbar } = useSnackbar()
+  const [type, setType] = useState<AlertType>()
+
+  // This example doesn't work
+  const optionsDoesntWork = {
+    style: {
+      backgroundColor:
+        type === 'success'
+          ? 'rgb(var(--color-success-700))'
+          : type === 'info'
+          ? 'rgb(var(--color-gray-700))'
+          : type === 'warning'
+          ? 'rgb(var(--color-warning-700))'
+          : 'rgb(var(--color-negative-700))',
+    },
+  }
+
+  // This example doesn't work
+  const [openSnackbar] = useSnackbar(optionsDoesntWork)
+
+  const optionsSuccess = {
+    style: {
+      backgroundColor: 'rgb(var(--color-success-700))',
+    },
+  }
+
+  const optionsWarning = {
+    style: {
+      backgroundColor: 'rgb(var(--color-warning-700))',
+    },
+  }
+
+  const optionsError = {
+    style: {
+      backgroundColor: 'rgb(var(--color-negative-700))',
+    },
+  }
+
+  const optionsInfo = {
+    style: {
+      backgroundColor: 'rgb(var(--color-gray-700))',
+    },
+  }
+
+  const [openSnackbarSuccess] = useSnackbar(optionsSuccess)
+  const [openSnackbarWarning] = useSnackbar(optionsWarning)
+  const [openSnackbarError] = useSnackbar(optionsError)
+  const [openSnackbarInfo] = useSnackbar(optionsInfo)
   return (
     <Wrapper direction="column" title="Snackbar">
       <Stack>
         <Button
-          text="Success Top-Left"
-          onPress={() => showSnackbar('Success Top-Left', 'top-left', 'success', true, 10_000)}
+          text="Success not working properly"
+          onPress={() => {
+            setType('success')
+            openSnackbar('Success')
+          }}
         />
         <Button
-          text="Warning Top-Center"
-          onPress={() => showSnackbar('Warning Top-Center', 'top-center', 'warning')}
+          text="Error not working properly"
+          onPress={() => {
+            setType('error')
+            openSnackbar('Error')
+          }}
         />
         <Button
-          text="Info Top-Right"
-          onPress={() => showSnackbar('Info Top-Right', 'top-right', 'info')}
+          text="Info not working properly"
+          onPress={() => {
+            setType('info')
+            openSnackbar('Info')
+          }}
+        />
+        <Button
+          text="Warning not working properly"
+          onPress={() => {
+            setType('warning')
+            openSnackbar('Warning')
+          }}
         />
       </Stack>
       <Stack>
         <Button
-          text="Error Bottom-Left"
-          onPress={() => showSnackbar('Error Bottom-Left', 'bottom-left', 'error')}
+          text="Success"
+          onPress={() => {
+            openSnackbarSuccess('Success')
+          }}
         />
         <Button
-          text="Info Bottom-Center"
-          onPress={() => showSnackbar('Info Bottom-Center', 'bottom-center', 'info')}
+          text="Error"
+          onPress={() => {
+            openSnackbarError('Error')
+          }}
         />
         <Button
-          text="Success Bottom-Right"
-          onPress={() => showSnackbar('Success Bottom-Right', 'bottom-right', 'success')}
+          text="Info"
+          onPress={() => {
+            openSnackbarInfo('Info')
+          }}
+        />
+        <Button
+          text="Warning"
+          onPress={() => {
+            openSnackbarWarning('Warning')
+          }}
         />
       </Stack>
     </Wrapper>
