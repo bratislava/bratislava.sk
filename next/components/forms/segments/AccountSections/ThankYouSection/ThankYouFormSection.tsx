abb69ab1c60a4a8bbccf3c2d062a9e4ff1b48003
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2023-03-13 13:50:06 +0100

    link to /account page, text-center, change mail from platbadane to info

--- next/components/forms/segments/AccountSections/ThankYouSection/ThankYouFormSection.tsx
+++ next/components/forms/segments/AccountSections/ThankYouSection/ThankYouFormSection.tsx
@@ -19,7 +19,7 @@ const ThankYouFormSection = () => {
           <span className="text-p2 flex">
             <AccountMarkdown
               variant="sm"
-              content={`<span className='text-p2'>V prípade akýchkoľvek otázok nás neváhajte kontaktovať na adrese:</span> <span className="text-p2-semibold underline">platbadane@bratislava.sk</span>`}
+              content={`<span className='text-p2'>V prípade akýchkoľvek otázok nás neváhajte kontaktovať na adrese:</span> <span className="text-p2-semibold underline">info@bratislava.sk</span>`}
             />
           </span>
           <div className="flex flex-col gap-3 mt-4 md:mt-6">
