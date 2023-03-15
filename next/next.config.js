7a666a426577efc7bcb724873cec316b690343f4
Marek Bodinger <marek.bodinger@gmail.com>
2023-03-15 21:21:14 +0100

    next.config.js cleanup

--- next/next.config.js
+++ next/next.config.js
@@ -2700,9 +2700,6 @@ const nextConfig = {
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
-    // used for loading eform xml template
-    config.module.rules.push({ test: /\.xml$/, loader: 'xml-loader' })
-
     return config
   },
 }
