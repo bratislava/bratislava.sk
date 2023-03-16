a707818f5b023f93891559f3a07929187a739b80
Marek Bodinger <marek.bodinger@gmail.com>
2023-03-16 12:59:13 +0100

    PageHeaderSections

---
+++ next/components/molecules/sections/pageHeader/SubpageListPageHeaderSection.tsx
@@ -0,0 +1,21 @@
+import { SubpageListPageHeaderSectionFragment } from '@bratislava/strapi-sdk-homepage'
+import { SubpageList } from '@bratislava/ui-bratislava'
+import { parsePageLink } from '@utils/page'
+import { isPresent } from '@utils/utils'
+import React from 'react'
+
+type SubpageListPageHeaderSectionProps = {
+  section: SubpageListPageHeaderSectionFragment
+}
+
+const SubpageListPageHeaderSection = ({ section }: SubpageListPageHeaderSectionProps) => {
+  return (
+    <SubpageList
+      className="mt-10"
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+      subpageList={section?.subpageList?.map(parsePageLink).filter(isPresent)}
+    />
+  )
+}
+
+export default SubpageListPageHeaderSection
