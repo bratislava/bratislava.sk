a43a97c2d9d5e5eb3adc6abed261671e825f818f
MarekBodingerBA <104828482+MarekBodingerBA@users.noreply.github.com>
2023-03-20 11:09:08 +0100

    Bundle analyzer (#719)
    Co-authored-by: Marek Bodinger <marek.bodinger@gmail.com>

--- next/next.config.js
+++ next/next.config.js
@@ -2711,15 +2711,19 @@ const sentryWebpackPluginOptions = {
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 }
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
 module.exports = withSentryConfig(
-  {
+  withBundleAnalyzer({
     ...nextConfig,
     sentry: {
       disableServerWebpackPlugin: true,
       disableClientWebpackPlugin: true,
     },
-  },
+  }),
   sentryWebpackPluginOptions,
 )
