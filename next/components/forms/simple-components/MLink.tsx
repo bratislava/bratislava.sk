d8b6113c44e3838b39d2ee21923fd67ff62c3aae
Radoslav Zeman <radoslav.zeman@bratislava.sk>
2023-03-24 14:12:33 +0100

    refactor desktop navbar header

--- next/components/forms/simple-components/MLink.tsx
+++ next/components/forms/simple-components/MLink.tsx
@@ -1,4 +1,3 @@
-import navMenu from '@bratislava/ui-bratislava/NavMenu/NavMenu'
 import cx from 'classnames'
 import Link from 'next/link'
 import { ComponentProps, forwardRef, ReactNode } from 'react'
@@ -6,7 +5,7 @@ import { twMerge } from 'tailwind-merge'
 
 export type LinkProps = Omit<ComponentProps<typeof Link>, 'as' | 'passHref'> & {
   children: ReactNode
-  variant?: 'unstyled' | 'underlineOnHover' | 'navMenu'
+  variant?: 'unstyled' | 'underlineOnHover' | 'navBarHeader'
   /**
    * Similar to this:
    * https://getbootstrap.com/docs/4.3/utilities/stretched-link/
@@ -24,7 +23,7 @@ const MLink = forwardRef<HTMLAnchorElement, LinkProps>(
     const styles = twMerge(
       cx({
         'hover:underline': variant === 'underlineOnHover',
-        'underline hover:font-semibold': variant === 'navMenu',
+        'font-semibold underline hover:text-gray-600': variant === 'navBarHeader',
 
         // https://github.com/tailwindlabs/tailwindcss/issues/1041#issuecomment-957425345
         'after:absolute after:inset-0': stretched,
