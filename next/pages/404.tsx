43070f37e48d350988345c3828d878c9701f9b45
Marek Bodinger <marek.bodinger@gmail.com>
2023-03-26 14:48:44 +0200

    Replace large images and remove redundant ones

--- next/pages/404.tsx
+++ next/pages/404.tsx
@@ -1,8 +1,6 @@
-import NoResultsFound from '@assets/images/404.png'
 import { useUIContext } from '@bratislava/common-frontend-ui-context'
 import { Button } from '@bratislava/ui-bratislava/Button/Button'
 import { GetStaticProps } from 'next'
-import Image from 'next/image'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
@@ -23,7 +21,7 @@ const NotFoundPage = () => {
   return (
     <div className="xl:pr-66 flex h-screen w-screen px-7 py-10 md:pl-36 md:pr-32 xl:pl-80">
       <div className="flex w-full flex-col items-center md:flex-row-reverse md:justify-between">
-        <Image src={NoResultsFound} alt="" width={216} className="mx-[67px]" />
+        <img src="/404_350px.png" alt="" />
         <div className="flex flex-col items-center lg:items-start">
           {/* text-5xl font-extrabold does not work */}
           <div className="pb-4 text-[48px] font-[800] lg:text-[64px]">404</div>
