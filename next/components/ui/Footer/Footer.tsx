00666307bc2867b49188a64548f8d47e7bf62076
Marek Bodinger <marek.bodinger@gmail.com>
2023-03-26 13:00:04 +0200

    Replace huge EU logo

--- next/components/ui/Footer/Footer.tsx
+++ next/components/ui/Footer/Footer.tsx
@@ -2,7 +2,6 @@
 import FBLogo from '@assets/images/FB.svg'
 import { useUIContext } from '@bratislava/common-frontend-ui-context'
 import cx from 'classnames'
-import Image from "next/legacy/image";
 import React from 'react'
 
 import BABrand from '../../../assets/images/BABrand.svg'
@@ -10,7 +9,6 @@ import EULogo from '../../../assets/images/EULogo.svg'
 import IGLogo from '../../../assets/images/IG.svg'
 import YTLogo from '../../../assets/images/YT.svg'
 import { FooterSection, FooterSectionProps } from '../FooterSection/FooterSection'
-import EULogoPng from '../images/EULogo.png'
 
 export interface FooterProps {
   className?: string
@@ -73,7 +71,7 @@ export const Footer = ({
             )}
           </nav>
           <span className="hidden xl:block">
-            <EULogo />
+            <EULogo className="w-15" />
           </span>
         </div>
       </section>
@@ -148,7 +146,7 @@ export const Footer = ({
           )}
         </nav>
         <span className="mt-5 xl:hidden">
-          <Image src={EULogoPng} />
+          <EULogo className="w-15 inline-block" />
         </span>
         <p className="mt-2 xl:mt-0">{copyright}</p>
         <div className="hidden text-right xl:block" aria-label="lang">
@@ -167,7 +165,7 @@ export const Footer = ({
         </div>
       </section>
     </footer>
-  );
+  )
 }
 
 export default Footer
