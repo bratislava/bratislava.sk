8457562bec2d6d45f700088d33b6fd6480d0cefd
Marek Bodinger <marek.bodinger@gmail.com>
2023-03-30 15:35:14 +0200

    generateImageSizes in PageHeader.tsx

--- next/components/ui/PageHeader/PageHeader.tsx
+++ next/components/ui/PageHeader/PageHeader.tsx
@@ -1,5 +1,6 @@
 import { Waves } from '@bratislava/ui-bratislava'
 import Breadcrumbs, { BreadcrumbsProps } from '@bratislava/ui-bratislava/Breadcrumbs/Breadcrumbs'
+import { generateImageSizes } from '@utils/generateImageSizes'
 import Image from 'next/image'
 import React, { PropsWithChildren } from 'react'
 import { twMerge } from 'tailwind-merge'
@@ -34,9 +35,9 @@ const PageHeader = ({
   children,
 }: PropsWithChildren<PageHeaderProps>) => {
   return (
-    <div className={twMerge('bg-category-200 relative', className)}>
+    <div className={twMerge('relative bg-category-200', className)}>
       {imageSrc && (
-        <div className="absolute top-0 right-0 w-[350px] lg:w-[750px] h-full hidden md:block">
+        <div className="absolute top-0 right-0 hidden h-full w-[350px] md:block lg:w-[750px]">
           <Image
             src={imageSrc}
             alt=""
@@ -46,29 +47,29 @@ const PageHeader = ({
               maskImage:
                 'linear-gradient(to right, rgba(0,0,0,0), rgba(0,0,0,1) 50%, rgba(0,0,0,1))',
             }}
-            sizes="100vw"
+            sizes={generateImageSizes({ lg: '750px', default: '350px' })}
             fill
-            className="object-cover w-full h-full pointer-events-none"
+            className="pointer-events-none h-full w-full object-cover"
           />
         </div>
       )}
       <div className="px-8">
-        <div className="flex flex-col max-w-screen-lg mx-auto relative">
+        <div className="relative mx-auto flex max-w-screen-lg flex-col">
           <Breadcrumbs breadcrumbs={breadcrumbs} />
-          <div className="gap-y-4 lg:gap-y-6 flex flex-col mt-3 lg:mt-6 mb-6 lg:mb-10">
+          <div className="mt-3 mb-6 flex flex-col gap-y-4 lg:mt-6 lg:mb-10 lg:gap-y-6">
             {tag && (
-              <span className="text-p2-medium inline-block rounded bg-category-700 px-3 py-1 text-white self-start">
+              <span className="text-p2-medium inline-block self-start rounded bg-category-700 px-3 py-1 text-white">
                 {tag}
               </span>
             )}
             {(title || subtext) && (
-              <div className="gap-y-1 lg:gap-y-4 flex flex-col max-w-[800px]">
+              <div className="flex max-w-[800px] flex-col gap-y-1 lg:gap-y-4">
                 {title && <h1 className="text-h1">{title}</h1>}
                 {subtext && <p>{subtext}</p>}
               </div>
             )}
             {buttons && buttons.length > 0 && (
-              <div className="gap-2 lg:gap-3 flex sm:flex-row flex-col max-w-[800px]">
+              <div className="flex max-w-[800px] flex-col gap-2 sm:flex-row lg:gap-3">
                 {buttons.map((button, index) => (
                   <Button
                     key={index}
@@ -76,7 +77,7 @@ const PageHeader = ({
                     label={button.label}
                     variant={index === 0 ? 'category' : 'category-outline'}
                     size="sm"
-                    className="sm:w-fit w-full"
+                    className="w-full sm:w-fit"
                   />
                 ))}
               </div>
