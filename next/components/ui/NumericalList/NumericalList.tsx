ec49fefdceb30e8b130b0ad398986a270ee2bc8d
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-04-03 11:17:13 +0200

    fix NumericalList props

--- next/components/ui/NumericalList/NumericalList.tsx
+++ next/components/ui/NumericalList/NumericalList.tsx
@@ -4,16 +4,17 @@ import { NumericalListItemObject } from '@bratislava/ui-bratislava'
 import { NumericalListItem } from '../NumericalListItem/NumericalListItem'
 
 export interface NumericalListProps {
-  items: NumericalListItemObject[]
-  hasBackground: boolean
+  items: NumericalListItemObject[] | null | undefined
+  hasBackground: boolean | null | undefined
   variant?: 'basic' | 'combined' | 'roadmap'
 }
 
 export const NumericalList = ({ items, hasBackground, variant }: NumericalListProps) => {
   return (
     <>
-      {items.map((item, index) => (
+      {items?.map((item, index) => (
         <NumericalListItem
+          // eslint-disable-next-line react/no-array-index-key
           key={index}
           index={index}
           item={item}
