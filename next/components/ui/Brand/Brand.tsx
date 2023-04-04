6737d0c693787905c47c45b39fa0aac1c971ec16
Marek Bodinger <marek.bodinger@gmail.com>
2023-04-04 13:11:13 +0200

    Fix Brand

--- next/components/ui/Brand/Brand.tsx
+++ next/components/ui/Brand/Brand.tsx
@@ -1,23 +1,32 @@
-import { useUIContext } from '@bratislava/common-frontend-ui-context'
-import cx from 'classnames'
-import React from 'react'
-
 import BALogo from '@assets/images/BALogo.svg'
+import MLink from '@components/forms/simple-components/MLink'
+import cx from 'classnames'
+import { Trans } from 'next-i18next'
+import React from 'react'
 
 export interface BrandProps {
   className?: string
-  title?: React.ReactNode
   url?: string
+  withTitle?: boolean
 }
 
-export const Brand = ({ className, title, url = '#' }: BrandProps) => {
-  const { Link: UILink } = useUIContext()
+export const Brand = ({ className, url = '#', withTitle }: BrandProps) => {
   return (
     <div className={cx('flex', className)} aria-label="brand">
-      <UILink className="flex items-center space-x-3" href={url}>
-        <BALogo className="lg:w-6.5 h-8 w-8 lg:h-6" />
-        {title && <div>{title}</div>}
-      </UILink>
+      <MLink className="flex items-center gap-x-3 text-[#F23005]" href={url} variant="unstyled">
+        <BALogo />
+        {withTitle && (
+          <span className="text-p2">
+            <Trans
+              i18nKey="capitalCity"
+              ns="common"
+              components={{
+                semibold: <span className="font-semibold" />,
+              }}
+            />
+          </span>
+        )}
+      </MLink>
     </div>
   )
 }
