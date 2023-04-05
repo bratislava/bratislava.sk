3b4eabe44345c2869ee4b52a387058b2e70c2c78
Marek Bodinger <marek.bodinger@gmail.com>
2023-04-05 14:45:25 +0200

    Replace footer

--- next/components/ui/Footer/MobileFooter.tsx
+++ next/components/ui/Footer/MobileFooter.tsx
@@ -24,44 +24,46 @@ const MobileFooter = () => {
   return (
     <SectionContainer>
       <HorizontalDivider />
-      <div className="mt-6 flex flex-col">
-        <div className="flex justify-center py-[9px] md:justify-start">
-          <Brand url="/" withTitle />
-        </div>
-        <div className="flex flex-col gap-6 py-6">
-          <div className="flex flex-col gap-6 ">
-            <div className="flex flex-col gap-6">
-              <FooterContacts {...attributes} />
-            </div>
+      <footer>
+        <div className="mt-6 flex flex-col">
+          <div className="flex justify-center py-[9px] md:justify-start">
+            <Brand url="/" withTitle />
           </div>
-          <HorizontalDivider />
-          {attributes.columns?.filter(isDefined).map((column, index) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <div className="flex flex-col gap-4" key={index}>
-              <p className="text-h5 text-gray-800">{column.title}</p>
-              <div className="flex flex-col gap-3">
-                <FooterColumnLinks {...column} />
+          <div className="flex flex-col gap-6 py-6">
+            <div className="flex flex-col gap-6 ">
+              <div className="flex flex-col gap-6">
+                <FooterContacts {...attributes} />
               </div>
             </div>
-          ))}
-        </div>
-      </div>
-      <HorizontalDivider />
-      <div className="flex flex-col gap-6 py-6">
-        <div className="flex flex-col items-center gap-4">
-          <div className="-my-2 flex gap-2">
-            <FooterSocialLinks {...attributes} />
+            <HorizontalDivider />
+            {attributes.columns?.filter(isDefined).map((column, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <div className="flex flex-col gap-4" key={index}>
+                <p className="text-h5 text-gray-800">{column.title}</p>
+                <div className="flex flex-col gap-3">
+                  <FooterColumnLinks {...column} />
+                </div>
+              </div>
+            ))}
           </div>
-          <FooterAccessibilityLink {...attributes} />
-          <div className="flex gap-4">
-            <FooterLanguageSwitcher />
+        </div>
+        <HorizontalDivider />
+        <div className="flex flex-col gap-6 py-6">
+          <div className="flex flex-col items-center gap-4">
+            <div className="-my-2 flex gap-2">
+              <FooterSocialLinks {...attributes} />
+            </div>
+            <FooterAccessibilityLink {...attributes} />
+            <div className="flex gap-4">
+              <FooterLanguageSwitcher />
+            </div>
+            <EULogo />
           </div>
-          <EULogo />
+          <div className="text-center text-p-base">
+            <FooterCopyright {...attributes} />
+          </div>
         </div>
-        <div className="text-center text-p-base">
-          <FooterCopyright {...attributes} />
-        </div>
-      </div>
+      </footer>
     </SectionContainer>
   )
 }
