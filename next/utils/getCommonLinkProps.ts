653d1fd1c345ece09ea8939e63ffebb715c8931f
Marek Bodinger <marek.bodinger@gmail.com>
2023-04-05 14:42:36 +0200

    getCommonLinkProps support for null, undefined

--- next/utils/getCommonLinkProps.ts
+++ next/utils/getCommonLinkProps.ts
@@ -1,22 +1,22 @@
 import { CommonLinkFragment } from '@bratislava/strapi-sdk-homepage'
 import { LinkPlausibleProps } from '@components/forms/simple-components/MLink'
 
-export const getCommonLinkProps = (link: CommonLinkFragment) => {
+export const getCommonLinkProps = (link: CommonLinkFragment | null | undefined) => {
   let href = '#'
   let target: '_blank' | undefined
 
-  if (link.page?.data?.attributes?.slug) {
-    href = `/${link.page?.data?.attributes?.slug}`
-  } else if (link.blogPost?.data?.attributes?.slug) {
-    href = `/blog/${link.blogPost?.data?.attributes?.slug}`
-  } else if (link.url) {
+  if (link?.page?.data?.attributes?.slug) {
+    href = `/${link.page.data.attributes.slug}`
+  } else if (link?.blogPost?.data?.attributes?.slug) {
+    href = `/blog/${link.blogPost.data.attributes.slug}`
+  } else if (link?.url) {
     href = link.url
     target = '_blank'
   }
 
-  const plausibleProps: LinkPlausibleProps | undefined = link.plausibleId
+  const plausibleProps: LinkPlausibleProps | undefined = link?.plausibleId
     ? { id: link.plausibleId }
     : undefined
 
-  return { label: link.label, href, target, plausibleProps }
+  return { label: link?.label ?? '', href, target, plausibleProps }
 }
