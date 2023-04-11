997882c13e16f72617da53194b982293116673e6
Marek Bodinger <marek.bodinger@gmail.com>
2023-04-11 13:39:13 +0200

    Footer fixes + accordion

--- next/components/ui/Footer/MobileFooter.tsx
+++ next/components/ui/Footer/MobileFooter.tsx
@@ -1,5 +1,6 @@
 import EULogo from '@assets/images/EULogo.svg'
 import { Brand, SectionContainer } from '@components/ui'
+import AccordionV2 from '@components/ui/AccordionV2/AccordionV2'
 import HorizontalDivider from '@components/ui/Divider/HorizontalDivider'
 import {
   FooterAccessibilityLink,
@@ -11,8 +12,11 @@ import {
 } from '@components/ui/Footer/FooterShared'
 import { useGeneralContext } from '@utils/generalContext'
 import { isDefined } from '@utils/isDefined'
-import React from 'react'
+import React, { Fragment } from 'react'
 
+/**
+ * Figma: https://www.figma.com/file/17wbd0MDQcMW9NbXl6UPs8/DS-ESBS%3A-Component-library?node-id=7558-20338&t=bWqVCwkV1Gv1qULG-0
+ */
 const MobileFooter = () => {
   const { footer } = useGeneralContext()
 
@@ -30,37 +34,36 @@ const MobileFooter = () => {
             <Brand url="/" withTitle />
           </div>
           <div className="flex flex-col gap-6 py-6">
-            <div className="flex flex-col gap-6 ">
-              <div className="flex flex-col gap-6">
-                <FooterContacts {...attributes} />
+            <div className="flex flex-col gap-6">
+              <FooterContacts {...attributes} />
+            </div>
+            <div>
+              <HorizontalDivider />
+              {attributes.columns?.filter(isDefined).map((column, index) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <Fragment key={index}>
+                  <AccordionV2 variant="footer" title={column.title}>
+                    <div className="flex flex-col gap-3">
+                      <FooterColumnLinks {...column} />
+                    </div>
+                  </AccordionV2>
+                  <HorizontalDivider />
+                </Fragment>
+              ))}
+            </div>
+            <div className="flex flex-col items-center gap-4">
+              <div className="-my-2 flex gap-2">
+                <FooterSocialLinks {...attributes} />
               </div>
-            </div>
-            <HorizontalDivider />
-            {attributes.columns?.filter(isDefined).map((column, index) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <div className="flex flex-col gap-4" key={index}>
-                <p className="text-h5 text-gray-800">{column.title}</p>
-                <div className="flex flex-col gap-3">
-                  <FooterColumnLinks {...column} />
-                </div>
+              <FooterAccessibilityLink {...attributes} />
+              <div className="flex gap-4">
+                <FooterLanguageSwitcher />
               </div>
-            ))}
-          </div>
-        </div>
-        <HorizontalDivider />
-        <div className="flex flex-col gap-6 py-6">
-          <div className="flex flex-col items-center gap-4">
-            <div className="-my-2 flex gap-2">
-              <FooterSocialLinks {...attributes} />
+              <EULogo />
             </div>
-            <FooterAccessibilityLink {...attributes} />
-            <div className="flex gap-4">
-              <FooterLanguageSwitcher />
+            <div className="text-center text-p-base">
+              <FooterCopyright {...attributes} />
             </div>
-            <EULogo />
-          </div>
-          <div className="text-center text-p-base">
-            <FooterCopyright {...attributes} />
           </div>
         </div>
       </footer>
