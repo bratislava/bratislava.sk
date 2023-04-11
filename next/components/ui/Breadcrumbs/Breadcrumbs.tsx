6be059ba1fd3c4bd5b912ad983bca69992d118ce
Marek Bodinger <marek.bodinger@gmail.com>
2023-04-11 13:41:31 +0200

    Mobile breadcrumbs

--- next/components/ui/Breadcrumbs/Breadcrumbs.tsx
+++ next/components/ui/Breadcrumbs/Breadcrumbs.tsx
@@ -1,8 +1,6 @@
+import DesktopBreadcrumbs from '@components/ui/Breadcrumbs/DesktopBreadcrumbs'
+import MobileBreadcrumbs from '@components/ui/Breadcrumbs/MobileBreadcrumbs'
 import React from 'react'
-
-import BreadcrumbsChevronIcon from '../../../assets/images/breadcrumbs-chevron.svg'
-import BreadcrumbsHomeIcon from '../../../assets/images/breadcrumbs-home.svg'
-import MLink from '../../forms/simple-components/MLink'
 
 export type Breadcrumb = {
   title: string
@@ -13,30 +11,16 @@ export type BreadcrumbsProps = {
   breadcrumbs: Breadcrumb[]
 }
 
-/**
- * TODO: Mobile breadcrumbs.
- */
-const Breadcrumbs = ({ breadcrumbs }: BreadcrumbsProps) => {
+const Breadcrumbs = (props: BreadcrumbsProps) => {
   return (
-    <nav>
-      <ol className="flex flex-wrap items-center gap-x-1 gap-y-1.5 py-3 lg:py-6">
-        <MLink href="/" variant="underlined" className="shrink-0">
-          <BreadcrumbsHomeIcon />
-        </MLink>
-        {breadcrumbs.map((breadcrumb, index) => (
-          <li className="text-p3-medium flex gap-1" key={index}>
-            <BreadcrumbsChevronIcon className="shrink-0" />
-            {breadcrumb.path ? (
-              <MLink href={breadcrumb.path} variant="underlined">
-                {breadcrumb.title}
-              </MLink>
-            ) : (
-              breadcrumb.title
-            )}
-          </li>
-        ))}
-      </ol>
-    </nav>
+    <>
+      <nav className="hidden lg:block">
+        <DesktopBreadcrumbs {...props} />
+      </nav>
+      <nav className="lg:hidden">
+        <MobileBreadcrumbs {...props} />
+      </nav>
+    </>
   )
 }
 
