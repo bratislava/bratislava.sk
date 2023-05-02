30f7cb31fb50925da56a3549289408673eba7f0a
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-05-02 14:32:40 +0200

    refactor Posts component to HomepageTabs

---
+++ next/components/ui/Sections/HomepageTabs/TabPanelRoadClosures.tsx
@@ -0,0 +1,137 @@
+import { ArrowRightIcon } from '@assets/images'
+import Button from '@components/forms/simple-components/Button'
+import MLink from '@components/forms/simple-components/MLink'
+import Tag from '@components/forms/simple-components/Tag'
+import BlogPostCard from '@components/molecules/presentation/BlogPostCard'
+import Carousel from '@components/organisms/Carousel/Carousel'
+import Iframe from '@components/ui/Iframe/Iframe'
+import { getCategoryColorLocalStyle } from '@utils/colors'
+import { generateImageSizes } from '@utils/generateImageSizes'
+import { useHomepageContext } from '@utils/homepageContext'
+import { isDefined } from '@utils/isDefined'
+import { getNumericLocalDate } from '@utils/local-date'
+import { useLocale, useTranslations } from 'next-intl'
+import React from 'react'
+import { TabPanel } from 'react-aria-components'
+
+const imageSizes = generateImageSizes({ lg: '33vw', default: '50vw' })
+
+const TabPanelRoadClosures = () => {
+  const t = useTranslations('HomepageTabs')
+  const locale = useLocale()
+
+  const roadClosuresAddress = '/doprava-a-mapy/sprava-a-udrzba-komunikacii/rozkopavky-a-uzavery'
+  const roadClosuresAddressNew = '/doprava-a-mapy/sprava-a-udrzba-komunikacii'
+
+  const { rozkopavkyBlogPosts } = useHomepageContext()
+  const postsFiltered = rozkopavkyBlogPosts?.filter(isDefined) ?? []
+
+  return (
+    <TabPanel id="RoadClosures">
+      {/* TODO when agreed, display only iframe part and remove everything else from this tab */}
+      <div className="hidden">
+        <div className="pb-8">
+          <Iframe
+            url={`https://cdn-api.bratislava.sk/static-pages/closures-and-restrictions-map/index.html?lang=${locale}`}
+            iframeWidth="container"
+            iframeHeight="620"
+            fullHeight={false}
+            allowFullscreen={false}
+          />
+        </div>
+        <div className="flex justify-center">
+          <Button
+            href={roadClosuresAddressNew}
+            variant="category-outline"
+            endIcon={<ArrowRightIcon />}
+          >
+            {t('moreInfo')}
+          </Button>
+        </div>
+      </div>
+      {/* TODO to be removed */}
+      <Carousel
+        className="-mx-8 lg:hidden"
+        itemClassName="w-[calc(100%-1rem)] md:w-[calc(50%-1rem)] py-8"
+        listClassName="px-8"
+        visibleCount={1}
+        hideControls
+        items={postsFiltered.map((post, index) => {
+          const { title, slug, coverImage, date_added, publishedAt, tag } = post.attributes ?? {}
+          const tagColor = tag?.data?.attributes?.pageCategory?.data?.attributes?.color
+          const tagTitle = tag?.data?.attributes?.title
+
+          return {
+            key: `${index}`,
+            element: (
+              <BlogPostCard
+                style={getCategoryColorLocalStyle({ color: tagColor })}
+                variant="shadow"
+                date={getNumericLocalDate(date_added ?? publishedAt)}
+                tag={tagTitle ?? undefined}
+                title={title ?? ''}
+                linkProps={{ children: t('readMore'), href: `/blog/${slug}` }}
+                imgSrc={coverImage?.data?.attributes?.url}
+                imgSizes={imageSizes}
+              />
+            ),
+          }
+        })}
+      />
+      <div className="mt-14 hidden pb-8 lg:block">
+        <div className="grid grid-cols-3 gap-x-8">
+          {postsFiltered.slice(0, 2).map((post) => {
+            const { title, slug, coverImage, date_added, publishedAt, tag, excerpt } =
+              post.attributes ?? {}
+            const tagColor = tag?.data?.attributes?.pageCategory?.data?.attributes?.color
+            const tagTitle = tag?.data?.attributes?.title
+
+            return (
+              <BlogPostCard
+                style={getCategoryColorLocalStyle({ color: tagColor })}
+                variant="shadow"
+                date={getNumericLocalDate(date_added ?? publishedAt)}
+                tag={tagTitle ?? undefined}
+                title={title ?? ''}
+                linkProps={{ children: t('readMore'), href: `/blog/${slug}` }}
+                imgSrc={coverImage?.data?.attributes?.url}
+                imgSizes={imageSizes}
+                text={excerpt ?? undefined}
+              />
+            )
+          })}
+
+          <div className="hidden flex-col gap-6 lg:flex">
+            {postsFiltered.slice(2, 7).map((newsCard, index) => {
+              const { tag, slug, title } = newsCard.attributes ?? {}
+              const colorStyle = getCategoryColorLocalStyle({
+                color: tag?.data?.attributes?.pageCategory?.data?.attributes?.color,
+              })
+
+              return (
+                // eslint-disable-next-line react/no-array-index-key
+                <div key={index} className="relative" style={colorStyle}>
+                  {tag?.data?.attributes?.title && (
+                    <div className="mb-3">
+                      <Tag text={tag.data.attributes.title} size="small" isColored />
+                    </div>
+                  )}
+                  <MLink href={`/blog/${slug}`} stretched variant="underlineOnHover">
+                    <h3 className="text-h5">{title}</h3>
+                  </MLink>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <Button href={roadClosuresAddress} variant="category-outline" endIcon={<ArrowRightIcon />}>
+          {t('seeMoreRoadClosures')}
+        </Button>
+      </div>
+    </TabPanel>
+  )
+}
+
+export default TabPanelRoadClosures
