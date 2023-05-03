06d387747be6d2183ceba6cf9ec41c44fd368443
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-05-03 12:41:28 +0200

    fix EN Mayor & Council translations (#914)

--- next/components/molecules/sections/homepage/BlogCardsPrimatorHomepageSection.tsx
+++ next/components/molecules/sections/homepage/BlogCardsPrimatorHomepageSection.tsx
@@ -4,55 +4,10 @@ import HomepageTabs from '@components/ui/Sections/HomepageTabs/HomepageTabs'
 import PrimatorCouncil from '@components/ui/Sections/PrimatorCouncil/PrimatorCouncil'
 import { useHomepageContext } from '@utils/homepageContext'
 import { isDefined } from '@utils/isDefined'
-import { useLocale } from 'next-intl'
-import React, { useMemo } from 'react'
-
-// TODO: Load from Strapi.
-const sk = {
-  councilCards: [
-    {
-      title: 'Primátor',
-      imageSrc: '/primatorReal.png',
-      href: 'mesto-bratislava/sprava-mesta/volene-organy/primator',
-    },
-    {
-      title: 'Zastupiteľstvo',
-      imageSrc: '/BACoatOfArms.svg',
-      smImageAlign: 'right' as const,
-      href: 'mesto-bratislava/sprava-mesta/volene-organy/zastupitelstvo',
-    },
-  ],
-}
-
-const en = {
-  councilCards: [
-    {
-      title: 'Primátor',
-      imageSrc: '/primatorReal.png',
-      href: 'mesto-bratislava/sprava-mesta/volene-organy/primator',
-    },
-    {
-      title: 'Zastupiteľstvo',
-      imageSrc: '/BACoatOfArms.svg',
-      smImageAlign: 'right' as const,
-      href: 'mesto-bratislava/sprava-mesta/volene-organy/zastupitelstvo',
-    },
-  ],
-}
+import React from 'react'
 
 const BlogCardsPrimatorHomepageSection = () => {
-  const locale = useLocale()
   const { homepage } = useHomepageContext()
-
-  const data = useMemo(() => {
-    if (locale === 'sk') {
-      return sk
-    }
-    if (locale === 'en') {
-      return en
-    }
-    return null
-  }, [locale])
 
   const homepagePosts = homepage?.attributes?.posts?.filter(isDefined) ?? []
 
@@ -60,8 +15,7 @@ const BlogCardsPrimatorHomepageSection = () => {
     <SectionContainer className="bg-gray-50 pb-14 md:px-8">
       <BlogCards posts={homepagePosts} />
       <HomepageTabs className="lg:mt-10" />
-
-      {data && <PrimatorCouncil className="mt-14 lg:mt-20" primatorCards={data.councilCards} />}
+      <PrimatorCouncil className="mt-14 lg:mt-20" />
     </SectionContainer>
   )
 }
--- next/components/ui/PrimatorCard/PrimatorCard.tsx
+++ next/components/ui/PrimatorCard/PrimatorCard.tsx
@@ -10,33 +10,14 @@ export interface PrimatorCardProps {
   title: string
   imageSrc: string
   href: string
-  smImageAlign?: 'left' | 'right'
 }
 
-export const PrimatorCard = ({
-  className,
-  title,
-  imageSrc,
-  href,
-  smImageAlign = 'left',
-}: PrimatorCardProps) => {
+export const PrimatorCard = ({ className, title, imageSrc, href }: PrimatorCardProps) => {
   const t = useTranslations()
-  const smRight = smImageAlign === 'right'
+
   return (
-    <div
-      className={cx(
-        'relative mt-16 w-full lg:mt-28',
-        {
-          'mt-12 lg:mt-28': smRight,
-        },
-        className,
-      )}
-    >
-      <Panel
-        className={cx(
-          'relative flex h-24 items-center justify-between overflow-visible px-6 lg:h-32 lg:justify-start lg:px-11',
-        )}
-      >
+    <div className={cx('relative mt-16 w-full lg:mt-28', className)}>
+      <Panel className="relative flex h-24 items-center justify-between overflow-visible px-6 lg:h-32 lg:justify-start lg:px-11">
         <div>
           <img src={imageSrc} alt={title} className="absolute bottom-0 h-40 lg:h-56" />
         </div>
