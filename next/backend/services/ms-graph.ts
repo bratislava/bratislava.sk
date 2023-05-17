838893aa245746b2350f998a3a2d01f9dda2af03
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-05-17 13:08:22 +0200

    order of deputy roles in organizational structure

--- next/backend/services/ms-graph.ts
+++ next/backend/services/ms-graph.ts
@@ -144,14 +144,26 @@ const roleOrderingScore = (role: string | null | undefined) => {
   // counting on 'Veduci/Veduca' and 'Riaditel/Riaditelka' to be the Role names in AD
   // deals with '1. ...' or 'Prvý ...' as super-special role (i.e. '1. námestníčka primátora')
   if (role.startsWith('1.') || role.startsWith('Prv')) {
+    score = 6
+  } else if (role.startsWith('Vedúc') || role.startsWith('Riadit') || role.startsWith('Poveren')) {
+    score = 5
+  } else if (role.includes('vedúc') || role.includes('riadit') || role.includes('poveren')) {
+    score = 4
+  } else if (
+    role.startsWith('Hlavná architektka') ||
+    role.startsWith('Hlavný architekt') ||
+    role.startsWith('Mestská kontrolórka') ||
+    role.startsWith('Mestský kontrolór') ||
+    role.startsWith('Hlavná ekonómka') ||
+    role.startsWith('Hlavný ekonóm')
+  ) {
     score = 3
-  } else if (role.startsWith('Vedúc') || role.startsWith('Riadit')) {
+  } else if (role.startsWith('Zástup')) {
     score = 2
-  } else if (role.includes('Vedúc') || role.includes('Riadit')) {
+  } else if (role.startsWith('Hovor')) {
     score = 1
   }
   return score
-  // TODO deal with other roles
 }
 
 export const getGroupMembersRecursive = async (
