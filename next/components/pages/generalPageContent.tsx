06fd87e730bca84ed51013c19dead241771341f3
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-05-22 11:32:28 +0200

    remove UA chatbot, revets #551, #594, #717 (#958)

--- next/components/pages/generalPageContent.tsx
+++ next/components/pages/generalPageContent.tsx
@@ -3,42 +3,18 @@ import PageHeader from '@bratislava/ui-bratislava/PageHeader/PageHeader'
 import RelatedBlogPostsSection from '@components/molecules/sections/general/RelatedBlogPostsSection'
 import { isDefined } from '@utils/isDefined'
 import { getPageBreadcrumbs } from '@utils/page'
-import { isProductionDeployment } from '@utils/utils'
-import dynamic from 'next/dynamic'
 import * as React from 'react'
 import { useMemo } from 'react'
-import { useIsClient } from 'usehooks-ts'
 
 import PageHeaderSections from '../molecules/PageHeaderSections'
 import Sections from '../molecules/Sections'
-
-// error with 'window' is not defined, that's because server side rendering + (ReactWebChat + DirectLine)
-// https://github.com/microsoft/BotFramework-WebChat/issues/4607
-const DynamicChat = dynamic(() => import('../molecules/chat'), {
-  ssr: false,
-})
 
 export interface GeneralPageProps {
   page: PageEntityFragment
 }
 
 const GeneralPageContent = ({ page }: GeneralPageProps) => {
-  const pageId = page.id
-
   const breadcrumbs = useMemo(() => getPageBreadcrumbs(page), [page])
-
-  const isClient = useIsClient()
-
-  // TODO: Refactor
-  const shouldDisplayUkraineSupportChat =
-    isProductionDeployment() &&
-    isClient &&
-    (pageId === '611' || // /bratislava-pre-ukrainu
-      pageId === '612' || // /братислава-для-украiни
-      pageId === '635' || // /en/bratislava-for-ukraine
-      pageId === '636' || // /en/братислава-для-украiни
-      page.attributes?.parentPage?.data?.attributes?.slug === 'bratislava-pre-ukrajinu' || // /bratislava-pre-ukrajinu/...
-      page.attributes?.parentPage?.data?.attributes?.slug === 'братислава-для-украiни') // /братислава-для-украiни/... || /en/братислава-для-украiни... because parent page slug is same for all languages
 
   return (
     <>
@@ -63,8 +39,6 @@ const GeneralPageContent = ({ page }: GeneralPageProps) => {
         />
       </div>
       <div className="mb-8" />
-
-      {shouldDisplayUkraineSupportChat && <DynamicChat />}
     </>
   )
 }
