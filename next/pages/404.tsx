4ba1eb941232a838159798c3eafbda73c802a347
Marek Bodinger <marek.bodinger@gmail.com>
2023-06-13 10:43:55 +0200

    Revalidate 404

--- next/pages/404.tsx
+++ next/pages/404.tsx
@@ -10,6 +10,7 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
     props: {
       messages: (await import(`../messages/${locale}.json`)).default,
     },
+    revalidate: 10,
   }
 }
 
