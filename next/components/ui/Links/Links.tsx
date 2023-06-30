94e7de1f5deda0ad20a2eae62592a8cb13af4222
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-06-30 14:42:43 +0200

    fix minor accessibility and layout fixes (#990)

--- next/components/ui/Links/Links.tsx
+++ next/components/ui/Links/Links.tsx
@@ -13,18 +13,20 @@ export const Links = ({ className, title, pageLinks }: LinksProps) => {
   return (
     <div className={cx(className, 'flex w-full flex-col md:w-10/12')}>
       {title && <h2 className="text-h4">{title}</h2>}
-      <div className="flex flex-col space-y-4 pt-6">
+      <ul className="flex flex-col space-y-4 pt-6">
         {pageLinks?.map((pageLink, index) => (
-          <Button
-            // eslint-disable-next-line react/no-array-index-key
-            key={index}
-            href={pageLink.url ? isExternalLink(pageLink.url) : `#${pageLink.anchor}`}
-            variant="black-link"
-          >
-            {pageLink?.title}
-          </Button>
+          <li>
+            <Button
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+              href={pageLink.url ? isExternalLink(pageLink.url) : `#${pageLink.anchor}`}
+              variant="black-link"
+            >
+              {pageLink?.title}
+            </Button>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   )
 }
