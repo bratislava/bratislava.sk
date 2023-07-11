024225993b3a1d24215c2c32452a0bee44176f7d
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-07-11 10:47:01 +0200

    revert not-mocking ginis in development (#1001)

--- next/backend/services/ginis.ts
+++ next/backend/services/ginis.ts
@@ -8,7 +8,6 @@ import { parseStringPromise } from 'xml2js'
 // ginis accessible only from internal network
 // if developing from internal network, change here
 export const shouldMockGinis = () => {
-  return false
   return (
     process.env.NODE_ENV === 'development' ||
     process.env.NODE_ENV === 'test' ||
