0913e91658e03b58175f673e79732a65999dd5db
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-09-07 17:03:02 +0200

    add inba content types to strapi model (#1052)

---
+++ strapi/src/api/inba-article/controllers/inba-article.ts
@@ -0,0 +1,7 @@
+/**
+ * inba-article controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::inba-article.inba-article');
---
+++ strapi/src/api/inba-tag/routes/inba-tag.ts
@@ -0,0 +1,7 @@
+/**
+ * inba-tag router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::inba-tag.inba-tag');
