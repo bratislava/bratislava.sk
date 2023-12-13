c775746d52b8c1f6faaadcc65964d0c6aa79a1c3
Adam <jakubicka.adam@gmail.com>
2023-12-13 19:30:39 +0100

    Undo unwanted change in PageCards component

--- next/components/ui/PageCards/PageCards.tsx
+++ next/components/ui/PageCards/PageCards.tsx
@@ -6,7 +6,7 @@ export interface PageCardsProps {
 
 export const PageCards = ({ pages }: PageCardsProps) => (
   <div className="flex flex-col gap-y-6">
-    {pages.slice(4).map((page, index) => (
+    {pages.map((page, index) => (
       // eslint-disable-next-line react/no-array-index-key
       <PageCard {...page} key={index} />
     ))}
