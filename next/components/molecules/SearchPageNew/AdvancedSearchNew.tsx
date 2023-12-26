c9eb59257a23c946c79e25e6df1b11147322e514
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-12-26 12:55:25 +0100

    use custom clear button in SearchField

--- next/components/molecules/SearchPageNew/AdvancedSearchNew.tsx
+++ next/components/molecules/SearchPageNew/AdvancedSearchNew.tsx
@@ -1,5 +1,6 @@
-import { SearchIcon } from '@assets/ui-icons'
+import { CrossInCircleIcon, SearchIcon } from '@assets/ui-icons'
 import { Typography } from '@bratislava/component-library'
+import Button from '@components/forms/simple-components/Button'
 import { useTranslations } from 'next-intl'
 import { Dispatch, SetStateAction } from 'react'
 import { Input, Label, SearchField } from 'react-aria-components'
@@ -38,10 +39,17 @@ const AdvancedSearchNew = ({
         </Typography>
       </Label>
       <div className="relative">
-        <Input className="relative w-full rounded-lg border-2 py-5 pl-[60px] pr-6" />
+        <Input className="relative w-full rounded-lg border-2 px-[60px] py-5" />
         <SearchIcon
           aria-hidden
           className="pointer-events-none absolute left-6 top-[calc(50%_-_12px)]"
+        />
+        <Button
+          icon={<CrossInCircleIcon />}
+          variant="icon-wrapped-negative-margin"
+          // Keyboard users use Escape to erase the input, they can't focus this button, so empty aria-label is okay
+          aria-label=""
+          className="absolute right-6 top-[calc(50%_-_12px)]"
         />
       </div>
     </SearchField>
