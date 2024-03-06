404f2ad211c670b1375daa761d98012a4214b469
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-03-06 11:41:00 +0100

    add nacist-soubor into tmp local ginis-sdk

--- next/ginis/api/json/ude/index.ts
+++ next/ginis/api/json/ude/index.ts
@@ -1,7 +1,9 @@
 import { detailDokumentu } from './detail-dokumentu'
+import { nacistSoubor } from './nacist-soubor'
 import { seznamDokumentu } from './seznam-dokumentu'
 
 export default {
   detailDokumentu,
+  nacistSoubor,
   seznamDokumentu,
 }
