edc647d827a81267e85c6e395b0c0e4f4d3574e8
Karolina Vorlickova <karolina.vorlickova@puxdesign.cz>
2024-03-18 14:01:03 +0100

    Add data-cy

--- next/components/organisms/SearchSection/SearchResults.tsx
+++ next/components/organisms/SearchSection/SearchResults.tsx
@@ -58,7 +58,7 @@ const SearchResults = ({
   }
 
   return (
-    <div className="flex flex-col gap-y-8">
+    <div className="flex flex-col gap-y-8" data-cy={`search-section-${searchOption?.displayNamePlural}`}>
       <div className="flex flex-col gap-y-4">
         {variant === 'allResults' && (
           <SearchResultsHeader
@@ -72,7 +72,7 @@ const SearchResults = ({
         )}
 
         {searchResultsData?.length ? (
-          <div className="flex flex-col lg:gap-y-2">
+          <div className="flex flex-col lg:gap-y-2" data-cy="search-section-results">
             {searchResultsData
               .slice(0, variant === 'allResults' ? GENERAL_RESULTS_COUNT : undefined)
               .map((item) => {
