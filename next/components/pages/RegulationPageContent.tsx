46f9e0ebe6b32538858f9434307b4d291acdfcfa
Adam <jakubicka.adam@gmail.com>
2024-03-23 16:56:19 +0100

    style: add responsivity to grid of amendmendts cards

--- next/components/pages/RegulationPageContent.tsx
+++ next/components/pages/RegulationPageContent.tsx
@@ -128,7 +128,7 @@ const RegulationPageContent = ({ regulation }: RegulationPageContentProps) => {
               {t('amendments')}
             </Typography>
             {amendments?.length ? (
-              <div className="grid grid-cols-4 gap-6">
+              <div className="grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-4 lg:grid-cols-4">
                 {amendments?.map((amendment) => {
                   return (
                     <RegulationCard
