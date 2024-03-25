6c7a65ef43cf894ebab042bd7e5d20aa79cfa29a
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-03-25 13:48:05 +0100

    move strapi logger middleware to first place to meet migration guides (#1230)

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -1,4 +1,5 @@
 module.exports = [
+  'strapi::logger',
   'strapi::errors',
   {
     name: 'strapi::security',
@@ -15,7 +16,6 @@ module.exports = [
   },
   'strapi::cors',
   'strapi::poweredBy',
-  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
