45f745146e1df7544175013cb5816d99563d6429
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-04-02 11:11:02 +0200

    minor fixes in PageHeader (#1245)
    * minor fixes in PageHeader
    
    * fix mobile overflow
    
    * fix thin line between waves and main content

--- next/components/ui/PageHeader/PageHeader.tsx
+++ next/components/ui/PageHeader/PageHeader.tsx
@@ -4,6 +4,7 @@ import Breadcrumbs, { BreadcrumbsProps } from '@bratislava/ui-bratislava/Breadcr
 import Waves from '@bratislava/ui-bratislava/Waves/Waves'
 import { generateImageSizes } from '@utils/generateImageSizes'
 import { getCommonLinkProps } from '@utils/getCommonLinkProps'
+import cx from 'classnames'
 import Image from 'next/image'
 import React, { PropsWithChildren } from 'react'
 import { twMerge } from 'tailwind-merge'
@@ -33,7 +34,7 @@ const PageHeader = ({
   children,
 }: PropsWithChildren<PageHeaderProps>) => {
   return (
-    <div className={twMerge('relative bg-category-200', className)}>
+    <div className={twMerge('relative overflow-x-clip bg-category-200', className)}>
       {imageSrc && (
         <div className="absolute right-0 top-0 hidden h-full w-[350px] md:block lg:w-[750px]">
           <Image
@@ -51,23 +52,33 @@ const PageHeader = ({
           />
         </div>
       )}
+
       <div className="mx-auto max-w-screen-xl px-4 lg:px-8">
         <div className="flex flex-col">
           <Breadcrumbs breadcrumbs={breadcrumbs} />
-          <div className="mb-6 mt-3 flex flex-col gap-y-4 lg:mb-10 lg:mt-6 lg:gap-y-6">
+
+          <div className="flex flex-col gap-y-4 pb-6 pt-3 lg:gap-y-6 lg:pb-10 lg:pt-6">
+            {/* TODO this tag is not in DS */}
             {tag && (
               <span className="text-default inline-block self-start rounded bg-category-700 px-3 py-1 font-medium text-white">
                 {tag}
               </span>
             )}
+
             {(title || subtext) && (
               <div className="flex max-w-[800px] flex-col gap-y-1 lg:gap-y-4">
                 {title && <Typography type="h1">{title}</Typography>}
-                {subtext && <Typography type="p">{subtext}</Typography>}
+                {subtext && (
+                  <Typography type="p" size="p-large">
+                    {subtext}
+                  </Typography>
+                )}
               </div>
             )}
-            {buttons && buttons.length > 0 && (
-              <div className="flex max-w-[800px] flex-col gap-2 sm:flex-row lg:gap-3">
+
+            {buttons?.length ? (
+              // wrapping to flex-row earlier (md) to prevent too wide buttons on tablet
+              <div className="flex max-w-[800px] flex-col gap-2 md:flex-row lg:gap-3">
                 {buttons.map((button, index) => (
                   <Button
                     // eslint-disable-next-line react/no-array-index-key
@@ -78,13 +89,40 @@ const PageHeader = ({
                   />
                 ))}
               </div>
-            )}
+            ) : null}
+
             {children && <div className="empty:hidden">{children}</div>}
           </div>
         </div>
       </div>
-      {/* Must be relative to cover up the image. */}
-      <Waves wavePosition="top" waveColor="white" className="relative" />
+
+      {imageSrc ? (
+        <div className="relative aspect-2/1 md:hidden">
+          <Image
+            src={imageSrc}
+            alt=""
+            sizes={generateImageSizes({ default: '100%' })}
+            fill
+            className="pointer-events-none h-full w-full object-cover"
+          />
+        </div>
+      ) : null}
+
+      {/*
+        Implemented by white "top" waves to cover up the image.
+        On desktop, it must be positioned 'relative', to add space to the whole header and to cover the image positioned 'absolute' on the right.
+        On mobile, if there is an image, it must be positioned 'absolute', to cover image positioned 'relative' below the Page header content,
+        if there is no image, it must be positioned 'relative' (otherwise it'll cover up the header content padding).
+          - Note: w-full fixes the overflow on mobile
+        Overlap by 1px (-mb-px) to prevent a thin line between the waves and the main content.
+      */}
+      <Waves
+        wavePosition="top"
+        waveColor="white"
+        className={cx('relative -mb-px', {
+          'max-md:absolute max-md:bottom-0 max-md:w-full': !!imageSrc,
+        })}
+      />
     </div>
   )
 }
