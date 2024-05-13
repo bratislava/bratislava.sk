528fde16f5565fec13a9a0ca624091fdb4dfbac2
Karolina Vorlickova <karolina.vorlickova@puxdesign.cz>
2024-05-13 09:41:39 +0200

    fix

--- tests/cypress/e2e/404Page.cy.ts
+++ tests/cypress/e2e/404Page.cy.ts
@@ -14,9 +14,9 @@ describe('Page 404', { testIsolation: false }, () => {
 
         it('1. Check if the 404 page is displayed.', () => {
           cy.dataCy("404-image").should("be.visible")
-          cy.dataCy("404-left-side", "div").contains("404").should("be.visible")
-          cy.dataCy("404-left-side", "p").contains("Ľutujeme, pre dané vyhľadávanie sa nenašli žiadne výsledky.").should("be.visible")
-          cy.dataCy("404-left-side", "a").should("be.visible").click()
+          cy.dataCy("404-left-side", "> div").contains("404").should("be.visible")
+          cy.dataCy("404-left-side", "> p").contains("Ľutujeme, pre dané vyhľadávanie sa nenašli žiadne výsledky.").should("be.visible")
+          cy.dataCy("404-left-side", "> a").should("be.visible").click()
         })
 
         it('2. Check if I was redirected to the homepage.', () => {
