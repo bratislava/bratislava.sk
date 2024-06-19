781865f4171fa07d81f0edfe5a8f658791a82181
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-06-19 20:25:39 +0200

    #1296 Add css variables for design tokens

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -212,6 +212,53 @@ module.exports = {
         700: 'rgb(var(--color-category-700))',
         800: 'rgb(var(--color-category-800))',
       },
+      background: {
+        primary: 'rgb(var(--token-background-primary))',
+        secondary: 'rgb(var(--token-background-secondary))',
+        tertiary: 'rgb(var(--token-background-tertiary))',
+        primaryInverted: 'rgb(var(--token-background-primary-inverted))',
+        disabled: 'rgb(var(--token-background-disabled))',
+      },
+      border: {
+        default: 'rgb(var(--token-border-default))',
+        hover: 'rgb(var(--token-border-hover))',
+        dark: 'rgb(var(--token-border-dark))',
+      },
+      content: {
+        primary: 'rgb(var(--token-content-primary))',
+        secondary: 'rgb(var(--token-content-secondary))',
+        tertiary: 'rgb(var(--token-content-tertiary))',
+        primaryInverted: 'rgb(var(--token-content-primary-inverted))',
+        secondaryInverted: 'rgb(var(--token-content-secondary-inverted))',
+        tertiaryInverted: 'rgb(var(--token-content-tertiary-inverted))',
+        disabled: 'rgb(var(--token-content-disabled))',
+        link: {
+          default: 'rgb(var(--token-content-link-default))',
+          hover: 'rgb(var(--token-content-link-hover))',
+          pressed: 'rgb(var(--token-content-link-pressed))',
+        },
+      },
+      action: {
+        content: {
+          default: 'rgb(var(--token-action-content-default))',
+          hover: 'rgb(var(--token-action-content-hover))',
+          pressed: 'rgb(var(--token-action-content-pressed))',
+        },
+        background: {
+          default: 'rgb(var(--token-action-background-default))',
+          hover: 'rgb(var(--token-action-background-hover))',
+          pressed: 'rgb(var(--token-action-background-pressed))',
+        },
+        softBackground: {
+          hover: 'rgb(var(--token-action-soft-background-hover))',
+          pressed: 'rgb(var(--token-action-soft-background-pressed))',
+        },
+        border: {
+          default: 'rgb(var(--token-action-border-default))',
+          hover: 'rgb(var(--token-action-border-hover))',
+          pressed: 'rgb(var(--token-action-border-pressed))',
+        },
+      },
     },
     extend: {
       rotate: {
