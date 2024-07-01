a185da097a959689cb1967cea30333cb4abbc7a1
katarinazaprazna <katarina.zaprazna@bratislava.sk>
2024-07-01 16:12:32 +0200

    Add comments regarding potential future changes

--- next/components/cards/FileRowCard.tsx
+++ next/components/cards/FileRowCard.tsx
@@ -30,6 +30,7 @@ const FileRowCard = ({
       <div className="flex flex-row items-start justify-between gap-x-4 py-4 pl-4 pr-2.5 md:px-6 lg:items-center">
         <div className="flex flex-row justify-between gap-x-2 md:items-center md:gap-x-4">
           <div className="md:rounded-lg md:bg-background-tertiary">
+            {/* TODO create a proper wrapper for the icon component to handle paddings */}
             <div className="md:p-3">
               <AttachmentIcon className="h-5 w-5 md:h-6 md:w-6" />
             </div>
@@ -58,11 +59,13 @@ const FileRowCard = ({
           </div>
         </div>
         <div className="flex shrink-0 items-center justify-center text-grey-700 md:rounded-lg md:border-2 md:border-grey-200">
+          {/* TODO create a proper wrapper for the icon component to handle paddings */}
           <div className="p-1.5 md:p-2.5">
             <DownloadIcon className="h-5 w-5" />
           </div>
         </div>
       </div>
+      {/* TODO consider using conditional styling instead of the HorizontalDivider component */}
       {hideBottomDivider ? null : (
         <div className="px-4 md:px-6">
           <HorizontalDivider />
