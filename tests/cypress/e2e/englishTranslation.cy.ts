523bb322d36b1d93476c7cd1fdb0cc5c74367df3
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-07-23 11:28:52 +0200

    Formatting

--- tests/cypress/e2e/englishTranslation.cy.ts
+++ tests/cypress/e2e/englishTranslation.cy.ts
@@ -7,42 +7,42 @@ describe('English translation', { testIsolation: false }, () => {
     .filter((device) => Cypress.env('devices')[`${device}`])
     .forEach((device) => {
       context(device, Cypress.env('resolution')[`${device}`], () => {
-
         before(() => {
           cy.visit('/kontakty')
         })
 
         it('1. Translating contact page.', () => {
-          cy.dataCy('heading-two').eq(0).invoke('text').then((text) => {
-            expect(text.replace(/\u00a0/g, ' ')).equal('Otváracie hodiny')
-          })
-          
+          cy.dataCy('heading-two')
+            .eq(0)
+            .invoke('text')
+            .then((text) => {
+              expect(text.replace(/\u00a0/g, ' ')).equal('Otváracie hodiny')
+            })
+
           if (device === 'desktop') {
             cy.dataCy('change-language-button').click()
-          } else { 
+          } else {
             cy.dataCy('mobile-change-language-button').click()
           }
         })
 
         it('2. Checking page heading translation.', () => {
           cy.wait(500)
-          cy.location('pathname', {timeout: 10000})
-          .should('eq', '/en/visiting-and-office-hours');
-          cy.dataCy('heading-two').eq(0).should("contain.text", "Opening hours")
+          cy.location('pathname', { timeout: 10000 }).should('eq', '/en/visiting-and-office-hours')
+          cy.dataCy('heading-two').eq(0).should('contain.text', 'Opening hours')
 
           if (device === 'desktop') {
             cy.dataCy('change-language-button').click()
-          } else { 
+          } else {
             cy.dataCy('mobile-change-language-button').click()
           }
         })
 
         it('3. Checking original language.', () => {
           cy.wait(500)
-          cy.location('pathname', {timeout: 10000})
-          .should('eq', '/kontakty');
-          cy.dataCy('heading-two').eq(0).should("contain.text", 'Otváracie hodiny')
+          cy.location('pathname', { timeout: 10000 }).should('eq', '/kontakty')
+          cy.dataCy('heading-two').eq(0).should('contain.text', 'Otváracie hodiny')
         })
       })
-   })
+    })
 })
--- tests/cypress/e2e/homepageSearch.cy.ts
+++ tests/cypress/e2e/homepageSearch.cy.ts
@@ -7,25 +7,27 @@ describe('S02 - ', { testIsolation: false }, () => {
     .filter((device) => Cypress.env('devices')[`${device}`])
     .forEach((device) => {
       context(device, Cypress.env('resolution')[`${device}`], () => {
-
         beforeEach(() => {
           cy.visit('/')
         })
-        
+
         it('1. Checking homepage search with results.', () => {
           cy.dataCy('homepage-search-field').should('be.visible')
           cy.dataCy('homepage-search-field').type('Bratislava')
           cy.dataCy('homepage-search-results').should('be.visible')
           cy.dataCy('homepage-search-results').then((results) => {
-            cy.wrap(Cypress.$('[data-cy=homepage-search-all-results]', results)).should('be.visible')
+            cy.wrap(Cypress.$('[data-cy=homepage-search-all-results]', results)).should(
+              'be.visible',
+            )
             cy.wrap(Cypress.$('[data-cy=homepage-search-all-results]', results)).click()
           })
-          cy.location('pathname', {timeout: 4000})
-          .should('eq', '/vyhladavanie');
+          cy.location('pathname', { timeout: 4000 }).should('eq', '/vyhladavanie')
           cy.dataCy('search-field').should('have.value', 'Bratislava')
           cy.dataCy('search-section-Stránky').should('be.visible')
           cy.dataCy('search-section-Stránky').then((section) => {
-            cy.wrap(Cypress.$('[data-cy=search-results]', section)).find('[data-cy=search-result-card]').should('exist')
+            cy.wrap(Cypress.$('[data-cy=search-results]', section))
+              .find('[data-cy=search-result-card]')
+              .should('exist')
           })
         })
 
@@ -33,11 +35,13 @@ describe('S02 - ', { testIsolation: false }, () => {
           cy.dataCy('homepage-search-field').should('be.visible')
           cy.dataCy('homepage-search-field').type('jbfhrebhgberhgerbhbcbeybgfyefbhbewhf')
           cy.dataCy('homepage-search-no-results').should('be.visible')
-          cy.dataCy('homepage-search-no-results').should('contain', 'Ľutujeme, pre dané vyhľadávanie sa nenašli žiadne výsledky.')
+          cy.dataCy('homepage-search-no-results').should(
+            'contain',
+            'Ľutujeme, pre dané vyhľadávanie sa nenašli žiadne výsledky.',
+          )
           cy.dataCy('homepage-search-button').click()
-          cy.location('pathname', {timeout: 4000})
-          .should('eq', '/vyhladavanie');
-          
+          cy.location('pathname', { timeout: 4000 }).should('eq', '/vyhladavanie')
+
           cy.dataCy('search-field').should('have.value', 'jbfhrebhgberhgerbhbcbeybgfyefbhbewhf')
           cy.dataCy('search-section-Stránky').should('be.visible')
           cy.dataCy('search-section-Stránky').should('contain', 'Žiadne výsledky')
