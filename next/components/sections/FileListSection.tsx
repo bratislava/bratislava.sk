f9bddfec2e2446b6f6006ef43cddf0eec1231605
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-09-19 11:13:11 +0200

    #1344 Display File List always as rows variant

--- next/components/sections/FileListSection.tsx
+++ next/components/sections/FileListSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import FileList from '@/components/common/FileList/FileList'
-import { Enum_Componentsectionsfilelist_Variant, FileListSectionFragment } from '@/services/graphql'
+import { FileListSectionFragment } from '@/services/graphql'
 import { groupByCategoryFileList } from '@/utils/pageUtils_Deprecated'
 import { isPresent } from '@/utils/utils'
 
@@ -16,7 +16,6 @@ const FileListSection = ({ section }: FileListSectionProps) => {
       title={section.title}
       text={section.text}
       hideCategory={!!section.title || !!section.text}
-      variantFileList={section.variantFileList ?? Enum_Componentsectionsfilelist_Variant.Grid}
       fileSections={groupByCategoryFileList(section.fileList?.filter(isPresent) ?? [])}
     />
   )
