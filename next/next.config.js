f9822c3d9a4541887ae3a8e5e57ebd7bfa7c8a72
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-03-25 12:45:09 +0100

    #1420 Update @svgr/webpack

--- next/next.config.js
+++ next/next.config.js
@@ -1,5 +1,6 @@
 const { withPlausibleProxy } = require('next-plausible')
 const i18nextConfig = require('./next-i18next.config.js')
+const svgoConfig = require('./svgo.config.js')
 
 /**
  * @type {import('next').NextConfig}
@@ -1745,24 +1746,33 @@ const nextConfig = {
       },
     ]
   },
+  // Docs: https://react-svgr.com/docs/next/
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: /\.[jt]sx?$/,
-      use: {
-        loader: '@svgr/webpack',
-        options: {
-          svgoConfig: {
-            plugins: [
-              {
-                name: 'removeViewBox',
-                active: false,
-              },
-            ],
-          },
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: {
+          loader: '@svgr/webpack',
+          options: { svgoConfig },
         },
       },
-    })
+    )
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i
+
     return config
   },
 }
