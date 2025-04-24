a500aa4fab2a5bf12e631278d980a92b2117d303
Jana Běhal Dadová <jana.dadova@bratislava.sk>
2025-04-24 10:41:48 +0200

    Add console logs to revalidate.ts

--- next/src/pages/api/revalidate.ts
+++ next/src/pages/api/revalidate.ts
@@ -14,8 +14,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<Response>) => {
   try {
     const payload = req.body as RequestPayload
 
+    console.log('api/revalidate: payload ==>', payload)
     const localePrefix = payload.entry.locale === 'en' ? '/en' : ''
+
+    console.log('api/revalidate: localePrefix ==>', localePrefix)
     const homepage = payload.entry.locale === 'en' ? '/en' : '/'
+    console.log('api/revalidate: homepage ==>', homepage)
 
     if (payload?.model === 'blog-post') {
       const blogUrl = `${localePrefix}/blog/${payload?.entry?.slug}`
@@ -23,11 +27,15 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<Response>) => {
       await res.revalidate(blogUrl)
     }
 
+    console.log('api/revalidate: blogPost')
+
     if (payload?.model === 'page') {
       const pageUrl = `${localePrefix}/${payload?.entry?.slug}`
       console.log('api/revalidate:', pageUrl)
       await res.revalidate(pageUrl)
     }
+
+    console.log('api/revalidate: page')
 
     /** Always revalidate homepage */
     console.log('api/revalidate:', homepage)
