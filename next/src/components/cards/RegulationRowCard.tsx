034bcf0f0838527669779ffb75cab5c54f24369c
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-04-30 10:39:41 +0200

    #1508 Use RegulationRowCard in Regulations section (#1509)
    * #1508 Use RegulationRowCard in Regulations section
    
    * #1508 Use RegulationRowCard in detail page
    
    - add helper function getRegulationMetadata
    - update color of state alert to red also when cancelled as amendee
    - show more details in Regulations section on staging

---
+++ next/src/components/cards/RegulationRowCard.tsx
@@ -0,0 +1,85 @@
+import { Typography } from '@bratislava/component-library'
+import React from 'react'
+
+import { ChevronRightIcon } from '@/src/assets/ui-icons'
+import CardBase from '@/src/components/cards/CardBase'
+import MLink from '@/src/components/common/MLink/MLink'
+import { isDefined } from '@/src/utils/isDefined'
+import { useTranslation } from '@/src/utils/useTranslation'
+
+type Props = {
+  title: string
+  path?: string
+  className?: string
+  isFullTextRegulation?: boolean | null | undefined
+  isAmendee?: boolean | null | undefined
+  isCancelled?: boolean | null | undefined
+  metadata?: string[] | null
+}
+
+/**
+ * Figma: https://www.figma.com/design/17wbd0MDQcMW9NbXl6UPs8/DS--Component-library?node-id=14811-27790&m=dev
+ */
+
+const RegulationRowCard = ({
+  title,
+  path,
+  className,
+  isFullTextRegulation,
+  isAmendee,
+  isCancelled,
+  metadata,
+}: Props) => {
+  const { t } = useTranslation()
+
+  return (
+    <CardBase className={className} variant="no-border">
+      <div className="flex flex-row justify-between gap-4 py-4">
+        <div className="flex flex-col gap-2 lg:gap-1">
+          <div className="flex flex-col items-start gap-2 lg:flex-row lg:items-center lg:gap-4">
+            <MLink
+              href={path ?? '#'}
+              className="text-h6 break-words font-bold"
+              stretched
+              variant="underlineOnHover"
+            >
+              {title}
+            </MLink>
+            {/* TODO style https://www.figma.com/design/17wbd0MDQcMW9NbXl6UPs8/DS--Component-library?node-id=14811-28150&m=dev */}
+            {isFullTextRegulation && (
+              <Typography type="p" size="p-small" className="rounded border px-2 py-0.5">
+                {t('Regulation.fullTextRegulation')}
+              </Typography>
+            )}
+            {/* TODO style and wording */}
+            {isAmendee && (
+              <Typography type="p" size="p-small" className="rounded border px-2 py-0.5">
+                {t('Regulation.amendment')}
+              </Typography>
+            )}
+            {/* TODO style */}
+            {isCancelled && (
+              <Typography type="p" size="p-small" className="rounded border px-2 py-0.5">
+                {t('Regulation.cancelled')}
+              </Typography>
+            )}
+          </div>
+          {metadata?.length ? (
+            <span className="text-component-default text-grey-700">
+              {metadata.filter(isDefined).join(' • ')}
+            </span>
+          ) : null}
+        </div>
+        {/* TODO styling (hover...) */}
+        <div className="flex shrink-0 items-center justify-center text-grey-700">
+          {/* TODO create a proper wrapper for the icon component to handle paddings */}
+          <div className="p-1.5 md:p-2.5">
+            <ChevronRightIcon className="size-5" />
+          </div>
+        </div>
+      </div>
+    </CardBase>
+  )
+}
+
+export default RegulationRowCard
--- next/src/components/common/Regulations/Regulations.tsx
+++ next/src/components/common/Regulations/Regulations.tsx
@@ -1,54 +1,76 @@
 import { Typography } from '@bratislava/component-library'
+import { Fragment } from 'react'
 
-import RegulationCard from '@/src/components/cards/RegulationCard/RegulationCard'
-import ResponsiveCarousel from '@/src/components/common/Carousel/ResponsiveCarousel'
+import RegulationRowCard from '@/src/components/cards/RegulationRowCard'
+import HorizontalDivider from '@/src/components/common/Divider/HorizontalDivider'
 import { RegulationEntityFragment } from '@/src/services/graphql'
+import cn from '@/src/utils/cn'
+import { getRegulationMetadata } from '@/src/utils/getRegulationMetadata'
+import { isDefined } from '@/src/utils/isDefined'
 import { useTranslation } from '@/src/utils/useTranslation'
+import { isProductionDeployment } from '@/src/utils/utils'
 
 type Props = {
   className?: string
   regulations?: RegulationEntityFragment[]
 }
 
+/**
+ * Figma: https://www.figma.com/design/17wbd0MDQcMW9NbXl6UPs8/DS--Component-library?node-id=16846-9451&m=dev
+ */
+
 const Regulations = ({ className, regulations }: Props) => {
   const { t } = useTranslation()
 
   return (
-    <div className={className}>
+    <div className={cn('flex flex-col gap-8', className)}>
       <Typography type="h2">{t('Regulation.relatedRegulations')}</Typography>
       {regulations?.length ? (
-        <>
-          <div className="mt-6 hidden grid-cols-3 gap-8 lg:grid">
-            {regulations?.map((regulation) => {
-              return (
-                <div className="w-full">
-                  <RegulationCard
-                    title={`VZN ${regulation.attributes?.regNumber ?? ''}`}
-                    key={regulation.attributes?.regNumber}
-                    metadata={t('Regulation.numberOfAmendments', {
-                      count: regulation.attributes?.amendments?.data.length ?? 0,
-                    })}
-                    path={`/vzn/${regulation.attributes?.slug}`}
-                  />
-                </div>
-              )
-            })}
-          </div>
-          <div className="lg:hidden">
-            <ResponsiveCarousel
-              items={regulations?.map((regulation) => (
-                <RegulationCard
-                  title={`VZN ${regulation.attributes?.regNumber ?? ''}`}
-                  key={regulation.attributes?.regNumber}
-                  metadata={t('Regulation.numberOfAmendments', {
-                    count: regulation.attributes?.amendments?.data.length ?? 0,
-                  })}
-                  path={`/vzn/${regulation.attributes?.slug ?? ''}`}
-                />
-              ))}
-            />
-          </div>
-        </>
+        <ul className="flex flex-col rounded-lg border-2 py-2">
+          {regulations?.map((regulation, index) => {
+            if (!regulation.attributes) {
+              return null
+            }
+
+            const { effectiveUntil } = getRegulationMetadata(regulation.attributes)
+
+            const numberOfAmendments = regulation.attributes.amendments?.data.length
+            const numberOfAmendmentsMessage = numberOfAmendments
+              ? t('Regulation.numberOfAmendments', { count: numberOfAmendments })
+              : null
+
+            return (
+              <Fragment key={regulation.attributes.regNumber}>
+                {index > 0 ? <HorizontalDivider asListItem className="mx-4 lg:mx-6" /> : null}
+                <li className="w-full">
+                  {/* TODO we show more details on staging for testing and cleanup purposes */}
+                  {/* TODO rewrite to one component when we know what details we want to show  */}
+                  {isProductionDeployment() ? (
+                    <RegulationRowCard
+                      title={`VZN ${regulation.attributes.regNumber} ${regulation.attributes.titleText ?? ''}`}
+                      metadata={[numberOfAmendmentsMessage].filter(isDefined)}
+                      isFullTextRegulation={regulation.attributes.isFullTextRegulation}
+                      // isAmendee={!!regulation.attributes.amending?.data?.length}
+                      // isCancelled={!!effectiveUntil}
+                      path={`/vzn/${regulation.attributes.slug}`}
+                      className="px-4 lg:px-6"
+                    />
+                  ) : (
+                    <RegulationRowCard
+                      title={`VZN ${regulation.attributes.regNumber} ${regulation.attributes.titleText ?? ''}`}
+                      metadata={[numberOfAmendmentsMessage].filter(isDefined)}
+                      isFullTextRegulation={regulation.attributes.isFullTextRegulation}
+                      isAmendee={!!regulation.attributes.amending?.data?.length}
+                      isCancelled={!!effectiveUntil}
+                      path={`/vzn/${regulation.attributes.slug}`}
+                      className="px-4 lg:px-6"
+                    />
+                  )}
+                </li>
+              </Fragment>
+            )
+          })}
+        </ul>
       ) : null}
     </div>
   )
