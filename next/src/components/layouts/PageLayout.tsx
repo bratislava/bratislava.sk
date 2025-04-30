436e6958c621f33535850846daf703a84b71b191
Jakubička Adam <jakubicka.adam@gmail.com>
2025-04-30 15:30:28 +0200

    Remove cookieconsent and hotjar

--- next/src/components/layouts/PageLayout.tsx
+++ next/src/components/layouts/PageLayout.tsx
@@ -1,6 +1,5 @@
 import { PropsWithChildren } from 'react'
 
-import CookieConsent from '@/src/components/common/CookieConsent/CookieConsent'
 import Footer from '@/src/components/common/Footer/Footer'
 import NavBar from '@/src/components/common/NavBar/NavBar'
 import ScrollToTopButton from '@/src/components/common/ScrollToTopButton/ScrollToTopButton'
@@ -13,8 +12,6 @@ const PageLayout = ({ className, children }: PropsWithChildren<PageLayoutProps>)
   return (
     // Z-indices are set to create stacking contexts for easier z-index management.
     <div className={className}>
-      <CookieConsent className="z-30" /* position: fixed */ />
-
       <header className="relative">
         <NavBar />
       </header>
