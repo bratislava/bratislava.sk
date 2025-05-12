e43b19edc07b1279af5ba228ed5bb6f7331e8320
Adam <jakubicka.adam@gmail.com>
2025-05-13 01:20:35 +0200

    Implement new design for Gallery

--- next/src/components/common/Gallery/Gallery.tsx
+++ next/src/components/common/Gallery/Gallery.tsx
@@ -2,20 +2,24 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 
+import { Typography } from '@bratislava/component-library'
 import { useCallback, useState } from 'react'
 import { useOverlayTriggerState } from 'react-stately'
-import { useWindowSize } from 'usehooks-ts'
 
 import StrapiImage from '@/src/components/common/Image/StrapiImage'
 import { UploadImageEntityFragment } from '@/src/services/graphql'
 import cn from '@/src/utils/cn'
+import { isDefined } from '@/src/utils/isDefined'
 import { onEnterOrSpaceKeyDown } from '@/src/utils/onEnterOrSpaceKeyDown'
+import { useTailwindBreakpointValue } from '@/src/utils/useTailwindBreakpointValue'
 import { useTranslation } from '@/src/utils/useTranslation'
-import screens from '@/tailwind.config.screens'
 
 import ImageLightBox from './ImageLightBox'
 
-// inspired by MKB project https://github.com/bratislava/mestskakniznica.sk/blob/master/next/modules/common/ImageGallery/ImageGallery.tsx
+/**
+ * Figma: https://www.figma.com/design/17wbd0MDQcMW9NbXl6UPs8/DS--Component-library?node-id=16846-35038&m=dev
+ * inspired by MKB project https://github.com/bratislava/mestskakniznica.sk/blob/master/next/modules/common/ImageGallery/ImageGallery.tsx
+ */
 
 export type GalleryProps = {
   images: UploadImageEntityFragment[]
@@ -24,17 +28,22 @@ export type GalleryProps = {
 const Gallery = ({ images = [] }: GalleryProps) => {
   const { t } = useTranslation()
 
-  const { width: windowWidth } = useWindowSize()
-  // TODO refactor to use som custom hook
-  const isMobile = windowWidth < parseInt(screens.md.slice(0, -2), 10)
+  const { isMobile } = useTailwindBreakpointValue()
 
-  const thumbnailCount = isMobile ? 4 : 8
+  const imageCount = images.length
+
+  // based on the number of images, some are shown and some may be hidden behind the "more images" button
+  let smallImagesCount = imageCount
+  if ((imageCount > 4 && isMobile) || (imageCount > 4 && imageCount < 8)) {
+    smallImagesCount = 3
+  } else if (imageCount > 8 && !isMobile) {
+    smallImagesCount = 7
+  }
+
+  const smallImages = images.slice(0, smallImagesCount)
 
   // number of not displayed images
-  const moreImagesCount = Math.max(images.length - thumbnailCount + 1, 0)
-
-  // if <=8, show all, else show 7 and "more button"
-  const smallImages = images.slice(0, moreImagesCount > 1 ? thumbnailCount - 1 : thumbnailCount + 1)
+  const moreImagesCount = imageCount - smallImagesCount
 
   const overlayState = useOverlayTriggerState({ defaultOpen: false })
   const [initialImageIndex, setInitialImageIndex] = useState(0)
@@ -58,35 +67,55 @@ const Gallery = ({ images = [] }: GalleryProps) => {
           className={cn('outline-primary cursor-default outline-offset-2 focus:outline-4')}
         >
           {smallImages.length > 0 && (
-            <div className="grid grid-cols-2 gap-2 md:grid-cols-4 md:gap-8">
+            <div
+              className={cn('grid grid-cols-2 gap-2 md:gap-8', {
+                'md:grid-cols-1': imageCount === 1,
+                'md:grid-cols-2': imageCount === 2,
+                'md:grid-cols-3': imageCount === 3,
+                'md:grid-cols-4': imageCount > 3,
+              })}
+            >
               {smallImages
-                .filter((image) => image.attributes)
-                .map((image, index) => (
-                  <div
-                    onClick={() => openAtImageIndex(index)}
-                    key={image.id}
-                    className="relative size-full cursor-pointer overflow-hidden rounded-lg pt-[100%]"
-                  >
-                    <StrapiImage
-                      // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-                      image={image.attributes!}
-                      fill
-                      className="absolute top-0 object-cover"
-                    />
-                  </div>
-                ))}
+                .map((image, index) => {
+                  if (!image.attributes) return null
+
+                  return (
+                    <div
+                      onClick={() => openAtImageIndex(index)}
+                      key={image.id}
+                      className={cn(
+                        'relative aspect-square cursor-pointer overflow-hidden rounded-lg',
+                        {
+                          'md:aspect-[592/400]': imageCount === 1 || imageCount === 2,
+                          'md:aspect-[384/272]': imageCount === 3,
+                          'col-span-2 aspect-[288/140]':
+                            isMobile && imageCount === 3 && index === 0,
+                        },
+                      )}
+                    >
+                      <StrapiImage
+                        image={image.attributes}
+                        fill
+                        className="absolute top-0 object-cover"
+                      />
+                    </div>
+                  )
+                })
+                .filter(isDefined)}
 
               {/* more images button */}
-              {moreImagesCount > 1 && (
+              {moreImagesCount > 0 && (
                 <div
                   onClick={() => openAtImageIndex(0)}
-                  className="relative w-full cursor-pointer overflow-hidden rounded-lg border-2 border-category-700 pt-[100%]"
+                  className="relative w-full cursor-pointer overflow-hidden rounded-lg border-2 border-action-content-default pt-[100%]"
                 >
                   <div className="absolute top-0 flex size-full flex-col items-center justify-center gap-0.5 bg-white p-2 text-center">
-                    <div className="text-h2 font-semibold">+{moreImagesCount}</div>
-                    <div className="text-large">
-                      {t('Gallery.morePhotos', { count: moreImagesCount })}
-                    </div>
+                    <Typography type="h2">+{moreImagesCount}</Typography>
+                    {isMobile ? null : (
+                      <Typography type="p" className="text-large">
+                        {t('Gallery.morePhotos', { count: moreImagesCount })}
+                      </Typography>
+                    )}
                   </div>
                 </div>
               )}
