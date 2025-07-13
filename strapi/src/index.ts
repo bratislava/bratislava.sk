56ed4c2eeeb6b98f369bf9bb4aedcd24b4ee4aff
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-07-13 18:06:48 +0200

    Delete legacy commented-out code from register function

--- strapi/src/index.ts
+++ strapi/src/index.ts
@@ -110,35 +110,6 @@ export default {
 
       return next()
     })
-
-    // const extensionService = strapi.plugin('graphql').service('extension');
-
-    // extensionService.use(({ nexus }) => {
-    //   const pageBySlug = nexus.extendType({
-    //     type: "Query",
-    //     definition(t) {
-    //        myQuery definition
-    //       t.field('pageBySlug', {
-    //         Response type
-    //         type: 'Page',
-
-    //         Args definition
-    //         args: { slug: 'String', locale: 'String' },
-
-    //         Resolver definition
-    //         resolve(parent, args, ctx) {
-    //           const { slug, locale } = args;
-    //           console.log({slug,locale})
-    //           return strapi.db.query('application::page.page').findOne({
-    //             where: { slug, locale },
-    //         });
-    //         }
-    //       });
-    //     }
-    //   });
-
-    //   return { types: [pageBySlug] };
-    // });
   },
 
   /**
