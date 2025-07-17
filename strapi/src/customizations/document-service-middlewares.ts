215973709ba3217011085c6f4589062ebe25985d
Adam <jakubicka.adam@gmail.com>
2025-07-17 23:46:58 +0200

    Fix document service middleware so it doesn't throw error when first saving and then publishing a new document

--- strapi/src/customizations/document-service-middlewares.ts
+++ strapi/src/customizations/document-service-middlewares.ts
@@ -1,7 +1,6 @@
 // Document Service middlewares docs: https://docs.strapi.io/cms/api/document-service/middlewares#context
 
 import { Core } from '@strapi/strapi'
-import { AdminRole } from '../../types/generated/contentTypes'
 
 type AdminGroupId = 'starz'
 
@@ -30,36 +29,35 @@ const getAdminGroup = async ({
 
 export const registerDocumentServiceMiddlewares = ({ strapi }: { strapi: Core.Strapi }) => {
   // TODO refactor to allow more adminGroup values
-  const STARZ_ADMINGROUP_ID = 'starz'   // Value of field adminGroupId of AdminGroup collection in Strapi, 
+  const STARZ_ADMINGROUP_ID = 'starz' // adminGroupId of AdminGroup collection in Strapi,
   const STARZ_ROLE_NAME_REGEX = 'starz' // Admin role name in Strapi
   const STARZ_ARTICLE_TAG_TITLE = 'Šport' // Tag name to be added to starz articles on creation
 
   strapi.documents.use(async (context, next) => {
     if (
-      (context.uid == 'api::article.article' ||
-        context.uid == 'api::page.page' ||
-        context.uid == 'api::document.document' ||
-        context.uid == 'api::faq.faq') &&
-      context.action == 'create'
+      (context.uid === 'api::article.article' ||
+        context.uid === 'api::page.page' ||
+        context.uid === 'api::document.document' ||
+        context.uid === 'api::faq.faq') &&
+      context.action === 'create'
     ) {
       const adminGroup = await getAdminGroup({ adminGroupId: STARZ_ADMINGROUP_ID, strapi })
 
       const document = context.params.data
-      const documentCreatorId = document.createdBy
+      const activeUserId = document.updatedBy
 
-      const documentCreatorUser = await strapi.db.query('admin::user').findOne({
+      const activeUser = await strapi.db.query('admin::user').findOne({
         where: {
-          id: documentCreatorId,
+          id: activeUserId,
         },
         populate: {
           roles: true,
         },
       })
 
-      // Check if document creator role includes starz - If yes, add starz to adminGroups
+      // Check if document creator role includes starz
       if (
-        documentCreatorUser.roles.some((role) => {
-          console.log(JSON.stringify(role))
+        activeUser.roles.some((role) => {
           return new RegExp(STARZ_ROLE_NAME_REGEX, 'i').test(role.name)
         })
       ) {
