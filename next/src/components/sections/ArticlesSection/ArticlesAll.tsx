40cc7d86d2f3082c44f0bbda93d3a3fbeaf589d1
Adam <jakubicka.adam@gmail.com>
2025-07-22 13:40:06 +0200

    Make changes according to code review

--- next/src/components/sections/ArticlesSection/ArticlesAll.tsx
+++ next/src/components/sections/ArticlesSection/ArticlesAll.tsx
@@ -73,10 +73,6 @@ const ArticlesAll = ({ section }: Props) => {
         // TODO Correct spacing between SectionHeader and remaining content
         className="pb-6 lg:pb-8"
       />
-      {/* {JSON.stringify(filters)} */}
-      {/* {JSON.stringify(pageCategoriesData)} */}
-      {/* {JSON.stringify(tagsData)} */}
-      {/* {JSON.stringify(data)} */}
       <div className="flex flex-col gap-8">
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
           {data?.hits.map((card) => (
