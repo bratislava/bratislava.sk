cc548df9b69875e644c975ebe5daa0dc7d9517ad
Adam <jakubicka.adam@gmail.com>
2025-07-25 16:42:04 +0200

    Add TableOfContentsSection

---
+++ next/src/components/sections/TableOfContentsSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import TableOfContents from '@/src/components/common/TableOfContents/TableOfContents'
+import SectionContainer from '@/src/components/layouts/SectionContainer'
+
+const TableOfContentsSection = () => {
+  return (
+    <SectionContainer>
+      <TableOfContents />
+    </SectionContainer>
+  )
+}
+
+export default TableOfContentsSection
