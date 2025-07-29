50b56e5b31a074f4c53c59c49e1b63f27d96d6db
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-07-29 15:25:06 +0200

    #1554 Rewrite VideoCard to handle video links more universally

--- next/src/components/cards/VideoCard.tsx
+++ next/src/components/cards/VideoCard.tsx
@@ -1,56 +1,120 @@
 import { Typography } from '@bratislava/component-library'
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 
 import MLink from '@/src/components/common/MLink/MLink'
 import { VideoBlockFragment } from '@/src/services/graphql'
 import cn from '@/src/utils/cn'
 
 /**
- * TODO revisit how url is handled
  * TODO figma link
+ *
+ * Based on AI suggestions.
  */
 
-const VideoCard = ({ title, speaker, url }: VideoBlockFragment) => {
-  const [embedUrl, setEmbedUrl] = React.useState('')
-  const [isLoaded, setLoaded] = React.useState(false)
+const VideoCard = ({ title, speaker, url: untrimmedUrl }: VideoBlockFragment) => {
+  const [isLoaded, setLoaded] = useState(false)
 
-  useEffect(() => {
-    const parseYoutubeUrl = async () => {
-      if (url?.includes('fb.watch')) {
-        const fembedUrl = `https://www.facebook.com/plugins/video.php?href=${url}`
-        setEmbedUrl(fembedUrl)
-      } else {
-        const oembedUrl = `https://www.youtube.com/oembed?url=${url}&format=json`
-        const res = await fetch(oembedUrl)
-        const { html }: { html: string } = await res.json()
+  const url = untrimmedUrl.trim()
 
-        const substrStart = html.indexOf('src="') + 5
-        const substrEnd = html.indexOf('oembed') + 6
-        const embededUrl = html.slice(substrStart, substrEnd)
-
-        setEmbedUrl(embededUrl)
-      }
+  const detectPlatform = () => {
+    if (url.includes('youtube.com') || url.includes('youtu.be')) {
+      return 'youtube'
+    }
+    if (url.includes('vimeo.com')) {
+      return 'vimeo'
+    }
+    if (url.includes('facebook.com') || url.includes('fb.watch')) {
+      return 'facebook'
     }
 
-    parseYoutubeUrl()
-  }, [url])
+    return 'unknown'
+  }
+
+  const getVideoId = (platform: string) => {
+    if (platform === 'youtube') {
+      const youtubeRegex =
+        /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[&?]v=)|youtu\.be\/)([^\s"&/?]{11})/
+      const youtubeMatch = url.match(youtubeRegex)
+
+      return youtubeMatch ? youtubeMatch[1] : null
+    }
+
+    if (platform === 'vimeo') {
+      const vimeoRegex = /vimeo\.com\/(?:.*\/)?(\d+)/
+      const vimeoMatch = url.match(vimeoRegex)
+
+      return vimeoMatch ? vimeoMatch[1] : null
+    }
+
+    if (platform === 'facebook') {
+      // Facebook videos have different URL patterns, we'll use the full URL
+      return url
+    }
+
+    return null
+  }
+
+  const platform = detectPlatform()
+  const videoId = getVideoId(platform)
+
+  if (platform === 'unknown' || !videoId) {
+    // eslint-disable-next-line i18next/no-literal-string
+    return <div>Unsupported url.</div>
+  }
+
+  const renderEmbed = () => {
+    const commonProps = {
+      width: '100%',
+      height: '100%',
+      allowFullScreen: true,
+      className: cn('absolute top-0 left-0 size-full border-0', {
+        'animate-pulse bg-grey-300': !isLoaded,
+      }),
+      onLoad: () => setLoaded(true),
+    }
+
+    switch (platform) {
+      case 'youtube':
+        return (
+          <iframe
+            {...commonProps}
+            src={`https://www.youtube.com/embed/${videoId}?rel=0&modestbranding=1`}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          />
+        )
+
+      case 'vimeo':
+        return (
+          <iframe
+            {...commonProps}
+            src={`https://player.vimeo.com/video/${videoId}?badge=0&autopause=0&player_id=0&app_id=58479`}
+            title="Vimeo video player"
+            allow="autoplay; fullscreen; picture-in-picture"
+          />
+        )
+
+      case 'facebook':
+        return (
+          <iframe
+            {...commonProps}
+            src={`https://www.facebook.com/plugins/video.php?href=${encodeURIComponent(
+              videoId,
+            )}&show_text=false&width=560&height=315&t=0`}
+            title="Facebook video player"
+            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+            style={{ border: 'none', overflow: 'hidden' }}
+          />
+        )
+
+      default:
+        return null
+    }
+  }
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="relative aspect-video w-full overflow-hidden rounded-lg">
-        <iframe
-          width="100%"
-          height="100%"
-          title={title ?? undefined}
-          src={embedUrl}
-          allow="accelerometer; encrypted-media; gyroscope; web-share"
-          className={cn('absolute top-0 left-0 size-full border-0', {
-            'animate-pulse bg-grey-300': !isLoaded,
-          })}
-          allowFullScreen
-          onLoad={() => setLoaded(true)}
-        />
-      </div>
+      <div className="relative aspect-video w-full overflow-hidden rounded-lg">{renderEmbed()}</div>
 
       <div className="relative flex grow flex-col gap-1">
         <MLink
