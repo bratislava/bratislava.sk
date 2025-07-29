7e955612849b82e3e4c838d72410b2cb081b7577
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-07-29 13:51:35 +0200

    #1680 Log search queries to plausible (#1725)

---
+++ next/src/utils/useLogSearchQueryToPlausible.ts
@@ -0,0 +1,53 @@
+import { usePlausible } from 'next-plausible'
+import { useEffect, useState } from 'react'
+import { useDebounceValue } from 'usehooks-ts'
+
+import { useLocale } from '@/src/utils/useLocale'
+import { isProductionDeployment } from '@/src/utils/utils'
+
+export type PlausibleEvents = {
+  Vyhladavanie: { Query: string; Language: string; 'Search source'?: string }
+}
+
+/**
+ * Log debounced searchValue to Plausible
+ * Based on mestskakniznica.sk: https://github.com/bratislava/mestskakniznica.sk/blob/master/next/components/pages/SearchPage.tsx#L93
+ *
+ * @param query
+ * @param language
+ * @param source
+ */
+export const useLogSearchQueryToPlausible = ({
+  query,
+  source,
+}: {
+  query: string
+  source?: string
+}) => {
+  const plausible = usePlausible<PlausibleEvents>()
+  const locale = useLocale()
+
+  const [debouncedInputForPlausible] = useDebounceValue<string>(query, 2000)
+  const [lastInputForPlausible, setLastInputForPlausible] = useState<string>('')
+
+  useEffect(() => {
+    const sanitizedInput = debouncedInputForPlausible.toLowerCase().replaceAll(/\s+/g, ' ').trim()
+    if (
+      // TODO Remove isProd check when tested properly
+      !isProductionDeployment() &&
+      sanitizedInput.length > 2 &&
+      sanitizedInput !== lastInputForPlausible
+    ) {
+      plausible('Vyhladavanie', {
+        props: {
+          Query: sanitizedInput,
+          Language: locale,
+          'Search source': source,
+        },
+      })
+
+      setLastInputForPlausible(sanitizedInput)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedInputForPlausible])
+}
