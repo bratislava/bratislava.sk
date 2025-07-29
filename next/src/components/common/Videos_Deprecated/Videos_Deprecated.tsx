7731b3bd44007088b046ec259cad088df23262a5
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-07-29 10:54:43 +0200

    #1554 Improve Videos section layout

--- next/src/components/common/Videos_Deprecated/Videos_Deprecated.tsx
+++ next/src/components/common/Videos_Deprecated/Videos_Deprecated.tsx
@@ -1,25 +1,21 @@
 import { Typography } from '@bratislava/component-library'
-import React from 'react'
+import React, { useEffect } from 'react'
 
+import { AllowedVisibleCount } from '@/src/components/common/Carousel/Carousel'
 import ResponsiveCarousel from '@/src/components/common/Carousel/ResponsiveCarousel'
 import MLink from '@/src/components/common/MLink/MLink'
 import SectionHeader from '@/src/components/layouts/SectionHeader'
 import { VideoBlockFragment, VideosSectionFragment } from '@/src/services/graphql'
 import cn from '@/src/utils/cn'
-import { isPresent } from '@/src/utils/utils'
+import { isDefined } from '@/src/utils/isDefined'
 
 // TODO split into separate components
 
-const Video = ({
-  title,
-  speaker,
-  url,
-  size = 'default',
-}: VideoBlockFragment & { size?: 'default' | 'small' }) => {
+const VideoCard = ({ title, speaker, url }: VideoBlockFragment) => {
   const [embedUrl, setEmbedUrl] = React.useState('')
   const [isLoaded, setLoaded] = React.useState(false)
 
-  React.useEffect(() => {
+  useEffect(() => {
     const parseYoutubeUrl = async () => {
       if (url?.includes('fb.watch')) {
         const fembedUrl = `https://www.facebook.com/plugins/video.php?href=${url}`
@@ -41,26 +37,36 @@ const Video = ({
   }, [url])
 
   return (
-    <div className="mb-8 w-full lg:mb-0 xl:w-88">
-      <iframe
-        className={cn('w-full rounded-[5]', {
-          'animate-pulse bg-grey-300': !isLoaded,
-        })}
-        title={title ?? undefined}
-        // width={size === 'default' ? '350' : '280'}
-        height={size === 'default' ? '196' : '157'}
-        src={embedUrl}
-        allowFullScreen
-        onLoad={() => setLoaded(true)}
-      />
-      <MLink href={url ?? '#'} variant="underlineOnHover" target="_blank" rel="noreferrer">
-        <Typography variant="h5" as="h3" className="mt-8 cursor-pointer hover:underline">
-          {title}
-        </Typography>
-      </MLink>
-      <Typography variant="p-default" className="mt-5">
-        {speaker}
-      </Typography>
+    <div className="flex flex-col gap-4">
+      <div className="relative aspect-video w-full overflow-hidden rounded-lg">
+        <iframe
+          width="100%"
+          height="100%"
+          title={title ?? undefined}
+          src={embedUrl}
+          allow="accelerometer; encrypted-media; gyroscope; web-share"
+          className={cn('absolute top-0 left-0 size-full border-0', {
+            'animate-pulse bg-grey-300': !isLoaded,
+          })}
+          allowFullScreen
+          onLoad={() => setLoaded(true)}
+        />
+      </div>
+
+      <div className="relative flex grow flex-col gap-1">
+        <MLink
+          href={url ?? '#'}
+          variant="underlineOnHover"
+          target="_blank"
+          rel="noreferrer"
+          stretched
+        >
+          <Typography variant="h5" as="h3">
+            {title}
+          </Typography>
+        </MLink>
+        <Typography variant="p-default">{speaker}</Typography>
+      </div>
     </div>
   )
 }
@@ -74,27 +80,23 @@ export const Videos = ({ title, subtitle, videos }: VideosSectionFragment) => {
     return null
   }
 
+  const filteredVideos = videos.filter(isDefined) ?? []
+  const videosCount = filteredVideos.length
+
   return (
     <div>
       <div className="py-8 md:pt-0">
         <SectionHeader title={title} text={subtitle} />
       </div>
 
-      {/* Mobile */}
+      {/* Using carousel for simplicity, it'll "behave as carousel" on desktop only if there is more than 4 videos  */}
       <ResponsiveCarousel
-        className="lg:hidden"
         hasVerticalPadding={false}
-        items={videos.filter(isPresent).map((video) => (
-          <Video key={video.url} size="small" {...video} />
+        items={filteredVideos.map((video) => (
+          <VideoCard key={video.url} {...video} />
         ))}
+        desktop={videosCount > 0 && videosCount <= 4 ? (videosCount as AllowedVisibleCount) : 4}
       />
-
-      {/* Desktop */}
-      <div className="hidden gap-8 lg:grid lg:grid-cols-3">
-        {videos.filter(isPresent).map((video) => (
-          <Video key={video.url} {...video} />
-        ))}
-      </div>
     </div>
   )
 }
