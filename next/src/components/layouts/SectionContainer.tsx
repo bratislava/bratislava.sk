5dc7a586efa5529efaf65139032e40e8c9d06266
Adam <jakubicka.adam@gmail.com>
2025-07-30 15:13:58 +0200

    Setup container-page-wide css variable, add @container to SectionContainer and adjust showMoreLink wrapping in SectionHeader

--- next/src/components/layouts/SectionContainer.tsx
+++ next/src/components/layouts/SectionContainer.tsx
@@ -7,7 +7,10 @@ type SectionContainerProps = HTMLAttributes<HTMLDivElement>
 const SectionContainer = ({ className, children, ...rest }: SectionContainerProps) => (
   // data-section-container-... attributes serve to target these divs by tailwind from above
   <div data-section-container-outer className={cn('relative', className)} {...rest}>
-    <div data-section-container-inner className="mx-auto max-w-(--breakpoint-xl) px-4 lg:px-8">
+    <div
+      data-section-container-inner
+      className="@container mx-auto max-w-(--breakpoint-xl) px-4 lg:px-8"
+    >
       {children}
     </div>
   </div>
