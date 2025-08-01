567e63d478d429ddd106f8c6d528d79a4bfc054e
Adam <jakubicka.adam@gmail.com>
2025-08-01 10:23:13 +0200

    Add LinkCardsSection

---
+++ next/src/components/sections/LinkCardsSection.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import LinkCards from '@/src/components/common/LinkCards/LinkCards'
+import SectionContainer from '@/src/components/layouts/SectionContainer'
+import { LinkCardsSectionFragment } from '@/src/services/graphql'
+
+type LinkCardsSectionProps = {
+  section: LinkCardsSectionFragment
+}
+
+/**
+ * https://www.figma.com/design/17wbd0MDQcMW9NbXl6UPs8/DS--Component-library?node-id=18052-16738&t=WYqf0QbVs3yYwwAP-4
+ */
+
+const LinkCardsSection = ({ section }: LinkCardsSectionProps) => {
+  return (
+    <SectionContainer className="py-6 lg:py-12">
+      <LinkCards section={section} />
+    </SectionContainer>
+  )
+}
+
+export default LinkCardsSection
