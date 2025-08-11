4bf4be194fa88f8268fadc1078ddaaa8e9b3ec79
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-08-11 14:25:04 +0200

    Change STRAPI_URL to NEXT_PUBLIC_STRAPI_URL

--- next/src/services/graphql/gql.ts
+++ next/src/services/graphql/gql.ts
@@ -1,6 +1,6 @@
 import { getSdk } from '@/src/services/graphql/index'
 import { GraphQLClient } from 'graphql-request'
 
-const gql = new GraphQLClient(`${process.env.STRAPI_URL}/graphql`)
+const gql = new GraphQLClient(`${process.env.NEXT_PUBLIC_STRAPI_URL}/graphql`)
 export const client = getSdk(gql)
 
