ea13ccbfec5faab49a15a1c270c8288dacea40e2
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-08-11 09:42:35 +0200

    Simplify GraphQLClient setup

--- next/src/services/graphql/gql.ts
+++ next/src/services/graphql/gql.ts
@@ -1,25 +1,6 @@
 import { getSdk } from '@/src/services/graphql/index'
 import { GraphQLClient } from 'graphql-request'
-import getConfig from 'next/config'
 
-type serverTypeConfig = {
-  strapiUrl: string
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const { serverRuntimeConfig }: { serverRuntimeConfig: serverTypeConfig } = getConfig()
-
-// URL becomes full url to strapi on server, but just /graphql (for proxy) on client
-
-const protocol =
-  serverRuntimeConfig?.strapiUrl &&
-  (serverRuntimeConfig?.strapiUrl.startsWith('http://') ||
-    serverRuntimeConfig?.strapiUrl.startsWith('https://'))
-    ? ''
-    : 'http://'
-
-const urlWithProtocol = `${protocol}${serverRuntimeConfig.strapiUrl}`
-const gql = new GraphQLClient(
-  `${serverRuntimeConfig?.strapiUrl ? urlWithProtocol : window.location.origin}/graphql`,
-)
+const gql = new GraphQLClient(`${process.env.STRAPI_URL}/graphql`)
 export const client = getSdk(gql)
+
