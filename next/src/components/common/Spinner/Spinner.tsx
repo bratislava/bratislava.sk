cabcc1eec0ac5968f4efd3cd74404cefaafcb40d
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-08-13 17:15:36 +0200

    Fix spinner styling after border width change

--- next/src/components/common/Spinner/Spinner.tsx
+++ next/src/components/common/Spinner/Spinner.tsx
@@ -13,7 +13,7 @@ type SpinnerBase = {
 
 const Spinner = ({ size = 'md', className }: SpinnerBase) => {
   const style = cn('animate-spin rounded-[50%] border-solid border-grey-700 border-t-grey-300', {
-    'h-5 w-5 border border-t-2': size === 'sm',
+    'h-5 w-5 border-2 border-t-2': size === 'sm',
     'h-8 w-8 border-3 border-t-3': size === 'md',
     'h-12 w-12 border-4 border-t-4': size === 'lg',
   })
