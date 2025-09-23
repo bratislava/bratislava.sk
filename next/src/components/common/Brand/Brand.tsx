68e431ed4a5365060017359987308f3a33757ffa
Adam <jakubicka.adam@gmail.com>
2025-09-23 14:38:50 +0200

    Temporarily revert changes on Brand to solve merge conflicts

--- next/src/components/common/Brand/Brand.tsx
+++ next/src/components/common/Brand/Brand.tsx
@@ -17,8 +17,7 @@ const Brand = ({ className, url = '/', withTitle }: BrandProps) => {
   return (
     <div className={cn('flex', className)}>
       <MLink
-        // margin and padding added to show full focus ring
-        className="base-focus-ring -m-2 flex items-center gap-x-3 p-2 text-[#F23005] hover:opacity-80"
+        className="flex items-center gap-x-3 text-[#F23005] hover:opacity-80"
         href={url}
         aria-label={t('Brand.ariaLabel')}
       >
