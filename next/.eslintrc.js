9a3390af82dd7b9de4f748302c28cd295701c535
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-10-14 15:40:34 +0200

    Rearrange eslint config without changes

--- next/.eslintrc.js
+++ next/.eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     'plugin:@tanstack/eslint-plugin-query/recommended',
   ],
   rules: {
-    'react/react-in-jsx-scope': 'off',
     /** We use this a lot with isDefined and hasAttributes */
     'unicorn/no-array-callback-reference': 'off',
     /** Named export is easier to refactor automatically */
@@ -21,30 +20,38 @@ module.exports = {
     /** These are exceptions that we use with "__" */
     'no-underscore-dangle': [
       2,
-      { allow: ['__NEXT_DATA__', '__NEXT_LOADED_PAGES__', '__typename'] },
+      { allow: ['__NEXT_DATA__', '__NEXT_LOADED_PAGES__', '__typename'] }, // TODO add '__errors' ? - see Konto
     ],
+    /** Too many false positives, especially with i18n keys */
     'no-secrets/no-secrets': 'off',
     /** Doesn't work without changing our ts config */
-    'unicorn/prefer-spread': 'off',
+    'unicorn/prefer-spread': 'off', // TODO enable - see Kniznica, Marianum, OLO, remove also on Konto
     /** We specify default props in props decomposition */
     'react/require-default-props': 'off',
+    /** This is no longer needed since React 17 */
+    'react/react-in-jsx-scope': 'off',
     /** Next Link does not need href in <a> tag */
     'jsx-a11y/anchor-is-valid': 'off',
     /** Do not work in our case */
-    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off', // TODO enable
     'lodash/prefer-noop': 'off',
     'jsx-a11y/img-redundant-alt': 'warn',
-    '@next/next/no-img-element': 'off',
-    'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
+    '@next/next/no-img-element': 'off', // TODO enable
+    'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }], // This works the best
     // https://github.com/jsx-eslint/eslint-plugin-react/issues/2584#issuecomment-1191175244
     'react/jsx-no-useless-fragment': [2, { allowExpressions: true }],
+
+    // Many false positives, if we add email/phone we probably want to have it anyway
     'pii/no-phone-number': 'off',
     'pii/no-email': 'off',
+    // Good rationale, but doesn't work for us
     'const-case/uppercase': 'off',
     'sonarjs/no-duplicate-string': 'off',
+    'unicorn/expiring-todo-comments': 'off',
+
+    // TODO revisit, it appeared after refactor to using @/* path alias
     'import/extensions': 'off',
     'react/display-name': 'off',
-    'unicorn/expiring-todo-comments': 'off',
 
     /* Formatting rules */
     'prettier/prettier': ['error', { endOfLine: 'auto' }], // TODO revisit, prettier should not be run by eslint
