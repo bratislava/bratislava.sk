04d4363859c93f52de78e9a15433156724c895bb
Adam <jakubicka.adam@gmail.com>
2025-12-02 16:49:54 +0100

    Implement starz submenu pages in next

--- next/src/components/page-contents/GeneralPageContent.tsx
+++ next/src/components/page-contents/GeneralPageContent.tsx
@@ -35,8 +35,8 @@ const GeneralPageContent = ({ page }: GeneralPageProps) => {
   return (
     <>
       {/* Starz submenu - TODO make it more generic in future when more organizations need it */}
-      {starzAdminGroup?.landingPage ? (
-        <StarzSubmenu landingPage={starzAdminGroup.landingPage} className="max-lg:hidden" />
+      {starzAdminGroup ? (
+        <StarzSubmenu adminGroup={starzAdminGroup} className="max-lg:hidden" />
       ) : null}
 
       {/* Header */}
