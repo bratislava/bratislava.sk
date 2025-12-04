5b396061bec0bc3ab3f7189f193f53554382dcf6
Adam <jakubicka.adam@gmail.com>
2025-12-04 23:33:44 +0100

    Add bratislava logo svg asset in BW

--- next/src/assets/images/index.ts
+++ next/src/assets/images/index.ts
@@ -1,3 +1,4 @@
 export { default as BratislavaLogoSvg } from './bratislava-logo.svg'
+export { default as BratislavaLogoBWSvg } from './bratislava-logo-BW.svg'
 export { default as EuFlagSvg } from './eu-flag.svg'
 export { default as StarzLogoSvg } from './starz-logo.svg'
