b217cf4f76fe118b1c59a30d03c9749d7331590a
Bardúnová Barbora <barbora.bardunova@bratislava.sk>
2026-02-02 16:27:55 +0100

    Add ColumnsListSection component

--- next/src/components/sections/ColumnsListSection.tsx
+++ next/src/components/sections/ColumnsListSection.tsx
@@ -1,109 +1,67 @@
 import React from 'react'
 
-import ColumnsSectionItem from '@/src/components/common/ColumnsSectionItem/ColumnsSectionItem'
-import Slider from '@/src/components/common/Slider/Slider'
 import SectionContainer from '@/src/components/layouts/SectionContainer'
 import SectionHeader from '@/src/components/layouts/SectionHeader'
 import { ColumnsListSectionFragment } from '@/src/services/graphql'
-import cn from '@/src/utils/cn'
 import { generateImageSizes } from '@/src/utils/generateImageSizes'
 import { isDefined } from '@/src/utils/isDefined'
 import StrapiImage from '@/src/components/common/Image/StrapiImage'
+import Markdown from '@/src/components/formatting/Markdown/Markdown'
 
 type Props = {
   section: ColumnsListSectionFragment
 }
 
 /**
- * Figma: https://www.figma.com/design/17wbd0MDQcMW9NbXl6UPs8/DS--Component-library?node-id=5865-16442&t=TcrEN6rQPU300Ipo-0
+ * Figma: https://www.figma.com/design/17wbd0MDQcMW9NbXl6UPs8/DS--Component-library?node-id=21143-18394&t=MUzJWPA0GjZQj44Y-4
  */
 
 const ColumnsSection = ({ section }: Props) => {
   const { title, text, leftColumn, rightColumn } = section
 
-  const filteredLeftColumn = leftColumn?.filter(isDefined) ?? []
-  const filteredRightColumn = rightColumn?.filter(isDefined) ?? []
-
-  console.log(filteredLeftColumn)
-
   return (
-    <SectionContainer>
-      <div className="flex flex-col gap-6 lg:gap-12">
-        <SectionHeader title={title} text={text} isCentered />
-        {/* Screen: Desktop */}
-        <ul className="flex w-full flex-wrap items-stretch justify-center gap-4 gap-y-6 max-lg:hidden lg:gap-8 lg:gap-y-14">
-          {filteredLeftColumn.map((item, index) => {
-            return (
-              <li
-                key={index}
-                className={cn('w-[33%] max-w-[50%]', {
-                  // Control number of items per row based on their count so it looks good
-                  'w-full max-w-[100%] sm:max-w-full': filteredLeftColumn.length === 1,
-                  'w-[40%] max-w-[100%] sm:max-w-full': filteredLeftColumn.length === 2,
-                  'sm:w-[25%]': filteredLeftColumn.length % 3 === 0,
-                  'sm:w-[20%] sm:max-w-[22%]':
-                    filteredLeftColumn.length > 2 && filteredLeftColumn.length % 3 === 1,
-                  'sm:w-[25%] sm:max-w-[33%]':
-                    filteredLeftColumn.length > 2 && filteredLeftColumn.length % 3 === 2,
-                })}
-              >
-                {item.icon && (
-                  <StrapiImage
-                    alt=""
-                    image={item.icon}
-                    sizes={generateImageSizes({
-                      // default: '100vw',
-                      default: '24px',
-                      // Note that this doesn't match the logic above for 100%, but it's okay, because it's just a simple optimization
-                      // sm: filteredLeftColumn.length === 2 ? '50vw' : '33vw',
-                    })}
-                    // pointer-events must be disabled to drag-events work properly in Slider
-                    // className={cn('pointer-events-none', {
-                    //   'object-contain': isCircleBackgroundVariant,
-                    // })}
-                    // fill={isCircleBackgroundVariant}
-                    className="w-[24px]"
-                  />
-                )}
-                {item.content}
-              </li>
-            )
-          })}
-        </ul>
+    <SectionContainer className="w-full">
+      <div className="flex flex-col gap-6 lg:gap-8">
+        <SectionHeader title={title} text={text} />
 
-        {/* Screen: Mobile */}
-        {/* {responsiveLayout === 'columnsSection_responsiveLayout_slider' ? (
-          <Slider
-            // aria-labelledby={titleId} // TODO for better accessibility
-            items={filteredColumns.map((item, index) => {
+        <div className="grid gap-3 lg:grid-cols-2 lg:gap-8">
+          {[leftColumn, rightColumn]
+            .filter((column) => column?.length)
+            .map((column, columnIndex) => {
               return (
-                <ColumnsSectionItem
-                  key={index}
-                  {...item}
-                  imageVariant={imageVariant}
-                  imageSizes={generateImageSizes({ default: '100vw' })}
-                  className="w-full"
-                />
+                // eslint-disable-next-line react/no-array-index-key
+                <div key={columnIndex} className="flex flex-col gap-3 lg:gap-4">
+                  {column
+                    ?.map((columnItem, columnItemIndex) => {
+                      if (!columnItem) {
+                        return null
+                      }
+
+                      const icon = columnItem.icon
+
+                      return (
+                        // eslint-disable-next-line react/no-array-index-key
+                        <div key={columnItemIndex} className="flex gap-4">
+                          {icon ? (
+                            <div className="relative size-6 shrink-0">
+                              <StrapiImage
+                                image={icon}
+                                alt=""
+                                fill
+                                sizes={generateImageSizes({ default: '50vw', lg: '10vw' })}
+                                className="object-contain"
+                              />
+                            </div>
+                          ) : null}
+                          <Markdown content={columnItem.content} />
+                        </div>
+                      )
+                    })
+                    .filter(isDefined)}
+                </div>
               )
             })}
-            className="lg:hidden"
-          />
-        ) : (
-          <ul className="flex flex-col gap-8 lg:hidden">
-            {filteredColumns.map((item, index) => {
-              return (
-                <li key={index}>
-                  <ColumnsSectionItem
-                    {...item}
-                    imageVariant={imageVariant}
-                    imageSizes={generateImageSizes({ default: '100vw' })}
-                    className="w-full"
-                  />
-                </li>
-              )
-            })}
-          </ul>
-        )} */}
+        </div>
       </div>
     </SectionContainer>
   )
