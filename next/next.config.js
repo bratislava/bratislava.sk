74fa6bac98c69a79b1d969d9f1e9686618fadfd9
Adam <jakubicka.adam@gmail.com>
2026-02-08 17:37:22 +0100

    Revert "Use turbopack - WIP"
    This reverts commit 8df4ce7c5deaa9e571f6abaa1eb05422c596cdb6.

--- next/next.config.js
+++ next/next.config.js
@@ -1,5 +1,6 @@
 const { withPlausibleProxy } = require('next-plausible')
 const i18nextConfig = require('./next-i18next.config.js')
+const svgoConfig = require('./svgo.config.js')
 
 /**
  * @type {import('next').NextConfig}
@@ -202,36 +203,34 @@ const nextConfig = {
       },
     ]
   },
-  // Turbopack configuration for SVG handling
-  // Docs: https://nextjs.org/docs/app/api-reference/next-config-js/turbopack
-  // SVGs are converted to React components by default using @svgr/webpack
-  // SVGs with ?url query parameter are handled as file URLs automatically by Turbopack
-  turbopack: {
-    rules: {
-      '*.svg': {
-        loaders: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              svgo: true,
-              svgoConfig: {
-                plugins: [
-                  {
-                    name: 'preset-default',
-                    params: {
-                      overrides: {
-                        removeViewBox: false,
-                      },
-                    },
-                  },
-                ],
-              },
-            },
-          },
-        ],
-        as: '*.js',
+  // Docs: https://react-svgr.com/docs/next/
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
       },
-    },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: {
+          loader: '@svgr/webpack',
+          options: { svgoConfig },
+        },
+      },
+    )
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
   },
 }
 
