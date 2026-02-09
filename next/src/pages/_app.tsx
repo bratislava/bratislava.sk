2c05e3b391492a16f85ac086c32fc4c18ce782ae
Adam <jakubicka.adam@gmail.com>
2026-02-09 16:36:18 +0100

    Revert "2086 investigate resource spike in production - upgrade next to v16 (#2089)"
    This reverts commit a8338ee0fa30051480f113cb0230073ebba6e61c.

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -6,8 +6,10 @@ import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import { appWithTranslation } from 'next-i18next'
 import PlausibleProvider from 'next-plausible'
+import { NextAdapter } from 'next-query-params'
 import { NuqsAdapter } from 'nuqs/adapters/next/pages'
 import { OverlayProvider } from 'react-aria'
+import { QueryParamProvider } from 'use-query-params'
 
 import { NavMenuContextProvider } from '@/src/components/common/NavBar/NavMenu/navMenuContext'
 import BAI18nProvider from '@/src/components/providers/BAI18nProvider'
@@ -44,27 +46,29 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <PlausibleProvider
         domain={isProd ? 'bratislava.sk' : 'testing.bratislava.sk'}
         taggedEvents
-      // uncomment for local testing, needs to be run with `build` and `start`
-      // trackLocalhost
+        // uncomment for local testing, needs to be run with `build` and `start`
+        // trackLocalhost
       >
         <BAQueryClientProvider>
-          <NuqsAdapter>
-            <BAI18nProvider>
-              <OverlayProvider>
-                <NavMenuContextProvider>
-                  {/* This root div is used for locked body when mobile menu ist open, see MobileNavMenu component */}
-                  <div id="root">
-                    <Component {...pageProps} />
-                  </div>
-                  <GoogleTagManager
-                    gtmId={process.env.NEXT_PUBLIC_GTM_ID ?? ''}
-                    auth={process.env.NEXT_PUBLIC_GTM_AUTH ?? ''}
-                    preview={process.env.NEXT_PUBLIC_GTM_PREVIEW ?? ''}
-                  />
-                </NavMenuContextProvider>
-              </OverlayProvider>
-            </BAI18nProvider>
-          </NuqsAdapter>
+          <QueryParamProvider adapter={NextAdapter}>
+            <NuqsAdapter>
+              <BAI18nProvider>
+                <OverlayProvider>
+                  <NavMenuContextProvider>
+                    {/* This root div is used for locked body when mobile menu ist open, see MobileNavMenu component */}
+                    <div id="root">
+                      <Component {...pageProps} />
+                    </div>
+                    <GoogleTagManager
+                      gtmId={process.env.NEXT_PUBLIC_GTM_ID ?? ''}
+                      auth={process.env.NEXT_PUBLIC_GTM_AUTH ?? ''}
+                      preview={process.env.NEXT_PUBLIC_GTM_PREVIEW ?? ''}
+                    />
+                  </NavMenuContextProvider>
+                </OverlayProvider>
+              </BAI18nProvider>
+            </NuqsAdapter>
+          </QueryParamProvider>
         </BAQueryClientProvider>
       </PlausibleProvider>
     </>
