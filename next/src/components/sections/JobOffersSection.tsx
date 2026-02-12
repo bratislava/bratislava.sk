d9069dc3f57c33e5f5859a1980ff94c9d6bc3b6b
Bardúnová Barbora <barbora.bardunova@bratislava.sk>
2026-02-12 13:53:43 +0100

    Add frontend pagination to Nalgoo job offers

--- next/src/components/sections/JobOffersSection.tsx
+++ next/src/components/sections/JobOffersSection.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 
 import JobOfferRowCard from '@/src/components/cards/JobOfferRowCard'
 import HorizontalDivider from '@/src/components/common/Divider/HorizontalDivider'
+import PaginationWithInput from '@/src/components/common/Pagination/PaginationWithInput'
 import Spinner from '@/src/components/common/Spinner/Spinner'
 import SectionContainer from '@/src/components/layouts/SectionContainer'
 import SectionHeader from '@/src/components/layouts/SectionHeader'
@@ -27,6 +28,22 @@ const JobOffersSection = ({ section }: JobOffersSectionProps) => {
     staleTime: Infinity,
   })
 
+  const [pagination, setPagination] = useState({
+    currentPage: 1,
+    pageSize: 12,
+    totalItems: 0,
+  })
+
+  useEffect(() => {
+    if (data?.length)
+      setPagination((prev) => ({ ...prev, currentPage: 1, totalItems: data.length }))
+  }, [data])
+
+  const paginatedData = data?.slice(
+    (pagination.currentPage - 1) * pagination.pageSize,
+    pagination.currentPage * pagination.pageSize,
+  )
+
   return (
     <SectionContainer>
       {isPending ? (
@@ -37,7 +54,7 @@ const JobOffersSection = ({ section }: JobOffersSectionProps) => {
         <div className="flex w-full flex-col gap-6">
           <SectionHeader title={title} text={text} titleLevel={titleLevel} />
           <ul className="flex flex-col rounded-lg border py-2">
-            {data.map((jobOffer, index) => {
+            {paginatedData?.map((jobOffer, index) => {
               return (
                 <Fragment key={jobOffer.id}>
                   {index > 0 ? <HorizontalDivider className="mx-4 lg:mx-6" asListItem /> : null}
@@ -48,6 +65,20 @@ const JobOffersSection = ({ section }: JobOffersSectionProps) => {
               )
             })}
           </ul>
+
+          <div className="self-center">
+            <PaginationWithInput
+              currentPage={pagination.currentPage}
+              totalCount={
+                pagination.totalItems > 0
+                  ? Math.ceil(pagination.totalItems / pagination.pageSize)
+                  : 1
+              }
+              onPageChange={(newPage) => {
+                setPagination((prev) => ({ ...prev, currentPage: newPage }))
+              }}
+            />
+          </div>
         </div>
       )}
     </SectionContainer>
