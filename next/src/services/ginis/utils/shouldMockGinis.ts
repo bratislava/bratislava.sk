fad8a8e3fa57481c35843081a64046bb30fa4aee
Bardúnová Barbora <barbora.bardunova@bratislava.sk>
2026-02-19 12:47:44 +0100

    Revert ginis mocking

--- next/src/services/ginis/utils/shouldMockGinis.ts
+++ next/src/services/ginis/utils/shouldMockGinis.ts
@@ -1,8 +1,6 @@
 // GINIS is accessible only from internal network
 // if developing from internal network, change here
 export const shouldMockGinis = () => {
-  return false
-
   return (
     process.env.NODE_ENV === 'development' ||
     process.env.NODE_ENV === 'test' ||
