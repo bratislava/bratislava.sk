27d62ecc30a5e80fbc6dc678310d79ba91b9f695
Bardúnová Barbora <barbora.bardunova@bratislava.sk>
2026-03-30 15:17:13 +0200

    Create sitemap with routes from strapi

--- next/next-sitemap.config.js
+++ next/next-sitemap.config.js
@@ -1,20 +1,56 @@
 const { client } = require('./dist/src/services/graphql/gql')
-
-const getDynamicPaths = async () => {
-  const { pages } = await client.PagesStaticPaths({ locale: 'sk' })
-  return pages.map((page) => ({
-    loc: `/${page.path}`,
-  }))
-}
+const i18nextConfig = require('./next-i18next.config.js')
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'bratislava.sk',
+  siteUrl: process.env.SITE_URL || 'bratislava.sk', // todo fix env variable
   generateRobotsTxt: false,
   changefreq: 'weekly',
-  sitemapSize: 5000,
+  sitemapSize: 7000,
   additionalPaths: async (config) => {
-    const paths = await getDynamicPaths()
+    const { locales } = i18nextConfig.i18n
+
+    const fetchArticlePaths = async () => {
+      const results = await Promise.all(
+        locales.map(async (locale) => {
+          const { articles } = await client.Articles({ limit: -1, locale })
+          return articles.map((article) => ({
+            loc: `${locale !== 'sk' ? `/${locale}` : ''}/spravy/${article.slug}`,
+          }))
+        }),
+      )
+      return results.flat()
+    }
+
+    const fetchInbaReleasePaths = async () => {
+      const { inbaReleases } = await client.InbaReleasesStaticPaths({ limit: -1 })
+      return inbaReleases.map((release) => ({ loc: `/inba/vydania/${release.slug}` }))
+    }
+
+    const fetchRegulationPaths = async () => {
+      const { regulations } = await client.RegulationsStaticPaths({ limit: -1 })
+      return regulations.map((regulation) => ({ loc: `/vzn/${regulation.slug}` }))
+    }
+
+    const fetchOtherPaths = async () => {
+      const results = await Promise.all(
+        locales.map(async (locale) => {
+          const { pages } = await client.PagesStaticPaths({ limit: -1, locale })
+          return pages.map((page) => ({
+            loc: `${locale !== 'sk' ? `/${locale}` : ''}/${page.path}`,
+          }))
+        }),
+      )
+      return results.flat()
+    }
+
+    const [articlePaths, inbaReleasePaths, regulationPaths, otherPaths] = await Promise.all([
+      fetchArticlePaths(),
+      fetchInbaReleasePaths(),
+      fetchRegulationPaths(),
+      fetchOtherPaths(),
+    ])
+    const paths = [...articlePaths, ...inbaReleasePaths, ...regulationPaths, ...otherPaths]
 
     return paths.map((path) => ({
       loc: path.loc,
