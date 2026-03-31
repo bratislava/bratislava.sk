d00b8dee391fb90ea7f9ff3d39b8d2252b518c4e
Bardúnová Barbora <barbora.bardunova@bratislava.sk>
2026-03-31 15:09:00 +0200

    Remove adminGroupId from lifecycles.ts

--- strapi/src/api/article/content-types/article/lifecycles.ts
+++ strapi/src/api/article/content-types/article/lifecycles.ts
@@ -1,4 +1,3 @@
-
 /*
  *
  * ============================================================
@@ -28,50 +27,50 @@
 
 // import { Attribute } from '@strapi/types'
 // import { Event } from '@strapi/database/dist/lifecycles'
-// 
+//
 // type LifecycleEvent<T> = Event & {
 //   result?: T
 // }
 // type ArticleLifecycleEvent = LifecycleEvent<Attribute.GetValues<'api::article.article'>>
-// 
+//
 // /**
 //  * TODO
 //  * - handle hardcoded strings better - maybe som config object is warranted
-//  * - we want to connect to a specific adminGroupId value, and also to an admin role
+//  * - we want to connect to a specific slug value, and also to an admin role
 //  * - make this more generic to allow more organizations in the future
 //  * - consider extracting some logic to avoid code duplicity across different content types
 //  */
-// 
-// type AdminGroupId = 'starz'
-// 
-// const getAdminGroup = async ({ adminGroupId }: { adminGroupId: AdminGroupId }) => {
+//
+// type AdminGroupSlug = 'starz'
+//
+// const getAdminGroup = async ({ slug }: { slug: AdminGroupSlug }) => {
 //   try {
 //     const adminGroups = await strapi.entityService.findMany('api::admin-group.admin-group', {
-//       filters: { adminGroupId },
+//       filters: { slug },
 //     })
-// 
+//
 //     if (!adminGroups?.length)
-//       throw new Error(`found no adminGroups for adminGroupId='${adminGroupId}'`)
-// 
-//     // adminGroupId has unique values, so we get at most one result
+//       throw new Error(`found no adminGroups for slug='${slug}'`)
+//
+//     // slug has unique values, so we get at most one result
 //     return adminGroups[0]
 //   } catch (error) {
 //     console.log('getAdminGroup failed with error', error)
 //   }
 // }
-// 
+//
 // export default {
 //   async afterCreate(event: ArticleLifecycleEvent) {
 //     const { result: article } = event
-// 
-//     const starzAdminGroup = await getAdminGroup({ adminGroupId: 'starz' })
-// 
+//
+//     const starzAdminGroup = await getAdminGroup({ slug: 'starz' })
+//
 //     if (!starzAdminGroup) return
-// 
+//
 //     const articleCreator = await strapi.entityService.findOne('admin::user', article.createdBy.id, {
 //       populate: 'roles',
 //     })
-// 
+//
 //     // Return if creator role doesn't include starz
 //     // TODO consider a more robust validation, this is just a quick simple solution for a specific case
 //     if (
@@ -80,15 +79,15 @@
 //       })
 //     )
 //       return
-// 
+//
 //     const existingGroupIds = article.adminGroups ? article.adminGroups.map((group) => group.id) : []
-// 
+//
 //     // Don't add starz group ID if already present
 //     // TODO check if this is even needed - maybe appending an already existing id to the array will do no harm
 //     if (existingGroupIds.includes(starzAdminGroup.id)) {
 //       return
 //     }
-// 
+//
 //     // Update the article with the new adminGroups relation
 //     await strapi.entityService.update('api::article.article', article.id, {
 //       data: {
@@ -97,4 +96,4 @@
 //     })
 //   },
 // }
-// 
+//
