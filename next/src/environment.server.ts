dd74dc48e8aa2158835a697562fd16d2c7d98ffd
Bardúnová Barbora <barbora.bardunova@bratislava.sk>
2026-04-07 13:11:17 +0200

    Fix failing build

--- next/src/environment.server.ts
+++ next/src/environment.server.ts
@@ -12,7 +12,6 @@ function assertEnv<T>(variable: string, value: T) {
 export const serverEnvironment = {
   ginisHostUde: assertEnv('GINIS_HOST_UDE', process.env.GINIS_HOST_UDE),
   msalScope: assertEnv('MSAL_SCOPE', process.env.MSAL_SCOPE),
-  nalgooApiKey: assertEnv('NALGOO_API_KEY', process.env.NALGOO_API_KEY),
   starzEcomailApiKey: assertEnv('STARZ_ECOMAIL_API_KEY', process.env.STARZ_ECOMAIL_API_KEY),
   revalidateSecretToken: assertEnv('REVALIDATE_SECRET_TOKEN', process.env.REVALIDATE_SECRET_TOKEN),
 }
