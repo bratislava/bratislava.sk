56eaf6ce31bc469dd1610d985152847cb5c1851c
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2026-06-03 11:23:13 +0200

    Add table of contents and render link using Links section

--- next/src/components/page-contents/UrbanStudyPageContent.tsx
+++ next/src/components/page-contents/UrbanStudyPageContent.tsx
@@ -1,19 +1,21 @@
 import { Typography } from '@bratislava/component-library'
+import slugify from '@sindresorhus/slugify'
 import { useTranslation } from 'next-i18next'
 import { Fragment } from 'react'
 
 import FileRowCard from '@/src/components/cards/FileRowCard'
 import HorizontalDivider from '@/src/components/common/Divider/HorizontalDivider'
 import Icon from '@/src/components/common/Icon/Icon'
-import MLink from '@/src/components/common/MLink/MLink'
+import Links from '@/src/components/common/Links/Links'
+import TableOfContents from '@/src/components/common/TableOfContents/TableOfContents'
+import { TABLE_OF_CONTENTS_HEADING_ATTRIBUTE } from '@/src/components/common/TableOfContents/useHeadings'
 import Markdown from '@/src/components/formatting/Markdown/Markdown'
 import SectionContainer from '@/src/components/layouts/SectionContainer'
-import { UrbanStudyEntityFragment } from '@/src/services/graphql'
+import { LinksSectionFragment, UrbanStudyEntityFragment } from '@/src/services/graphql'
 import cn from '@/src/utils/cn'
 import { formatDate } from '@/src/utils/formatDate'
 import { formatFileExtension } from '@/src/utils/formatFileExtension'
 import { formatFileSize } from '@/src/utils/formatFileSize'
-import { getLinkProps } from '@/src/utils/getLinkProps'
 import { isDefined } from '@/src/utils/isDefined'
 
 type Props = {
@@ -98,62 +100,68 @@ const UrbanStudyPageContent = ({ urbanStudy }: Props) => {
 
       <SectionContainer className="py-6 lg:py-12">
         <div className="flex max-w-200 flex-col gap-8 lg:gap-10">
-          {body ? <Markdown content={body} /> : null}
+          <TableOfContents />
 
-          {fileGroups.map((group) => (
-            <div className="flex flex-col gap-4" key={group.label}>
-              <Typography variant="h2">{group.label}</Typography>
-              <ul className="flex flex-col rounded-lg border py-2">
-                {group.files.map((file, index) => (
-                  <Fragment key={file.documentId}>
-                    {index > 0 ? <HorizontalDivider asListItem className="mx-4 lg:mx-6" /> : null}
-                    <li className="w-full">
-                      <FileRowCard
-                        title={file.name}
-                        downloadLink={file.url}
-                        format={formatFileExtension(file.ext) ?? undefined}
-                        size={formatFileSize(file.size, locale)}
-                        uploadDate={formatDate(file.createdAt)}
-                      />
-                    </li>
-                  </Fragment>
-                ))}
-              </ul>
-            </div>
-          ))}
+          <div {...TABLE_OF_CONTENTS_HEADING_ATTRIBUTE} className="flex flex-col gap-8 lg:gap-10">
+            {body ? <Markdown content={body} /> : null}
 
-          {detailItems.length > 0 ? (
-            <>
-              <HorizontalDivider />
-              <div className="flex flex-col gap-4">
-                <Typography variant="h2">{t('UrbanStudyPageContent.detailsTitle')}</Typography>
-                <div className="flex flex-col gap-4">
-                  {detailItems.map(({ label, value }, index) => (
-                    <div className="flex flex-col flex-wrap sm:flex-row sm:gap-x-6" key={index}>
-                      <Typography className="basis-1/3">{`${label}:`}</Typography>
-                      <Typography>{value}</Typography>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </>
-          ) : null}
-
-          {filteredLinks.length > 0 ? (
-            <>
-              <HorizontalDivider />
-              <div className="flex flex-col gap-4">
-                <Typography variant="h2">{t('UrbanStudyPageContent.linksTitle')}</Typography>
-                <ul className="flex flex-col gap-2">
-                  {filteredLinks.map((link, index) => (
-                    <li key={index}>
-                      <MLink variant="underlined" {...getLinkProps(link)} />
-                    </li>
+            {fileGroups.map((group) => (
+              <div className="flex flex-col gap-4" key={group.label}>
+                <Typography variant="h2" id={slugify(group.label)}>
+                  {group.label}
+                </Typography>
+                <ul className="flex flex-col rounded-lg border py-2">
+                  {group.files.map((file, index) => (
+                    <Fragment key={file.documentId}>
+                      {index > 0 ? (
+                        <HorizontalDivider asListItem className="mx-4 lg:mx-6" />
+                      ) : null}
+                      <li className="w-full">
+                        <FileRowCard
+                          title={file.name}
+                          downloadLink={file.url}
+                          format={formatFileExtension(file.ext) ?? undefined}
+                          size={formatFileSize(file.size, locale)}
+                          uploadDate={formatDate(file.createdAt)}
+                        />
+                      </li>
+                    </Fragment>
                   ))}
                 </ul>
               </div>
-            </>
-          ) : null}
+            ))}
+
+            {detailItems.length > 0 ? (
+              <>
+                <HorizontalDivider />
+                <div className="flex flex-col gap-4">
+                  <Typography variant="h2" id={slugify(t('UrbanStudyPageContent.detailsTitle'))}>
+                    {t('UrbanStudyPageContent.detailsTitle')}
+                  </Typography>
+                  <div className="flex flex-col gap-4">
+                    {detailItems.map(({ label, value }, index) => (
+                      <div className="flex flex-col flex-wrap sm:flex-row sm:gap-x-6" key={index}>
+                        <Typography className="basis-1/3">{`${label}:`}</Typography>
+                        <Typography>{value}</Typography>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </>
+            ) : null}
+
+            {filteredLinks.length > 0 ? (
+              <>
+                <HorizontalDivider />
+                <Links
+                  title={t('UrbanStudyPageContent.linksTitle')}
+                  // Urban study links use the CommonLink fragment, which is structurally compatible
+                  // with getLinkProps used inside Links (typed for PageLink).
+                  pageLinks={links as LinksSectionFragment['pageLinks']}
+                />
+              </>
+            ) : null}
+          </div>
         </div>
       </SectionContainer>
     </>
