e36a146fcd43e15b0aa9c59e6dfdda4de4238849
Adam Jakubička <139510883+Ty-ci@users.noreply.github.com>
2026-07-08 14:42:11 +0200

    #2386 spike strapi logs out user after a short time (#2390)
    * Allow markdown render in ColumnsSection items
    
    * Increase idle session lifespan for strapi admin panel to 4 hours
    
    * Revert "Allow markdown render in ColumnsSection items"
    
    This reverts commit d83e2c75ac3cd89aada5488eaa7061644d3e14dd.

--- strapi/config/admin.ts
+++ strapi/config/admin.ts
@@ -1,6 +1,14 @@
+/**
+ * Docs: https://docs.strapi.io/cms/configurations/admin-panel
+ */
+
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    sessions: {
+      // How long an inactive session survives before forcing re-login.
+      idleSessionLifespan: 4 * 60 * 60, // 4 hours
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
