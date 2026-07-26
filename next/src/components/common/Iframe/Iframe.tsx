f0fbf880adcd8934580c0ea559232b912fa02f4e
Ty-ci <teachy.jaho@gmail.com>
2026-07-26 22:52:11 +0200

    Trim incoming url string in Iframe

--- next/src/components/common/Iframe/Iframe.tsx
+++ next/src/components/common/Iframe/Iframe.tsx
@@ -47,6 +47,8 @@ const Iframe = ({
 
   const allowDownloads = allowDownloadsWhitelist.some((allowedUrl) => url.startsWith(allowedUrl))
 
+  const sanitizedUrl = url.trim()
+
   return (
     <div className="flex flex-col gap-4 lg:gap-6">
       <SectionHeader title={title} titleLevel={titleLevel} text={text} />
@@ -54,7 +56,7 @@ const Iframe = ({
         license="GPLv3"
         title={iframeTitle ?? undefined}
         forwardRef={iframeRef}
-        src={url}
+        src={sanitizedUrl}
         className={cn('w-full', {
           border: hasBorder,
         })}
