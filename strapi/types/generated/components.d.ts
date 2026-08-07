4822d9d069d0c6fde1c8436d45d45857e3815c99
Petr Šigut <64728606+shegood@users.noreply.github.com>
2026-08-07 16:20:39 +0200

    Bugfix: Return back the documents field in Documents section view (#2447)
    * Bugfix: Return back the documents field in Documents section view
    
    * make allowCollapsingDocuments conditional

--- strapi/types/generated/components.d.ts
+++ strapi/types/generated/components.d.ts
@@ -371,7 +371,7 @@ export interface BlocksTopServicesItem extends Struct.ComponentSchema {
         'organizacna_struktura',
         'uradne_hodiny',
         'pracovne_prilezitosti',
-        'prenajom_priestorov'
+        'prenajom_priestorov',
       ]
     > &
       Schema.Attribute.Required &
@@ -475,7 +475,7 @@ export interface MenuMenuItem extends Struct.ComponentSchema {
         'zp_vystavba_03',
         'socialna_pomoc_04',
         'vzdelavanie_05',
-        'kultura_06'
+        'kultura_06',
       ]
     > &
       Schema.Attribute.Required &
@@ -547,7 +547,7 @@ export interface MenuMenuSection extends Struct.ComponentSchema {
         'sluzby_06',
         'koncepcia_06',
         'komunity_06',
-        'covid_06'
+        'covid_06',
       ]
     > &
       Schema.Attribute.Required &
@@ -694,7 +694,7 @@ export interface SectionsColumns extends Struct.ComponentSchema {
     imageVariant: Schema.Attribute.Enumeration<
       [
         'columnsSection.imageVariant.withCircleBackground',
-        'columnsSection.imageVariant.imageOriginalSize'
+        'columnsSection.imageVariant.imageOriginalSize',
       ]
     > &
       Schema.Attribute.Required &
@@ -790,7 +790,7 @@ export interface SectionsDivider extends Struct.ComponentSchema {
         'budovy_04_full_width',
         'vzdelavanie',
         'skola',
-        'divadlo'
+        'divadlo',
       ]
     >
   }
