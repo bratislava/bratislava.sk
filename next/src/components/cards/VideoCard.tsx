3f368ce9861c80c153c2b91e49c82ce61c718181
Ty-ci <teachy.jaho@gmail.com>
2026-08-10 16:41:22 +0200

    Implement CookieConsentGate in relevant components

--- next/src/components/cards/VideoCard.tsx
+++ next/src/components/cards/VideoCard.tsx
@@ -3,10 +3,11 @@ import { useTranslation } from 'next-i18next'
 import { useState } from 'react'
 
 import { CardTitleLevel } from '@/src/components/cards/getCardTitleLevel'
+import CookieConsentGate from '@/src/components/common/CookieConsentGate/CookieConsentGate'
 import MLink from '@/src/components/common/MLink/MLink'
 import { VideoBlockFragment } from '@/src/services/graphql'
 import cn from '@/src/utils/cn'
-import { getVideoIframeSrc } from '@/src/utils/videoEmbedUtils'
+import { detectVideoPlatform, getVideoIframeSrc } from '@/src/utils/videoEmbedUtils'
 
 /**
  * TODO figma link
@@ -22,29 +23,32 @@ const VideoCard = ({ title, speaker, url: untrimmedUrl, cardTitleLevel = 'h3' }:
 
   const url = untrimmedUrl.trim()
   const src = getVideoIframeSrc(url)
+  const platform = detectVideoPlatform(url)
 
   return (
     <div className="flex flex-col gap-4 rounded-lg wrapper-focus-ring">
-      <div className="relative aspect-video w-full overflow-hidden rounded-lg">
-        {src ? (
-          <iframe
-            src={src}
-            title={title ?? t('VideoCard.videoPlayer')}
-            onLoad={() => {
-              setLoaded(true)
-            }}
-            width="100%"
-            height="100%"
-            className={cn('absolute top-0 left-0 size-full border-0', {
-              'animate-pulse bg-background-passive-tertiary': !isLoaded,
-            })}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-          />
-        ) : (
-          <Typography>{t('VideoCard.unsupportedVideo')}</Typography>
-        )}
-      </div>
+      <CookieConsentGate contentUrl={url} providerName={platform}>
+        <div className="relative aspect-video w-full overflow-hidden rounded-lg">
+          {src ? (
+            <iframe
+              src={src}
+              title={title ?? t('VideoCard.videoPlayer')}
+              onLoad={() => {
+                setLoaded(true)
+              }}
+              width="100%"
+              height="100%"
+              className={cn('absolute top-0 left-0 size-full border-0', {
+                'animate-pulse bg-background-passive-tertiary': !isLoaded,
+              })}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            />
+          ) : (
+            <Typography>{t('VideoCard.unsupportedVideo')}</Typography>
+          )}
+        </div>
+      </CookieConsentGate>
 
       <div className="relative flex grow flex-col gap-1">
         <MLink
