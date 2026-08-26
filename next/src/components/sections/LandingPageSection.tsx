8992024269c96b0d75e7f3f4d17a6517558fc4ad
shegood <petrsigut10@gmail.com>
2026-08-26 16:46:19 +0200

    add titles, use strapi image and use ternary operator

--- next/src/components/sections/LandingPageSection.tsx
+++ next/src/components/sections/LandingPageSection.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image'
-
 import LinkCard from '@/src/components/cards/LinkCard'
 import Banner from '@/src/components/common/Banner/Banner'
 import ResponsiveCarousel from '@/src/components/common/Carousel/ResponsiveCarousel'
+import StrapiImage from '@/src/components/common/Image/StrapiImage'
 import SectionContainer from '@/src/components/layouts/SectionContainer'
 import { LandingPageSectionFragment } from '@/src/services/graphql'
 import { generateImageSizes } from '@/src/utils/generateImageSizes'
@@ -12,53 +11,56 @@ import { isDefined } from '@/src/utils/isDefined'
 type LandingPageSectionProps = { section: LandingPageSectionFragment }
 
 const LandingPageSection = ({ section }: LandingPageSectionProps) => {
-  const { media, variant, bannerVariant, ...restBannerProps } = section.landingPageBanner
+  const { landingPageBanner, landingPageVariant, landingPageImage } = section
+  const bannerColorVariant = landingPageBanner?.variant
 
   const filteredCardLinks = section.cardLinks?.filter(isDefined) ?? []
 
   return (
     <SectionContainer className="py-6 lg:py-12">
       <div className="flex flex-col gap-6 lg:gap-8">
-        {bannerVariant === 'banner' ? (
-          <Banner imagePath={media.url} variant={variant} {...restBannerProps} />
-        ) : (
+        {landingPageVariant === 'banner' && landingPageBanner && bannerColorVariant ? (
+          <Banner
+            {...landingPageBanner}
+            imagePath={landingPageBanner.media.url}
+            variant={bannerColorVariant}
+          />
+        ) : landingPageImage ? (
           <div className="relative h-147 w-full overflow-hidden rounded-xl">
-            <Image
-              src={media.url}
-              className="object-cover"
+            <StrapiImage
               alt=""
+              image={landingPageImage}
+              sizes="100vw"
+              className="object-cover"
               fill
-              sizes={generateImageSizes({ default: '100vw', lg: '50vw' })}
             />
           </div>
-        )}
+        ) : null}
 
-        {filteredCardLinks.length > 0 && (
+        {filteredCardLinks.length > 0 ? (
           <ResponsiveCarousel
-            items={filteredCardLinks
-              .map((card, index) => {
-                const cardImage =
-                  card.media ??
-                  // If more links are filled in strapi (e.g. both page and article), choose the first non-empty field
-                  (card.page ? card.page.pageBackgroundImage : card.article?.coverMedia)
+            items={filteredCardLinks.map((card, index) => {
+              const cardImage =
+                card.media ??
+                // If more links are filled in strapi (e.g. both page and article), choose the first non-empty field
+                (card.page ? card.page.pageBackgroundImage : card.article?.coverMedia)
 
-                const imageSizes = generateImageSizes({ default: '100vw', md: '50vw', lg: '33vw' })
+              const imageSizes = generateImageSizes({ default: '100vw', md: '50vw', lg: '33vw' })
 
-                return (
-                  <LinkCard
-                    key={index}
-                    text={card.subtext}
-                    image={cardImage}
-                    imageSizes={imageSizes}
-                    linkProps={getLinkProps(card)}
-                  />
-                )
-              })
-              .filter(isDefined)}
+              return (
+                <LinkCard
+                  key={index}
+                  text={card.subtext}
+                  image={cardImage}
+                  imageSizes={imageSizes}
+                  linkProps={getLinkProps(card)}
+                />
+              )
+            })}
             hasVerticalPadding={false}
             hideControls
           />
-        )}
+        ) : null}
       </div>
     </SectionContainer>
   )
